d97673d414b47dd4696be2464bbc18e5b19c06cb
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-09 03:33:24 +0100

    Update and rename Farida to Gold Coin.cpp

--- DP/Gold Coin.cpp
+++ DP/Gold Coin.cpp
@@ -1,7 +1,8 @@
-//Once upon time there was a cute princess called Farida living in a castle with her father, mother and uncle. On the way to the castle there lived many monsters. Each one of them had some gold coins. Although they are monsters they will not hurt. Instead they will give you the gold coins, but if and only if you didn't take any coins from the monster directly before the current one. To marry princess Farida you have to pass all the monsters and collect as many coins as possible. Given the number of gold coins each monster has, calculate the maximum number of coins you can collect on your way to the castle. 
+// On the way to the castle there lived many monsters. Each one of them had some gold coins. Although they are monsters they will not hurt. 
+//Instead they will give you the gold coins, but if and only if you didn't take any coins from the monster directly before the current one.pass all the monsters and collect as many coins as possible. 
+//Given the number of gold coins each monster has, calculate the maximum number of coins you can collect on your way to the castle. 
 #include <iostream>
 #include <vector>
-
 using namespace std;
 
 int maxGold(vector<int>& monsterGold) {
@@ -11,7 +12,6 @@ int maxGold(vector<int>& monsterGold) {
     for (int i = n - 1; i >= 0; i--) {
         dp[i] = max(monsterGold[i] + dp[i + 2], dp[i + 1]);
     }
-
     return dp[0];
 }
 
