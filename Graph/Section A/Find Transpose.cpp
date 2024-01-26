08f429da55f56ce2e67f27432a28bbbf8704ad82
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-26 08:29:57 +0100

    Update and rename Find transpose to Find Transpose.cpp

--- Graph/Section A/Find Transpose.cpp
+++ Graph/Section A/Find Transpose.cpp
@@ -1,13 +1,11 @@
-// CPP program to find transpose of a graph.
+// find transpose of a graph.
 #include <bits/stdc++.h>
 using namespace std;
- 
 // function to add an edge from vertex source to vertex dest
 void addEdge(vector<int> adj[], int src, int dest)
 {
     adj[src].push_back(dest); 
 }
- 
 // function to print adjacency list of a graph
 void displayGraph(vector<int> adj[], int v)
 {
@@ -18,20 +16,16 @@ void displayGraph(vector<int> adj[], int v)
         cout << "\n";
     }
 }
- 
 // function to get Transpose of a graph taking adjacency
 // list of given graph and that of Transpose graph
-void transposeGraph(vector<int> adj[], 
-                     vector<int> transpose[], int v)
+void transposeGraph(vector<int> adj[], vector<int> transpose[], int v)
 {
-    // traverse the adjacency list of given graph and
-    // for each edge (u, v) add an edge (v, u) in the
+    // traverse the adjacency list of given graph and for each edge (u, v) add an edge (v, u) in the
     // transpose graph's adjacency list
     for (int i = 0; i < v; i++)
         for (int j = 0; j < adj[i].size(); j++)
             addEdge(transpose, adj[i][j], i);
 }
- 
 int main()
 {
     int v = 5;
@@ -44,14 +38,10 @@ int main()
     addEdge(adj, 4, 1);
     addEdge(adj, 4, 3);
  
-    // Finding transpose of graph represented
-    // by adjacency list adj[]
+    // Finding transpose of graph represented by adjacency list adj[]
     vector<int> transpose[v];
     transposeGraph(adj, transpose, v);
- 
     // displaying adjacency list of transpose 
-    // graph i.e. b
     displayGraph(transpose, v);
- 
     return 0;
 }
