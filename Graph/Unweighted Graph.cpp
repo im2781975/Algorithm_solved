f9cfae18a2556e6f9aa309d927cb49f6b27aba94
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-26 15:58:02 +0100

    Update and rename Graph/Section C/Unweighted graph to Graph/Unweighted Graph.cpp

--- Graph/Unweighted Graph.cpp
+++ Graph/Unweighted Graph.cpp
@@ -1,48 +1,34 @@
-// CPP code for printing shortest path between
+// printing shortest path between
 // two vertices of unweighted graph
 #include <bits/stdc++.h>
 using namespace std;
- 
-// utility function to form edge between two vertices
-// source and dest
+//function to form edge between two vertices source and dest
 void add_edge(vector<int> adj[], int src, int dest)
 {
     adj[src].push_back(dest);
     adj[dest].push_back(src);
 }
- 
-// a modified version of BFS that stores predecessor
-// of each vertex in array p
+// a modified version of BFS that stores predecessor of each vertex in array p
 // and its distance from source in array d
 bool BFS(vector<int> adj[], int src, int dest, int v,
          int pred[], int dist[])
 {
-    // a queue to maintain queue of vertices whose
-    // adjacency list is to be scanned as per normal
-    // DFS algorithm
+    // a queue to maintain queue of vertices whose adjacency list is to be scanned as per normal DFS algorithm
     list<int> queue;
- 
-    // boolean array visited[] which stores the
-    // information whether ith vertex is reached
+    // boolean array visited[] which stores the information whether ith vertex is reached
     // at least once in the Breadth first search
     bool visited[v];
- 
-    // initially all vertices are unvisited
-    // so v[i] for all i is false
-    // and as no path is yet constructed
-    // dist[i] for all i set to infinity
+    // initially all vertices are unvisited so v[i] for all i is false
+    // and as no path is yet constructed dist[i] for all i set to infinity
     for (int i = 0; i < v; i++) {
         visited[i] = false;
         dist[i] = INT_MAX;
         pred[i] = -1;
     }
- 
-    // now source is first to be visited and
-    // distance from source to itself should be 0
+    // now source is first to be visited and distance from source to itself should be 0
     visited[src] = true;
     dist[src] = 0;
     queue.push_back(src);
- 
     // standard BFS algorithm
     while (!queue.empty()) {
         int u = queue.front();
@@ -61,17 +47,12 @@ bool BFS(vector<int> adj[], int src, int dest, int v,
             }
         }
     }
- 
     return false;
 }
- 
-// utility function to print the shortest distance
-// between source vertex and destination vertex
-void printShortestDistance(vector<int> adj[], int s,
-                           int dest, int v)
+// utility function to print the shortest distance between src vertex and dst vertex
+void printShortestDistance(vector<int> adj[], int s, int dest, int v)
 {
-    // predecessor[i] array stores predecessor of
-    // i and distance array stores distance of i
+    // predecessor[i] array stores predecessor of i and distance array stores distance of i
     // from s
     int pred[v], dist[v];
  
@@ -80,7 +61,6 @@ void printShortestDistance(vector<int> adj[], int s,
              << " are not connected";
         return;
     }
- 
     // vector path stores the shortest path
     vector<int> path;
     int crawl = dest;
@@ -89,30 +69,21 @@ void printShortestDistance(vector<int> adj[], int s,
         path.push_back(pred[crawl]);
         crawl = pred[crawl];
     }
- 
     // distance from source is in distance array
-    cout << "Shortest path length is : "
-         << dist[dest];
- 
+    cout << "Shortest path length is : "  << dist[dest];
     // printing path from source to destination
     cout << "\nPath is::\n";
     for (int i = path.size() - 1; i >= 0; i--)
         cout << path[i] << " ";
 }
- 
-// Driver program to test above functions
 int main()
 {
-    // no. of vertices
     int v = 8;
- 
     // array of vectors is used to store the graph
     // in the form of an adjacency list
     vector<int> adj[v];
  
-    // Creating graph given in the above diagram.
-    // add_edge function takes adjacency list, source
-    // and destination vertex as argument and forms
+    // add_edge function takes adjacency list, source and destination vertex as argument and forms
     // an edge between them.
     add_edge(adj, 0, 1);
     add_edge(adj, 0, 3);
