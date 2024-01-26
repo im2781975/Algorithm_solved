c39483e8d797c9b861910ac1bebd2c0452e9dc32
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-26 19:48:32 +0100

    Update and rename Graph/Section D/Biconnected component to Graph/Biconnected Component.cpp

--- Graph/Biconnected Component.cpp
+++ Graph/Biconnected Component.cpp
@@ -1,4 +1,4 @@
-// A C++ program to find biconnected components in a given undirected graph
+// find biconnected components in a given undirected graph
 #include <iostream>
 #include <list>
 #include <stack>
@@ -6,7 +6,7 @@
 using namespace std;
 int count = 0;
 class Edge {
-public:
+    public:
     int u;
     int v;
     Edge(int u, int v);
@@ -16,36 +16,30 @@ Edge::Edge(int u, int v)
     this->u = u;
     this->v = v;
 }
- 
 // A class that represents an directed graph
 class Graph {
-    int V; // No. of vertices
-    int E; // No. of edges
-    list<int>* adj; // A dynamic array of adjacency lists
- 
+    int V; 
+    int E; 
+    list<int>* adj;
     // A Recursive DFS based function used by BCC()
-    void BCCUtil(int u, int disc[], int low[],
-                 list<Edge>* st, int parent[]);
- 
-public:
-    Graph(int V); // Constructor
-    void addEdge(int v, int w); // function to add an edge to graph
-    void BCC(); // prints strongly connected components
+    void BCCUtil(int u, int disc[], int low[], list<Edge>* st, int parent[]);
+    public:
+    Graph(int V); 
+    void addEdge(int v, int w); 
+    void BCC(); 
+    // prints strongly connected components
 };
- 
 Graph::Graph(int V)
 {
     this->V = V;
     this->E = 0;
     adj = new list<int>[V];
 }
- 
 void Graph::addEdge(int v, int w)
 {
     adj[v].push_back(w);
     E++;
 }
- 
 // A recursive function that finds and prints strongly connected
 // components using DFS traversal
 // u --> The vertex to be visited next
@@ -54,13 +48,11 @@ void Graph::addEdge(int v, int w)
 // discovery time) that can be reached from subtree
 // rooted with current vertex
 // *st -- >> To store visited edges
-void Graph::BCCUtil(int u, int disc[], int low[], list<Edge>* st,
-                    int parent[])
+void Graph::BCCUtil(int u, int disc[], int low[], list<Edge>* st, int parent[])
 {
     // A static variable is used for simplicity, we can avoid use
     // of static variable by passing a pointer.
     static int time = 0;
- 
     // Initialize discovery time and low value
     disc[u] = low[u] = ++time;
     int children = 0;
@@ -78,13 +70,11 @@ void Graph::BCCUtil(int u, int disc[], int low[], list<Edge>* st,
             st->push_back(Edge(u, v));
             BCCUtil(v, disc, low, st, parent);
  
-            // Check if the subtree rooted with 'v' has a
-            // connection to one of the ancestors of 'u'
+            // Check if the subtree rooted with 'v' has a connection to one of the ancestors of 'u'
             // Case 1 -- per Strongly Connected Components Article
             low[u] = min(low[u], low[v]);
  
-            // If u is an articulation point,
-            // pop all edges from stack till u -- v
+            // If u is an articulation point, pop all edges from stack till u -- v
             if ((disc[u] == 1 && children > 1) || (disc[u] > 1 && low[v] >= disc[u])) {
                 while (st->back().u != u || st->back().v != v) {
                     cout << st->back().u << "--" << st->back().v << " ";
@@ -96,9 +86,7 @@ void Graph::BCCUtil(int u, int disc[], int low[], list<Edge>* st,
                 count++;
             }
         }
- 
         // Update low value of 'u' only of 'v' is still in stack
-        // (i.e. it's a back edge, not cross edge).
         // Case 2 -- per Strongly Connected Components Article
         else if (v != parent[u]) {
             low[u] = min(low[u], disc[v]);
@@ -108,7 +96,6 @@ void Graph::BCCUtil(int u, int disc[], int low[], list<Edge>* st,
         }
     }
 }
- 
 // The function to do DFS traversal. It uses BCCUtil()
 void Graph::BCC()
 {
@@ -116,14 +103,12 @@ void Graph::BCC()
     int* low = new int[V];
     int* parent = new int[V];
     list<Edge>* st = new list<Edge>[E];
- 
     // Initialize disc and low, and parent arrays
     for (int i = 0; i < V; i++) {
         disc[i] = NIL;
         low[i] = NIL;
         parent[i] = NIL;
     }
- 
     for (int i = 0; i < V; i++) {
         if (disc[i] == NIL)
             BCCUtil(i, disc, low, st, parent);
@@ -141,7 +126,6 @@ void Graph::BCC()
         }
     }
 }
-// Driver program to test above function
 int main()
 {
     Graph g(12);
@@ -177,3 +161,4 @@ int main()
     cout << "Above are " << count << " biconnected components in graph";
     return 0;
 }
+
