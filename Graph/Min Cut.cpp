b7c54ed2e09e047415a25ed397e6359e59a47616
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-27 15:44:36 +0100

    Update and rename Graph/Section D/Min s-t cut to Graph/Min Cut.cpp

--- Graph/Min Cut.cpp
+++ Graph/Min Cut.cpp
@@ -1,29 +1,22 @@
-// C++ program for finding minimum cut using Ford-Fulkerson
+// finding minimum cut using Ford-Fulkerson
 #include <iostream>
 #include <limits.h>
 #include <string.h>
 #include <queue>
 using namespace std;
- 
-// Number of vertices in given graph
 #define V 6
- 
-/* Returns true if there is a path from source 's' to sink 't' in
-  residual graph. Also fills parent[] to store the path */
+//Returns true if there is a path from source 's' to sink 't' in residual graph. Also fills parent[] to store the path
 int bfs(int rGraph[V][V], int s, int t, int parent[])
 {
     // Create a visited array and mark all vertices as not visited
     bool visited[V];
     memset(visited, 0, sizeof(visited));
  
-    // Create a queue, enqueue source vertex and mark source vertex
-    // as visited
+    // Create a queue, enqueue source vertex and mark source vertex as visited
     queue <int> q;
     q.push(s);
     visited[s] = true;
     parent[s] = -1;
- 
-    // Standard BFS Loop
     while (!q.empty())
     {
         int u = q.front();
@@ -39,9 +32,7 @@ int bfs(int rGraph[V][V], int s, int t, int parent[])
             }
         }
     }
- 
-    // If we reached sink in BFS starting from source, then return
-    // true, else false
+    // If we reached sink in BFS starting from source, then return true, else false
     return (visited[t] == true);
 }
  
@@ -55,37 +46,34 @@ void dfs(int rGraph[V][V], int s, bool visited[])
        if (rGraph[s][i] && !visited[i])
            dfs(rGraph, i, visited);
 }
- 
 // Prints the minimum s-t cut
 void minCut(int graph[V][V], int s, int t)
 {
     int u, v;
  
     // Create a residual graph and fill the residual graph with
-    // given capacities in the original graph as residual capacities
-    // in residual graph
-    int rGraph[V][V]; // rGraph[i][j] indicates residual capacity of edge i-j
+    // given capacities in the original graph as residual capacities in residual graph
+    int rGraph[V][V];
+    // rGraph[i][j] indicates residual capacity of edge i-j
     for (u = 0; u < V; u++)
         for (v = 0; v < V; v++)
              rGraph[u][v] = graph[u][v];
  
-    int parent[V];  // This array is filled by BFS and to store path
+    int parent[V]; 
+    // This array is filled by BFS and to store path
  
     // Augment the flow while there is a path from source to sink
     while (bfs(rGraph, s, t, parent))
     {
         // Find minimum residual capacity of the edges along the
-        // path filled by BFS. Or we can say find the maximum flow
-        // through the path found.
+        // path filled by BFS. Or we can say find the maximum flow through the path found.
         int path_flow = INT_MAX;
         for (v=t; v!=s; v=parent[v])
         {
             u = parent[v];
             path_flow = min(path_flow, rGraph[u][v]);
         }
- 
-        // update residual capacities of the edges and reverse edges
-        // along the path
+        // update residual capacities of the edges and reverse edges along the path
         for (v=t; v != s; v=parent[v])
         {
             u = parent[v];
@@ -93,7 +81,6 @@ void minCut(int graph[V][V], int s, int t)
             rGraph[v][u] += path_flow;
         }
     }
- 
     // Flow is maximum now, find vertices reachable from s
     bool visited[V];
     memset(visited, false, sizeof(visited));
@@ -108,20 +95,14 @@ void minCut(int graph[V][V], int s, int t)
  
     return;
 }
- 
-// Driver program to test above functions
 int main()
 {
-    // Let us create a graph shown in the above example
     int graph[V][V] = { {0, 16, 13, 0, 0, 0},
-                        {0, 0, 10, 12, 0, 0},
-                        {0, 4, 0, 0, 14, 0},
-                        {0, 0, 9, 0, 0, 20},
-                        {0, 0, 0, 7, 0, 4},
-                        {0, 0, 0, 0, 0, 0}
-                      };
- 
+    {0, 0, 10, 12, 0, 0},
+    {0, 4, 0, 0, 14, 0},
+    {0, 0, 9, 0, 0, 20},
+    {0, 0, 0, 7, 0, 4},
+    {0, 0, 0, 0, 0, 0}};
     minCut(graph, 0, 5);
- 
     return 0;
 }
