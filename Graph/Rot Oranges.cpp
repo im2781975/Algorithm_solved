8faa4821d1246b8832b623099c0aa189d0dabaa6
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-27 15:36:21 +0100

    Update and rename Graph/Section D/Min rot to Graph/Rot Oranges.cpp

--- Graph/Rot Oranges.cpp
+++ Graph/Rot Oranges.cpp
@@ -1,11 +1,8 @@
-// C++ program to rot all oranges when you can move
-// in all the four direction from a rotten orange
+//rot all oranges when you can move in all the four direction from a rotten orange
 #include <bits/stdc++.h>
 using namespace std;
- 
 const int R = 3;
 const int C = 5;
- 
 // Check if i, j is under the array limits of row and column
 bool issafe(int i, int j)
 {
@@ -13,7 +10,6 @@ bool issafe(int i, int j)
         return true;
     return false;
 }
- 
 int rotOranges(int v[R][C])
 {
     bool changed = false;
@@ -21,9 +17,7 @@ int rotOranges(int v[R][C])
     while (true) {
         for (int i = 0; i < R; i++) {
             for (int j = 0; j < C; j++) {
- 
-                // Rot all other oranges present at
-                // (i+1, j), (i, j-1), (i, j+1), (i-1, j)
+                // Rot all other oranges present at(i+1, j), (i, j-1), (i, j+1), (i-1, j)
                 if (v[i][j] == no) {
                     if (issafe(i + 1, j)
                         && v[i + 1][j] == 1) {
@@ -48,39 +42,27 @@ int rotOranges(int v[R][C])
                 }
             }
         }
- 
-        // if no rotten orange found it means all
-        // oranges rottened now
+        // if no rotten orange found it means all oranges rottened now
         if (!changed)
             break;
         changed = false;
         no++;
     }
- 
     for (int i = 0; i < R; i++) {
         for (int j = 0; j < C; j++) {
  
-            // if any orange is found to be
-            // not rotten then ans is not possible
+            // if any orange is found to be not rotten then ans is not possible
             if (v[i][j] == 1)
                 return -1;
         }
     }
- 
-    // Because initial value for a rotten
-    // orange was 2
+    // Because initial value for a rotten orange was 2
     return no - 2;
 }
- 
-// Driver function
 int main()
 {
- 
     int v[R][C] = { { 2, 1, 0, 2, 1 },
-                    { 1, 0, 1, 2, 1 },
-                    { 1, 0, 0, 2, 1 } };
- 
+    { 1, 0, 1, 2, 1 },{ 1, 0, 0, 2, 1 } };
     cout << "Max time incurred: " << rotOranges(v);
- 
     return 0;
 }
