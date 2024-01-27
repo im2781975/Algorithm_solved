475f98f917acbe30502f3c6e03042f19cc91f308
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-27 19:16:54 +0100

    Update and rename Convert array to Convert Array.cpp

--- List/Circular doubly/Convert Array.cpp
+++ List/Circular doubly/Convert Array.cpp
@@ -1,23 +1,18 @@
-// CPP program to convert array to 
-// circular doubly linked list
- 
+
+//convert array
 #include<bits/stdc++.h>
 using namespace std;
- 
-// Doubly linked list node
 struct node
 {
     int data;
     struct node *next;
     struct node *prev;
 };
- 
-// Utility function to create a node in memory
+// function to create a node in memory
 struct node* getNode()
 {
     return ((struct node *)malloc(sizeof(struct node)));
 }
- 
 // Function to display the list
 int displayList(struct node *temp)
 {
@@ -33,32 +28,25 @@ int displayList(struct node *temp)
             cout<<temp->data<<" ";
             temp = temp->next;
         }
-         
         cout<<temp->data;
-         
         return 1;
     } 
 }
- 
 // Function to convert array into list
 void createList(int arr[], int n, struct node **start)
 {
     // Declare newNode and temporary pointer
     struct node *newNode,*temp;
     int i;
-     
     // Iterate the loop until array length
     for(i=0;i<n;i++)
     {
         // Create new node
         newNode = getNode();
-         
         // Assign the array data
         newNode->data = arr[i];
          
-        // If it is first element
-        // Put that node prev and next as start
-        // as it is circular
+        // If it is first element Put that node prev and next as start as it is circular
         if(i==0)
         {
             *start = newNode;
@@ -69,9 +57,7 @@ void createList(int arr[], int n, struct node **start)
         {   
             // Find the last node
             temp = (*start)->prev;
-             
-            // Add the last node to make them
-            // in circular fashion
+            // Add the last node to make them in circular fashion
             temp->next = newNode;
             newNode->next = *start;
             newNode->prev = temp;
@@ -80,22 +66,15 @@ void createList(int arr[], int n, struct node **start)
         }
     }
 }
- 
-// Driver Code
 int main()
 {
     // Array to be converted
     int arr[] = {1,2,3,4,5};
     int n = sizeof(arr) / sizeof(arr[0]);
-     
     // Start Pointer
     struct node *start = NULL;
-     
     // Create the List
     createList(arr, n, &start);
-     
-    // Display the list
     displayList(start);
-     
     return 0;
 }
