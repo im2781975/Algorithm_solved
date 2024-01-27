393f33ec058b50e45da76a0b0d3901e542d97ed0
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-27 16:48:19 +0100

    Update and rename Graph/Section D/Travelling salesman to Graph/Travelling Salesman.cpp

--- Graph/Travelling Salesman.cpp
+++ Graph/Travelling Salesman.cpp
@@ -1,10 +1,7 @@
-// CPP program to implement traveling salesman
-// problem using naive approach.
+// implement traveling salesman problem using naive approach.
 #include <bits/stdc++.h>
 using namespace std;
 #define V 4
- 
-// implementation of traveling Salesman Problem
 int travllingSalesmanProblem(int graph[][V], int s)
 {
     // store all vertex apart from source vertex
@@ -12,14 +9,11 @@ int travllingSalesmanProblem(int graph[][V], int s)
     for (int i = 0; i < V; i++)
         if (i != s)
             vertex.push_back(i);
- 
     // store minimum weight Hamiltonian Cycle.
     int min_path = INT_MAX;
     do {
- 
         // store current Path weight(cost)
         int current_pathweight = 0;
- 
         // compute current path weight
         int k = s;
         for (int i = 0; i < vertex.size(); i++) {
@@ -27,24 +21,17 @@ int travllingSalesmanProblem(int graph[][V], int s)
             k = vertex[i];
         }
         current_pathweight += graph[k][s];
- 
         // update minimum
         min_path = min(min_path, current_pathweight);
- 
     } while (
         next_permutation(vertex.begin(), vertex.end()));
- 
     return min_path;
 }
- 
-// Driver Code
 int main()
 {
-    // matrix representation of graph
     int graph[][V] = { { 0, 10, 15, 20 },
-                       { 10, 0, 35, 25 },
-                       { 15, 35, 0, 30 },
-                       { 20, 25, 30, 0 } };
+    { 10, 0, 35, 25 },{ 15, 35, 0, 30 },
+    { 20, 25, 30, 0 } };
     int s = 0;
     cout << travllingSalesmanProblem(graph, s) << endl;
     return 0;
