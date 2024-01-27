d06bafb2db6208a05c8d8022bdc70de2b25d8cf3
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-27 15:19:24 +0100

    Update and rename Graph/Section D/K centre to Graph/K Cities.cpp

--- Graph/K Cities.cpp
+++ Graph/K Cities.cpp
@@ -1,7 +1,5 @@
-// C++ program for the above approach
 #include <bits/stdc++.h>
 using namespace std;
- 
 int maxindex(int* dist, int n)
 {
     int mi = 0;
@@ -11,7 +9,6 @@ int maxindex(int* dist, int n)
     }
     return mi;
 }
- 
 void selectKcities(int n, int weights[4][4], int k)
 {
     int* dist = new int[n];
@@ -19,51 +16,31 @@ void selectKcities(int n, int weights[4][4], int k)
     for (int i = 0; i < n; i++) {
         dist[i] = INT_MAX;
     }
- 
-    // index of city having the
-    // maximum distance to it's
-    // closest center
+    // index of city having the maximum distance to it's closest center
     int max = 0;
     for (int i = 0; i < k; i++) {
         centers.push_back(max);
         for (int j = 0; j < n; j++) {
- 
-            // updating the distance
-            // of the cities to their
-            // closest centers
+            // updating the distance of the cities to their closest centers
             dist[j] = min(dist[j], weights[max][j]);
         }
- 
-        // updating the index of the
-        // city with the maximum
-        // distance to it's closest center
+        // updating the index of the city with the maximum distance to it's closest center
         max = maxindex(dist, n);
     }
- 
-    // Printing the maximum distance
-    // of a city to a center
-    // that is our answer
+    // Printing the maximum distance of a city to a center that is our answer
     cout << endl << dist[max] << endl;
- 
-    // Printing the cities that
-    // were chosen to be made
-    // centers
+    // Printing the cities that were chosen to be made centers
     for (int i = 0; i < centers.size(); i++) {
         cout << centers[i] << " ";
     }
     cout << endl;
 }
- 
-// Driver Code
 int main()
 {
     int n = 4;
     int weights[4][4] = { { 0, 4, 8, 5 },
-                          { 4, 0, 10, 7 },
-                          { 8, 10, 0, 9 },
-                          { 5, 7, 9, 0 } };
+    { 4, 0, 10, 7 }, { 8, 10, 0, 9 },
+   { 5, 7, 9, 0 } };
     int k = 2;
- 
-    // Function Call
     selectKcities(n, weights, k);
 }
