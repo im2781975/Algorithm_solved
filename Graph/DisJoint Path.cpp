4cbabdb8f5d69a2cdf42b8da37cb717eb963b701
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-27 13:13:27 +0100

    Update and rename Graph/Section D/Edje disjoint path to Graph/DisJoint Path.cpp

--- Graph/DisJoint Path.cpp
+++ Graph/DisJoint Path.cpp
@@ -1,29 +1,22 @@
-// C++ program to find maximum number of edge disjoint paths 
+// find maximum number of edge disjoint paths 
 #include <iostream> 
 #include <limits.h> 
 #include <string.h> 
 #include <queue> 
 using namespace std; 
- 
-// Number of vertices in given graph 
 #define V 8 
- 
-/* Returns true if there is a path from source 's' to sink 't' in 
-residual graph. Also fills parent[] to store the path */
+//Returns true if there is a path from source 's' to sink 't' in
+//residual graph. Also fills parent[] to store the path */
 bool bfs(int rGraph[V][V], int s, int t, int parent[]) 
 { 
-    // Create a visited array and mark all vertices as not visited 
     bool visited[V]; 
     memset(visited, 0, sizeof(visited)); 
  
-    // Create a queue, enqueue source vertex and mark source vertex 
-    // as visited 
+    // Create a queue, enqueue source vertex and mark source vertex  as visited 
     queue <int> q; 
     q.push(s); 
     visited[s] = true; 
     parent[s] = -1; 
- 
-    // Standard BFS Loop 
     while (!q.empty()) 
     { 
         int u = q.front(); 
@@ -40,8 +33,7 @@ bool bfs(int rGraph[V][V], int s, int t, int parent[])
         } 
     } 
  
-    // If we reached sink in BFS starting from source, then return 
-    // true, else false 
+    // If we reached sink in BFS starting from source, then return true, else false 
     return (visited[t] == true); 
 } 
  
@@ -54,40 +46,36 @@ int findDisjointPaths(int graph[V][V], int s, int t)
     // Create a residual graph and fill the residual graph with 
     // given capacities in the original graph as residual capacities 
     // in residual graph 
-    int rGraph[V][V]; // Residual graph where rGraph[i][j] indicates 
-                    // residual capacity of edge from i to j (if there 
-                    // is an edge. If rGraph[i][j] is 0, then there is not) 
+    int rGraph[V][V]; 
+    // Residual graph where rGraph[i][j] indicates residual capacity of edge
+     //from i to j (if there is an edge. If rGraph[i][j] is 0, then there is not) 
     for (u = 0; u < V; u++) 
         for (v = 0; v < V; v++) 
             rGraph[u][v] = graph[u][v]; 
  
-    int parent[V]; // This array is filled by BFS and to store path 
- 
+    int parent[V]; 
+    // This array is filled by BFS and to store path 
     int max_flow = 0; // There is no flow initially 
  
     // Augment the flow while there is path from source to sink 
     while (bfs(rGraph, s, t, parent)) 
     { 
         // Find minimum residual capacity of the edges along the 
-        // path filled by BFS. Or we can say find the maximum flow 
-        // through the path found. 
+        // path filled by BFS. Or we can say find the maximum flow  through the path found. 
         int path_flow = INT_MAX; 
- 
         for (v=t; v!=s; v=parent[v]) 
         { 
             u = parent[v]; 
             path_flow = min(path_flow, rGraph[u][v]); 
         } 
  
-        // update residual capacities of the edges and reverse edges 
-        // along the path 
+        // update residual capacities of the edges and reverse edges along the path 
         for (v=t; v != s; v=parent[v]) 
         { 
             u = parent[v]; 
             rGraph[u][v] -= path_flow; 
             rGraph[v][u] += path_flow; 
         } 
- 
         // Add path flow to overall flow 
         max_flow += path_flow; 
     } 
@@ -96,25 +84,20 @@ int findDisjointPaths(int graph[V][V], int s, int t)
     // number of edge-disjoint paths) 
     return max_flow; 
 } 
- 
-// Driver program to test above functions 
 int main() 
 { 
-    // Let us create a graph shown in the above example 
     int graph[V][V] = { {0, 1, 1, 1, 0, 0, 0, 0}, 
-                        {0, 0, 1, 0, 0, 0, 0, 0}, 
-                        {0, 0, 0, 1, 0, 0, 1, 0}, 
-                        {0, 0, 0, 0, 0, 0, 1, 0}, 
-                        {0, 0, 1, 0, 0, 0, 0, 1}, 
-                        {0, 1, 0, 0, 0, 0, 0, 1}, 
-                        {0, 0, 0, 0, 0, 1, 0, 1}, 
-                        {0, 0, 0, 0, 0, 0, 0, 0} 
-                    }; 
+    {0, 0, 1, 0, 0, 0, 0, 0}, 
+     {0, 0, 0, 1, 0, 0, 1, 0}, 
+    {0, 0, 0, 0, 0, 0, 1, 0}, 
+    {0, 0, 1, 0, 0, 0, 0, 1}, 
+    {0, 1, 0, 0, 0, 0, 0, 1}, 
+    {0, 0, 0, 0, 0, 1, 0, 1}, 
+    {0, 0, 0, 0, 0, 0, 0, 0} }; 
  
     int s = 0; 
     int t = 7; 
     cout << "There can be maximum " << findDisjointPaths(graph, s, t) 
         << " edge-disjoint paths from " << s <<" to "<< t ; 
- 
     return 0; 
 }
