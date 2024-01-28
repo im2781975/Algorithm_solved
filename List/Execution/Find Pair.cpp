d07dda6dccc1add0caa4be2a1b9eaeeb30c21a15
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-28 15:33:48 +0100

    Update and rename Find pair to Find Pair.cpp

--- List/Execution/Find Pair.cpp
+++ List/Execution/Find Pair.cpp
@@ -1,30 +1,23 @@
-// C++ program to find a pair with given sum x.  
+//find a pair with given sum x.  
 #include<bits/stdc++.h>  
 using namespace std;  
-  
-// structure of node of doubly linked list  
 struct Node  
 {  
     int data;  
     struct Node *next, *prev;  
 };  
-  
 // Function to find pair whose sum equal to given value x.  
 void pairSum(struct Node *head, int x)  
 {  
-    // Set two pointers, first to the beginning of DLL  
-    // and second to the end of DLL.  
+    // Set two pointers, first to the beginning of DLL &  second to the end of DLL 
     struct Node *first = head;  
     struct Node *second = head;  
     while (second->next != NULL)  
         second = second->next;  
-  
     // To track if we find a pair or not  
     bool found = false;  
   
-    // The loop terminates when two pointers  
-    // cross each other (second->next  
-    // == first), or they become same (first == second)  
+    // The loop terminates when two pointers  cross each other (second->next == first), or they become same (first == second)  
     while (first != second && second->next != first)  
     {  
         // pair found  
@@ -33,12 +26,10 @@ void pairSum(struct Node *head, int x)
             found = true;  
             cout << "(" << first->data<< ", "
                 << second->data << ")" << endl;  
-  
             // move first in forward direction  
             first = first->next;  
-  
-            // move second in backward direction  
-            second = second->prev;  
+            // move second in backward direction
+            second = second->prev;
         }  
         else
         {  
@@ -48,19 +39,16 @@ void pairSum(struct Node *head, int x)
                 second = second->prev;  
         }  
     }  
-  
     // if pair is not present  
     if (found == false)  
         cout << "No pair found";  
-}  
-  
-// A utility function to insert a new node at the  
-// beginning of doubly linked list  
+}
+// insert a new node at the beginning of doubly linked list
 void insert(struct Node **head, int data)  
 {  
     struct Node *temp = new Node;  
     temp->data = data;  
-    temp->next = temp->prev = NULL;  
+    temp->next = temp->prev = NULL;
     if (!(*head))  
         (*head) = temp;  
     else
@@ -69,9 +57,7 @@ void insert(struct Node **head, int data)
         (*head)->prev = temp;  
         (*head) = temp;  
     }  
-}  
-  
-// Driver program  
+}
 int main()  
 {  
     struct Node *head = NULL;  
@@ -83,7 +69,6 @@ int main()
     insert(&head, 2);  
     insert(&head, 1);  
     int x = 7;  
-  
     pairSum(head, x);  
   
     return 0;  
