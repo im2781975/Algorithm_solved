beb846f68e7083e6dbed142130ceecf221f22c42
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-28 17:45:05 +0100

    Update and rename Reverse_list<> to Reverse 3.cpp

--- List/Double/Reverse 3.cpp
+++ List/Double/Reverse 3.cpp
@@ -1,9 +1,6 @@
 #include <iostream> 
 #include <cstring> 
-  
 using namespace std; 
-  
-// Represents node of a linkedlist 
 class Node { 
   public: 
       int data; 
@@ -14,27 +11,21 @@ class Node {
           next = nullptr; 
       } 
 }; 
-    
 void printReverse(Node *head, int n) 
 { 
         int j = 0; 
         Node *current = head; 
-        while (current != nullptr) { 
+        while (current != nullptr){ 
              
-           // For each node, print proper number  
-           // of spaces before printing it  
            for (int i = 0; i < 2 * (n - j); i++) 
                 cout << " "; 
   
-           // use of carriage return to move back  
-           // and print. 
+           // use of carriage return to move back   and print. 
            cout << "\r" << current->data; 
-  
-           current = current->next; 
+           current = current->next;
            j++;  
         } 
 } 
-  
 Node *push(Node *head, int data)  
 { 
         Node *new_node = new Node(data); 
@@ -55,17 +46,11 @@ int printList(Node *head)
                temp = temp->next; 
                i++; 
         } 
-  
         return i; 
 } 
-    
-// Driver code 
 int main() 
-{ 
-      /* Start with the empty list */
+{
       Node *head = nullptr; 
-  
-      // list nodes are as 6 5 4 3 2 1 
       head = push(head, 1); 
       head = push(head, 2); 
       head = push(head, 3); 
@@ -74,15 +59,13 @@ int main()
       head = push(head, 6); 
   
       cout << "Given linked list: " << endl; 
-      // printlist print the list and  
-      // return the size of list  
+      // printlist print the list and return the size of list  
       int n = printList(head);  
   
-      // print reverse list with help  
-      // of carriage return function 
+      // print reverse list with help of carriage return function 
       cout << "Reversed Linked list: " << endl; 
       printReverse(head, n);  
       cout << endl; 
-  
       return 0; 
 } 
+
