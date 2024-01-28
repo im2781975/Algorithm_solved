328b53a2aae9e863921d5123922446919eb300de
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-28 19:20:47 +0100

    Update and rename Sublist search to Find List.cpp

--- List/Execution/Find List.cpp
+++ List/Execution/Find List.cpp
@@ -1,42 +1,32 @@
-// C++ program to find a list in second list
+// find a list in second list
 #include <bits/stdc++.h>
 using namespace std;
- 
 // A Linked List node
 struct Node
 {
     int data;
     Node* next;
 };
- 
-// Returns true if first list is present in second
-// list
+// Returns true if first list is present in second list
 bool findList(Node* first, Node* second)
 {
     Node* ptr1 = first, *ptr2 = second;
- 
     // If both linked lists are empty, return true
     if (first == NULL && second == NULL)
         return true;
- 
-    // Else If one is empty and other is not return
-    // false
+    // Else If one is empty and other is not return false
     if ( first == NULL ||
         (first != NULL && second == NULL))
         return false;
- 
-    // Traverse the second list by picking nodes
-    // one by one
+    // Traverse the second list by picking nodes one by one
     while (second != NULL)
     {
         // Initialize ptr2 with current node of second
         ptr2 = second;
- 
         // Start matching first list with second list
         while (ptr1 != NULL)
         {
-            // If second list becomes empty and first
-            // not then return false
+            // If second list becomes empty and first not then return false
             if (ptr2 == NULL)
                 return false;
  
@@ -47,27 +37,19 @@ bool findList(Node* first, Node* second)
                 ptr1 = ptr1->next;
                 ptr2 = ptr2->next;
             }
- 
             // If not equal then  break the loop
             else break;
         }
- 
-        // Return true if first list gets traversed
-        // completely that means it is matched.
+        // Return true if first list gets traversed completely that means it is matched.
         if (ptr1 == NULL)
             return true;
- 
         // Initialize ptr1 with first again
         ptr1 = first;
- 
         // And go to next node of second list
         second = second->next;
     }
- 
     return false;
 }
- 
-/* Function to print nodes in a given linked list */
 void printList(Node* node)
 {
     while (node != NULL)
@@ -76,7 +58,6 @@ void printList(Node* node)
         node = node->next;
     }
 }
- 
 // Function to add new node to linked lists
 Node *newNode(int key)
 {
@@ -85,14 +66,8 @@ Node *newNode(int key)
     temp->next = NULL;
     return temp;
 }
- 
-/* Driver program to test above functions*/
 int main()
 {
-    /* Let us create two linked lists to test
-    the above functions. Created lists shall be
-        a: 1->2->3->4
-        b: 1->2->1->2->3->4*/
     Node *a = newNode(1);
     a->next = newNode(2);
     a->next->next = newNode(3);
@@ -105,8 +80,6 @@ int main()
     b->next->next->next->next = newNode(3);
     b->next->next->next->next->next = newNode(4);
  
-    findList(a,b) ? cout << "LIST FOUND" :
-                    cout << "LIST NOT FOUND";
- 
+    findList(a,b) ? cout << "LIST FOUND" :cout << "LIST NOT FOUND";
     return 0;
 }
