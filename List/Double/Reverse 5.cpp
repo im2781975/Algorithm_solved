b5b63c46bceead10bbe60d58ea30e68d1a42e301
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-28 18:59:07 +0100

    Update and rename Rev list<0> to Reverse 5.cpp

--- List/Double/Reverse 5.cpp
+++ List/Double/Reverse 5.cpp
@@ -1,16 +1,11 @@
-// C++ implementation to reverse a doubly linked list
-// in groups of given size without recursion
-// Iterative Method
- 
+//reverse a doubly linked list in groups of given size without recursion Iterative Method
 #include <iostream>
 using namespace std;
- 
 // Represents a node of doubly linked list
 struct Node {
     int data;
     Node *next, *prev;
 };
- 
 // function to get a new node
 Node* getNode(int data)
 {
@@ -18,31 +13,23 @@ Node* getNode(int data)
     Node* new_node = new Node();
     new_node->data = data;
     new_node->next = new_node->prev = NULL;
- 
     return new_node;
 }
- 
 // function to insert a node at the beginning
-// of the Doubly Linked List
 Node* push(Node* head, Node* new_node)
 {
-    // since we are adding at the beginning,
-    // prev is always NULL
+    // since we are adding at the beginning, prev is always NULL
     new_node->prev = NULL;
- 
     // link the old list of the new node
     new_node->next = head;
     // change prev of head node to new node
     if (head != NULL)
         head->prev = new_node;
- 
     // move the head to point to the new node
     head = new_node;
     return head;
 }
- 
-// function to reverse a doubly linked list
-// in groups of given size
+// function to reverse a doubly linked list in groups of given size
 Node* revListInGroupOfGivenSize(Node* head, int k)
 {
     if (!head)
@@ -52,7 +39,8 @@ Node* revListInGroupOfGivenSize(Node* head, int k)
     Node* globprev = NULL;
     Node* ans = NULL;
     while (st) {
-        int count = 1; // to count k nodes
+        int count = 1; 
+        // to count k nodes
         Node* curr = st;
         Node* prev = NULL;
         Node* next = NULL;
@@ -71,25 +59,20 @@ Node* revListInGroupOfGivenSize(Node* head, int k)
         }
  
         if (!globprev)
-            globprev = st; // assigning the last node of the
-                           // reversed k nodes
+            globprev = st; // assigning the last node of the reversed k nodes
         else {
             globprev->next = prev;
             prev->prev
-                = globprev; // connecting last node of last
-                            // k group to the first node of
-                            // present k group
+                = globprev;
+                // connecting last node of last k group to the first node of present k group
             globprev = st;
         }
  
-        st = curr; // advancing the pointer for the next k
-                   // group
+        st = curr;
+        // advancing the pointer for the next k group
     }
     return ans;
 }
- 
-// Function to print nodes in a
-// given doubly linked list
 void printList(Node* head)
 {
     while (head) {
@@ -97,24 +80,17 @@ void printList(Node* head)
         head = head->next;
     }
 }
- 
-// Driver code
 int main()
 {
-    // Start with the empty list
     Node* head = NULL;
- 
-    // Create doubly linked: 10<->8<->4<->2
     head = push(head, getNode(2));
     head = push(head, getNode(4));
     head = push(head, getNode(8));
     head = push(head, getNode(10));
- 
     int k = 2;
  
     cout << "Original list: ";
     printList(head);
- 
     // Reverse doubly linked list in groups of
     // size 'k'
     head = revListInGroupOfGivenSize(head, k);
