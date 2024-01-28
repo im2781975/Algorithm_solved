4064b2e2e878d5c1bf01350f588a33982f66b5fe
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-28 16:19:29 +0100

    Update and rename Intersec sorted<> to InterSec 1.cpp

--- List/Single/InterSec 1.cpp
+++ List/Single/InterSec 1.cpp
@@ -1,16 +1,10 @@
-// C++ program to implement above approach
 #include <bits/stdc++.h>
- 
-/* Link list node */
 struct Node {
     int data;
     struct Node* next;
 };
- 
 void push(struct Node** head_ref,
           int new_data);
- 
-/* This solution uses the local reference */
 struct Node* sortedIntersect(
     struct Node* a,
     struct Node* b)
@@ -18,13 +12,9 @@ struct Node* sortedIntersect(
     struct Node* result = NULL;
     struct Node** lastPtrRef = &result;
  
-    /* Advance comparing the first
-     nodes in both lists.
-    When one or the other list runs
-     out, we're done. */
     while (a != NULL && b != NULL) {
         if (a->data == b->data) {
-            /* found a node for the intersection */
+            // found a node for the intersection 
             push(lastPtrRef, a->data);
             lastPtrRef = &((*lastPtrRef)->next);
             a = a->next;
@@ -37,10 +27,6 @@ struct Node* sortedIntersect(
     }
     return (result);
 }
- 
-/* UTILITY FUNCTIONS */
-/* Function to insert a node at the
-   beginning of the linked list */
 void push(struct Node** head_ref,
           int new_data)
 {
@@ -48,17 +34,14 @@ void push(struct Node** head_ref,
     struct Node* new_node = (struct Node*)malloc(
         sizeof(struct Node));
  
-    /* put in the data  */
+    // put in the data
     new_node->data = new_data;
  
-    /* link the old list of the new node */
+    //link the old list of the new node
     new_node->next = (*head_ref);
- 
-    /* move the head to point to the new node */
+    // move the head to point to the new node 
     (*head_ref) = new_node;
 }
- 
-/* Function to print nodes in a given linked list */
 void printList(struct Node* node)
 {
     while (node != NULL) {
@@ -66,19 +49,12 @@ void printList(struct Node* node)
         node = node->next;
     }
 }
- 
-/* Driver program to test above functions*/
 int main()
 {
-    /* Start with the empty lists */
     struct Node* a = NULL;
     struct Node* b = NULL;
     struct Node* intersect = NULL;
  
-    /* Let us create the first sorted 
-     linked list to test the functions
-   Created linked list will be 
-   1->2->3->4->5->6 */
     push(&a, 6);
     push(&a, 5);
     push(&a, 4);
@@ -86,18 +62,13 @@ int main()
     push(&a, 2);
     push(&a, 1);
  
-    /* Let us create the second sorted linked list 
-   Created linked list will be 2->4->6->8 */
     push(&b, 8);
     push(&b, 6);
     push(&b, 4);
     push(&b, 2);
  
-    /* Find the intersection two linked lists */
     intersect = sortedIntersect(a, b);
- 
     printf("\n Linked list containing common items of a & b \n ");
     printList(intersect);
- 
     return 0;
 }
