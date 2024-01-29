07344965fc19e802728e0052ea2a044bff43c6ea
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-29 07:29:38 +0100

    Update and rename List/Singly/Section A/Add_val<1> to List/Single/Add Data 5.cpp

--- List/Single/Add Data 5.cpp
+++ List/Single/Add Data 5.cpp
@@ -1,82 +1,57 @@
 #include <iostream>
 using namespace std;
- 
 struct Node {
     int data;
     struct Node* next;
 };
- 
 // recursive function
-Node* addition(Node* temp1, Node* temp2, int size1,
-               int size2)
+Node* addition(Node* temp1, Node* temp2, int size1, int size2)
 {
     // creating a new Node
     Node* newNode
         = (struct Node*)malloc(sizeof(struct Node));
- 
     // base case
     if (temp1->next == NULL && temp2->next == NULL) {
-        // addition of current nodes which is the last nodes
-        // of both linked lists
+        // addition of current nodes which is the last nodes of both linked lists
         newNode->data = (temp1->data + temp2->data);
- 
         // set this current node's link null
         newNode->next = NULL;
- 
         // return the current node
         return newNode;
     }
- 
-    // creating a node that contains sum of previously added
-    // number
+    // creating a node that contains sum of previously added number
     Node* returnedNode
         = (struct Node*)malloc(sizeof(struct Node));
- 
     // if sizes are same then we move in both linked list
     if (size2 == size1) {
-        // recursively call the function
-        // move ahead in both linked list
-        returnedNode = addition(temp1->next, temp2->next,
-                                size1 - 1, size2 - 1);
- 
+        // recursively call the function move ahead in both linked list
+        returnedNode = addition(temp1->next, temp2->next,size1 - 1, size2 - 1);
         // add the current nodes and append the carry
-        newNode->data = (temp1->data + temp2->data)
-                        + ((returnedNode->data) / 10);
+        newNode->data = (temp1->data + temp2->data)+ ((returnedNode->data) / 10);
     }
     // or else we just move in big linked list
     else {
-        // recursively call the function
-        // move ahead in big linked list
-        returnedNode = addition(temp1, temp2->next, size1,
-                                size2 - 1);
- 
+        // recursively call the function move ahead in big linked list
+        returnedNode = addition(temp1, temp2->next, size1, size2 - 1);
         // add the current node and carry
         newNode->data
             = (temp2->data) + ((returnedNode->data) / 10);
     }
- 
-    // this node contains previously added numbers
-    // so we need to set only rightmost digit of it
+    // this node contains previously added numbers so we need to set only rightmost digit of it
     returnedNode->data = (returnedNode->data) % 10;
- 
     // set the returned node to the current node
     newNode->next = returnedNode;
- 
     // return the current node
     return newNode;
 }
- 
 // Function to add two numbers represented by nexted list.
-struct Node* addTwoLists(struct Node* head1,
-                         struct Node* head2)
+struct Node* addTwoLists(struct Node* head1, struct Node* head2)
 {
     struct Node *temp1, *temp2, *ans = NULL;
- 
     temp1 = head1;
     temp2 = head2;
  
     int size1 = 0, size2 = 0;
- 
     // calculating the size of first linked list
     while (temp1 != NULL) {
         temp1 = temp1->next;
@@ -87,10 +62,8 @@ struct Node* addTwoLists(struct Node* head1,
         temp2 = temp2->next;
         size2++;
     }
- 
     Node* returnedNode
         = (struct Node*)malloc(sizeof(struct Node));
- 
     // traverse the bigger linked list
     if (size2 > size1) {
         returnedNode = addition(head1, head2, size1, size2);
@@ -98,7 +71,6 @@ struct Node* addTwoLists(struct Node* head1,
     else {
         returnedNode = addition(head2, head1, size2, size1);
     }
- 
     // creating new node if head node is >10
     if (returnedNode->data >= 10) {
         ans = (struct Node*)malloc(sizeof(struct Node));
