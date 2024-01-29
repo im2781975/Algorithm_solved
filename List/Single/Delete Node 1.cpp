4800a27e9f33e1f6d1b5b4d1d76d7b863ad3ac07
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-29 12:29:45 +0100

    Update and rename List/Singly/Section A/Delete node<> to List/Single/Delete Node 1.cpp

--- List/Single/Delete Node 1.cpp
+++ List/Single/Delete Node 1.cpp
@@ -1,17 +1,12 @@
-// C++ program to delete every k-th Node of 
-// a singly linked list. 
+// delete every k-th Node of  a singly linked list. 
 #include<bits/stdc++.h> 
 using namespace std; 
-  
-/* Linked list Node */
 struct Node 
 { 
     int data; 
     struct Node* next; 
 }; 
-  
-// To remove complete list (Needed for 
-// case when k is 1) 
+// To remove complete list (Needed for case when k is 1) 
 void freeList(Node *node) 
 { 
     while (node != NULL) 
@@ -21,57 +16,41 @@ void freeList(Node *node)
         node  = next; 
     } 
 } 
-  
-// Deletes every k-th node and returns head 
-// of modified list. 
+// Deletes every k-th node and returns head of modified list 
 Node *deleteKthNode(struct Node *head, int k) 
 { 
     // If linked list is empty 
     if (head == NULL) 
         return NULL; 
-  
     if (k == 1) 
     { 
        freeList(head); 
        return NULL; 
     } 
-  
-    // Initialize ptr and prev before starting 
-    // traversal. 
+    // Initialize ptr and prev before starting  traversal
     struct Node *ptr = head, *prev = NULL; 
-  
     // Traverse list and delete every k-th node 
     int count = 0; 
     while (ptr != NULL) 
     { 
         // increment Node count 
         count++; 
-  
-        // check if count is equal to k 
-        // if yes, then delete current Node 
+        // check if count is equal to k .  if yes, then delete current Node 
         if (k == count) 
         { 
-            // put the next of current Node in 
-            // the next of previous Node 
+            // put the next of current Node in  the next of previous Node 
             delete(prev->next); 
-            prev->next = ptr->next; 
-  
-            // set count = 0 to reach further 
-            // k-th Node 
+            prev->next = ptr->next;
+            // set count = 0 to reach further  k-th Node 
             count = 0; 
         } 
-  
         // update prev if count is not 0 
         if (count != 0) 
             prev = ptr; 
-  
         ptr = prev->next; 
     } 
-  
     return head; 
-} 
-  
-/* Function to print linked list */
+}
 void displayList(struct Node *head) 
 { 
     struct Node *temp = head; 
@@ -80,21 +59,16 @@ void displayList(struct Node *head)
         cout<<temp->data<<" "; 
         temp = temp->next; 
     } 
-} 
-  
-// Utility function to create a new node. 
+}
 struct Node *newNode(int x) 
 { 
     Node *temp = new Node; 
     temp->data = x; 
     temp->next = NULL; 
     return temp; 
-} 
-  
-/* Driver program to test count function*/
+}
 int main() 
-{ 
-    /* Start with the empty list */
+{
     struct Node* head = newNode(1); 
     head->next = newNode(2); 
     head->next->next = newNode(3); 
@@ -102,13 +76,12 @@ int main()
     head->next->next->next->next = newNode(5); 
     head->next->next->next->next->next = newNode(6); 
     head->next->next->next->next->next->next = 
-                                        newNode(7); 
+     newNode(7); 
     head->next->next->next->next->next->next->next = 
                                         newNode(8); 
   
     int k = 3; 
     head = deleteKthNode(head, k); 
-  
     displayList(head); 
   
     return 0; 
