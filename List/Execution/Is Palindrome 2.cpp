63e7981694d20eb5f7b61ddebd7c13a766f294f9
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-30 11:45:48 +0100

    Update and rename List/Singly/Section B/Is palindrome<> to List/Execution/Is Palindrome 2.cpp

--- List/Execution/Is Palindrome 2.cpp
+++ List/Execution/Is Palindrome 2.cpp
@@ -1,8 +1,6 @@
-// C++ program to check if a linked list is palindrome
+// check if a linked list is palindrome
 #include <bits/stdc++.h>
 using namespace std;
- 
-// Link list node
 struct Node {
     char data;
     struct Node* next;
@@ -10,65 +8,43 @@ struct Node {
  
 void reverse(struct Node**);
 bool compareLists(struct Node*, struct Node*);
- 
-// Function to check if given linked list is
-// palindrome or not
+// Function to check if given linked list is palindrome or not
 bool isPalindrome(struct Node* head)
 {
     struct Node *slow_ptr = head, *fast_ptr = head;
     struct Node *second_half, *prev_of_slow_ptr = head;
- 
     // To handle odd size list
     struct Node* midnode = NULL;
- 
     // initialize result
     bool res = true;
- 
     if (head != NULL && head->next != NULL) {
- 
-        // Get the middle of the list. Move slow_ptr by 1
-        // and fast_ptr by 2, slow_ptr will have the middle
-        // node
+        // Get the middle of the list. Move slow_ptr by 1 and fast_ptr by 2, slow_ptr will have the middle node
         while (fast_ptr != NULL && fast_ptr->next != NULL) {
             fast_ptr = fast_ptr->next->next;
- 
-            // We need previous of the slow_ptr for
-            // linked lists with odd elements
+            // We need previous of the slow_ptr for linked lists with odd elements
             prev_of_slow_ptr = slow_ptr;
             slow_ptr = slow_ptr->next;
         }
- 
-        // fast_ptr would become NULL when there
-        // are even elements in list. And not NULL
+        // fast_ptr would become NULL when there are even elements in list. And not NULL
         // for odd elements. We need to skip the
-        // middle node for odd case and store it
-        // somewhere so that we can restore the
-        // original list
+        // middle node for odd case and store it somewhere so that we can restore the original list
         if (fast_ptr != NULL) {
             midnode = slow_ptr;
             slow_ptr = slow_ptr->next;
         }
- 
-        // Now reverse the second half and
-        // compare it with first half
+        // Now reverse the second half and compare it with first half
         second_half = slow_ptr;
- 
         // NULL terminate first half
         prev_of_slow_ptr->next = NULL;
- 
         // Reverse the second half
         reverse(&second_half);
- 
         // compare
         res = compareLists(head, second_half);
- 
         // Construct the original list back
-        reverse(
-            &second_half); // Reverse the second half again
+        reverse(&second_half);
+            // Reverse the second half again
  
-        // If there was a mid node (odd size case)
-        // which was not part of either first half
-        // or second half.
+        // If there was a mid node (odd size case) which was not part of either first half or second half.
         if (midnode != NULL) {
             prev_of_slow_ptr->next = midnode;
             midnode->next = second_half;
@@ -78,10 +54,7 @@ bool isPalindrome(struct Node* head)
     }
     return res;
 }
- 
-// Function to reverse the linked list
-// Note that this function may change
-// the head
+// Function to reverse the linked list Note that this function may change the head
 void reverse(struct Node** head_ref)
 {
     struct Node* prev = NULL;
@@ -96,9 +69,7 @@ void reverse(struct Node** head_ref)
     }
     *head_ref = prev;
 }
- 
-// Function to check if two input
-// lists have same data
+// Function to check if two input lists have same data
 bool compareLists(struct Node* head1, struct Node* head2)
 {
     struct Node* temp1 = head1;
@@ -112,36 +83,24 @@ bool compareLists(struct Node* head1, struct Node* head2)
         else
             return 0;
     }
- 
     // Both are empty return 1
     if (temp1 == NULL && temp2 == NULL)
         return 1;
- 
-    // Will reach here when one is NULL
-    // and other is not
+    // Will reach here when one is NULL and other is not
     return 0;
 }
- 
-// Push a node to linked list. Note
-// that this function changes the head
+// Push a node to linked list. Note that this function changes the head
 void push(struct Node** head_ref, char new_data)
 {
- 
     // Allocate node
     struct Node* new_node
         = (struct Node*)malloc(sizeof(struct Node));
- 
     // Put in the data
     new_node->data = new_data;
- 
-    // Link the old list of the new node
     new_node->next = (*head_ref);
- 
-    // Move the head to point to the new node
     (*head_ref) = new_node;
 }
- 
-// A utility function to print a given linked list
+// function to print a given linked list
 void printList(struct Node* ptr)
 {
     while (ptr != NULL) {
@@ -153,8 +112,6 @@ void printList(struct Node* ptr)
 }
 int main()
 {
- 
-    // Start with the empty list
     struct Node* head = NULL;
     char str[] = "abacaba";
     int i;
@@ -162,9 +119,8 @@ int main()
     for (i = 0; str[i] != '\0'; i++) {
         push(&head, str[i]);
     }
-    isPalindrome(head) ? cout << "Is Palindrome"
-                              << "\n\n"
-                       : cout << "Not Palindrome"
-                              << "\n\n";
+    isPalindrome(head) ? cout << "Is Palindrome"<< "\n\n"
+    : cout << "Not Palindrome"
+    << "\n\n";
     return 0;
 }
