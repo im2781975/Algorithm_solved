8f7e576f197b1477bfa352bb13cba4e49e638138
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-30 11:42:00 +0100

    Update and rename List/Singly/Section B/Is palindrome<1> to List/Execution/Is Palindrome 1.cpp

--- List/Execution/Is Palindrome 1.cpp
+++ List/Execution/Is Palindrome 1.cpp
@@ -1,62 +1,42 @@
-// Recursive program to check if a given linked list is
-// palindrome
+// check if a given linked list is palindrome
 #include <bits/stdc++.h>
 using namespace std;
- 
-/* Link list node */
 struct node {
     char data;
     struct node* next;
 };
- 
 // Initial parameters to this function are &head and head
-bool isPalindromeUtil(struct node** left,
-                      struct node* right)
+bool isPalindromeUtil(struct node** left,struct node* right)
 {
-    /* stop recursion when right becomes NULL */
+    // stop recursion when right becomes NULL
     if (right == NULL)
         return true;
- 
-    /* If sub-list is not palindrome then no need to
-    check for current left and right, return false */
+    // If sub-list is not palindrome then no need to check for current left and right, return false
     bool isp = isPalindromeUtil(left, right->next);
     if (isp == false)
         return false;
- 
-    /* Check values at current left and right */
+    // Check values at current left and right 
     bool isp1 = (right->data == (*left)->data);
  
-    /* Move left to next node */
+    // Move left to next node 
     *left = (*left)->next;
- 
     return isp1;
 }
- 
 // A wrapper over isPalindromeUtil()
 bool isPalindrome(struct node* head)
 {
     return isPalindromeUtil(&head, head);
 }
- 
-/* Push a node to linked list. Note that this function
-changes the head */
+//Push a node to linked list. Note that this function changes the head 
 void push(struct node** head_ref, char new_data)
 {
-    /* allocate node */
+    // allocate node 
     struct node* new_node
         = (struct node*)malloc(sizeof(struct node));
- 
-    /* put in the data */
     new_node->data = new_data;
- 
-    /* link the old list of the new node */
     new_node->next = (*head_ref);
- 
-    /* move the head to point to the new node */
     (*head_ref) = new_node;
 }
- 
-// A utility function to print a given linked list
 void printList(struct node* ptr)
 {
     while (ptr != NULL) {
@@ -65,19 +45,14 @@ void printList(struct node* ptr)
     }
     cout << "NULL\n";
 }
- 
-/* Driver program to test above function*/
 int main()
 {
-    /* Start with the empty list */
     struct node* head = NULL;
     char str[] = "abacaba";
     int i;
- 
     for (i = 0; str[i] != '\0'; i++) {
         push(&head, str[i]);
     }
-    isPalindrome(head) ? cout << "Is Palindrome\n\n"
-                       : cout << "Not Palindrome\n\n";
+    isPalindrome(head) ? cout << "Is Palindrome\n\n": cout << "Not Palindrome\n\n";
     return 0;
 }
