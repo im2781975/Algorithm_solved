4ffab6549011954b0c7209ee516d09a30a7529b4
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-30 11:55:29 +0100

    Update and rename List/Singly/Section B/Remove duplicate to List/Execution/Remove Dup 9.cpp

--- List/Execution/Remove Dup 9.cpp
+++ List/Execution/Remove Dup 9.cpp
@@ -1,15 +1,10 @@
-/* C++ Program to remove duplicates in an unsorted
-   linked list */
+//remove duplicates
 #include <bits/stdc++.h>
 using namespace std;
- 
-/* A linked list node */
 struct Node {
     int data;
     struct Node* next;
 };
- 
-// Utility function to create a new Node
 struct Node* newNode(int data)
 {
     Node* temp = new Node;
@@ -17,36 +12,28 @@ struct Node* newNode(int data)
     temp->next = NULL;
     return temp;
 }
- 
-/* Function to remove duplicates from a
-   unsorted linked list */
 void removeDuplicates(struct Node* start)
 {
     struct Node *ptr1, *ptr2, *dup;
     ptr1 = start;
- 
-    /* Pick elements one by one */
+    // Pick elements one by one
     while (ptr1 != NULL && ptr1->next != NULL) {
         ptr2 = ptr1;
- 
-        /* Compare the picked element with rest
-           of the elements */
+        //Compare the picked element with rest of the elements 
         while (ptr2->next != NULL) {
-            /* If duplicate then delete it */
+            //If duplicate then delete it 
             if (ptr1->data == ptr2->next->data) {
-                /* sequence of steps is important here */
+                // sequence of steps is important here 
                 dup = ptr2->next;
                 ptr2->next = ptr2->next->next;
                 delete (dup);
             }
-            else /* This is tricky */
+            else // This is tricky 
                 ptr2 = ptr2->next;
         }
         ptr1 = ptr1->next;
     }
 }
- 
-/* Function to print nodes in a given linked list */
 void printList(struct Node* node)
 {
     while (node != NULL) {
@@ -54,12 +41,8 @@ void printList(struct Node* node)
         node = node->next;
     }
 }
- 
-// Driver code
 int main()
 {
-    /* The constructed linked list is:
-     10->12->11->11->12->11->10*/
     struct Node* start = newNode(10);
     start->next = newNode(12);
     start->next->next = newNode(11);
