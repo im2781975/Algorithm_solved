4243cfb59b3e28644aa249d83e03f71095fd68dc
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-30 11:03:14 +0100

    Update and rename List/Singly/Section B/Intersection<0> to List/Execution/Intersec Point.cpp

--- List/Execution/Intersec Point.cpp
+++ List/Execution/Intersec Point.cpp
@@ -1,33 +1,41 @@
-// C++ program to get intersection point of two linked list
+//get intersection point of two linked list
 #include <bits/stdc++.h>
 using namespace std;
- 
-/* Link list node */
 class Node {
 public:
     int data;
     Node* next;
 };
- 
-/* Function to get the counts of node in a linked list */
+//function to get the intersection point
+Node* getIntesectionNode(Node* head1, Node* head2)
+{
+    while (head2) {
+        Node* temp = head1;
+        while (temp) {
+            // if both Nodes are same
+            if (temp == head2)
+                return head2;
+            temp = temp->next;
+        }
+        head2 = head2->next;
+    }
+    // intersection is not present between the lists
+    return NULL;
+}
+//Function to get the counts of node in a linked list 
 int getCount(Node* head);
  
-/* function to get the intersection point of two linked 
-lists head1 and head2 where head1 has d more nodes than 
-head2 */
+//function to get the intersection point 
 int _getIntesectionNode(int d, Node* head1, Node* head2);
  
-/* function to get the intersection point of two linked 
-lists head1 and head2 */
+// function to get the intersection point of two linked lists head1 and head2 
 int getIntesectionNode(Node* head1, Node* head2)
 {
  
-    // Count the number of nodes in
-    // both the linked list
+    // Count the number of nodes in both the linked list
     int c1 = getCount(head1);
     int c2 = getCount(head2);
     int d;
- 
     // If first is greater
     if (c1 > c2) {
         d = c1 - c2;
@@ -38,16 +46,11 @@ int getIntesectionNode(Node* head1, Node* head2)
         return _getIntesectionNode(d, head2, head1);
     }
 }
- 
-/* function to get the intersection point of two linked 
-lists head1 and head2 where head1 has d more nodes than 
-head2 */
 int _getIntesectionNode(int d, Node* head1, Node* head2)
 {
     // Stand at the starting of the bigger list
     Node* current1 = head1;
     Node* current2 = head2;
- 
     // Move the pointer forward
     for (int i = 0; i < d; i++) {
         if (current1 == NULL) {
@@ -55,57 +58,35 @@ int _getIntesectionNode(int d, Node* head1, Node* head2)
         }
         current1 = current1->next;
     }
- 
-    // Move both pointers of both list till they
-    // intersect with each other
+    // Move both pointers of both list till they intersect with each other
     while (current1 != NULL && current2 != NULL) {
         if (current1 == current2)
             return current1->data;
- 
         // Move both the pointers forward
         current1 = current1->next;
         current2 = current2->next;
     }
- 
     return -1;
 }
- 
-/* Takes head pointer of the linked list and 
-returns the count of nodes in the list */
+// Takes head pointer of the linked list and returns the count of nodes in the list 
 int getCount(Node* head)
 {
     Node* current = head;
- 
     // Counter to store count of nodes
     int count = 0;
- 
     // Iterate till NULL
     while (current != NULL) {
- 
         // Increase the counter
         count++;
- 
         // Move the Node ahead
         current = current->next;
     }
- 
     return count;
 }
- 
-// Driver Code
 int main()
 {
-    /* 
-        Create two linked lists 
-     
-        1st 3->6->9->15->30 
-        2nd 10->15->30 
-     
-        15 is the intersection point 
-    */
  
     Node* newNode;
- 
     // Addition of new nodes
     Node* head1 = new Node();
     head1->data = 10;
@@ -132,5 +113,14 @@ int main()
  
     head1->next->next->next = NULL;
  
+    Node* intersectionPoint
+        = getIntesectionNode(head1, head2);
+        
     cout << "The node of intersection is " << getIntesectionNode(head1, head2);
+    
+    if (!intersectionPoint)
+        cout << " No Intersection Point \n";
+    else
+        cout << "Intersection Point: "
+             << intersectionPoint->data << endl;
 }
