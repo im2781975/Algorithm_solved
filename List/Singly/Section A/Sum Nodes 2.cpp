4e91a23520e65d5e470380549ae1893b63c88e77
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-30 05:51:39 +0100

    Update and rename Sum n nodes<1> to Sum Nodes 2.cpp

--- List/Singly/Section A/Sum Nodes 2.cpp
+++ List/Singly/Section A/Sum Nodes 2.cpp
@@ -1,76 +1,55 @@
-// C++ implementation to find the sum of last
-// 'n' nodes of the Linked List
+//find the sum of last 'n' nodes of the Linked List
 #include <bits/stdc++.h>
 using namespace std;
- 
-/* A Linked list node */
 struct Node {
     int data;
     struct Node* next;
 };
- 
-// function to insert a node at the
-// beginning of the linked list
+// insert a node at the beginning of the linked list
 void push(struct Node** head_ref, int new_data)
 {
-    /* allocate node */
+    // allocate node
     struct Node* new_node = new Node;
- 
-    /* put in the data  */
+    // put in the data
     new_node->data = new_data;
- 
-    /* link the old list to the new node */
+    //link the old list to the new node 
     new_node->next = (*head_ref);
- 
-    /* move the head to point to the new node */
+    //move the head to point to the new node
     (*head_ref) = new_node;
 }
- 
-// utility function to find the sum of last 'n' nodes
+// find the sum of last 'n' nodes
 int sumOfLastN_NodesUtil(struct Node* head, int n)
 {
     // if n == 0
     if (n <= 0)
         return 0;
- 
     int sum = 0, len = 0;
     struct Node* temp = head;
- 
     // calculate the length of the linked list
     while (temp != NULL) {
         len++;
         temp = temp->next;
     }
- 
     // count of first (len - n) nodes
     int c = len - n;
     temp = head;
- 
     // just traverse the 1st 'c' nodes
-    while (temp != NULL && c--)                    
+    while (temp != NULL && c--)
         // move to next node
         temp = temp->next;
- 
     // now traverse the last 'n' nodes and add them
     while (temp != NULL) {
- 
         // accumulate node's data to sum
         sum += temp->data;
- 
         // move to next node
         temp = temp->next;
     }
- 
     // required sum
     return sum;
 }
- 
-// Driver program to test above
 int main()
 {
     struct Node* head = NULL;
- 
-    // create linked list 10->6->8->4->12
     push(&head, 12);
     push(&head, 4);
     push(&head, 8);
