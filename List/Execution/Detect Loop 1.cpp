4d290d864d20281050b916bbbcbf0bed307c23f9
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-30 10:24:11 +0100

    Update and rename List/Singly/Section B/Detect loop<0> to List/Execution/Detect Loop 1.cpp

--- List/Execution/Detect Loop 1.cpp
+++ List/Execution/Detect Loop 1.cpp
@@ -1,29 +1,18 @@
-// C++ program to detect loop in a linked list
+// detect loop in a linked list
 #include <bits/stdc++.h>
 using namespace std;
- 
-/* Link list node */
 class Node {
 public:
     int data;
     Node* next;
 };
- 
 void push(Node** head_ref, int new_data)
 {
-    /* allocate node */
     Node* new_node = new Node();
- 
-    /* put in the data */
     new_node->data = new_data;
- 
-    /* link the old list of the new node */
     new_node->next = (*head_ref);
- 
-    /* move the head to point to the new node */
     (*head_ref) = new_node;
 }
- 
 int detectLoop(Node* list)
 {
     Node *slow_p = list, *fast_p = list;
@@ -37,19 +26,15 @@ int detectLoop(Node* list)
     }
     return 0;
 }
- 
-/* Driver code*/
 int main()
 {
-    /* Start with the empty list */
     Node* head = NULL;
  
     push(&head, 20);
     push(&head, 4);
     push(&head, 15);
     push(&head, 10);
- 
-    /* Create a loop for testing */
+    // Create a loop for testing 
     head->next->next->next->next = head;
     if (detectLoop(head))
         cout << "Loop Found";
