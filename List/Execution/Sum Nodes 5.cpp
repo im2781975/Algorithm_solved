b999a617df989b7fb85543cf391c759881e964d2
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-30 05:58:08 +0100

    Update and rename List/Singly/Section A/Sum n nodes<> to List/Execution/Sum Nodes 5.cpp

--- List/Execution/Sum Nodes 5.cpp
+++ List/Execution/Sum Nodes 5.cpp
@@ -1,28 +1,18 @@
-// C++ implementation to find the sum of last
-// 'n' nodes of the Linked List
+// find the sum of last 'n' nodes of the Linked List
 #include <bits/stdc++.h>
 using namespace std;
- 
-/* A Linked list node */
 struct Node {
     int data;
     struct Node* next;
 };
  
-// function to insert a node at the
-// beginning of the linked list
+//insert a node at the beginning
 void push(struct Node** head_ref, int new_data)
 {
-    /* allocate node */
+    // allocate node
     struct Node* new_node = new Node;
- 
-    /* put in the data  */
     new_node->data = new_data;
- 
-    /* link the old list to the new node */
     new_node->next = (*head_ref);
- 
-    /* move the head to point to the new node */
     (*head_ref) = new_node;
 }
  
@@ -35,34 +25,25 @@ int sumOfLastN_NodesUtil(struct Node* head, int n)
  
     stack<int> st;
     int sum = 0;
- 
     // traverses the list from left to right
     while (head != NULL) {
- 
         // push the node's data onto the stack 'st'
         st.push(head->data);
- 
         // move to next node
         head = head->next;
     }
- 
     // pop 'n' nodes from 'st' and
     // add them
     while (n--) {
         sum += st.top();
         st.pop();
     }
- 
     // required sum
     return sum;
 }
- 
-// Driver program to test above
 int main()
 {
     struct Node* head = NULL;
- 
-    // create linked list 10->6->8->4->12
     push(&head, 12);
     push(&head, 4);
     push(&head, 8);
