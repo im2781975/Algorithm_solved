16f633468b44ba8b553b7b13181d64f9843ebe9e
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-30 16:30:47 +0100

    Update and rename List/Singly/Section B/Swap node<> to List/Execution/Swap Node 1.cpp

--- List/Execution/Swap Node 1.cpp
+++ List/Execution/Swap Node 1.cpp
@@ -1,18 +1,12 @@
-// C++ program to swap two given nodes of a linked list
+// swap two given nodes of a linked list
 #include <iostream>
 using namespace std;
- 
-// A linked list node class
 class Node {
 public:
     int data;
     class Node* next;
-    // constructor
     Node(int val, Node* next)
-        : data(val)
-        , next(next)
-    {
-    }
+        : data(val), next(next){}
     // print list from this to last till null
     void printList()
     {
@@ -24,29 +18,24 @@ public:
         cout << endl;
     }
 };
- 
-// Function to add a node at the beginning of List
 void push(Node** head_ref, int new_data)
 {
     // allocate node
     (*head_ref) = new Node(new_data, *head_ref);
 }
- 
 void swap(Node*& a, Node*& b)
 {
     Node* temp = a;
     a = b;
     b = temp;
 }
- 
 void swapNodes(Node** head_ref, int x, int y)
 {
     // Nothing to do if x and y are same
     if (x == y)
         return;
     Node **a = NULL, **b = NULL;
-    // search for x and y in the linked list
-    // and store their pointer in a and b
+    // search for x and y in the linked list and store their pointer in a and b
     while (*head_ref) {
         if ((*head_ref)->data == x)
             a = head_ref;
@@ -54,21 +43,15 @@ void swapNodes(Node** head_ref, int x, int y)
             b = head_ref;
         head_ref = &((*head_ref)->next);
     }
-    // if we have found both a and b
-    // in the linked list swap current
-    // pointer and next pointer of these
+    // if we have found both a and b in the linked list swap current pointer and next pointer of these
     if (a && b) {
         swap(*a, *b);
         swap(((*a)->next), ((*b)->next));
     }
 }
- 
-// Driver code
 int main()
 {
     Node* start = NULL;
-    // The constructed linked list is:
-    // 1->2->3->4->5->6->7
     push(&start, 7);
     push(&start, 6);
     push(&start, 5);
