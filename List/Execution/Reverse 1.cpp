a3a73d5024d19bb9042dfec243bc697253ccd6a4
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-30 16:04:12 +0100

    Update and rename List/Singly/Section B/Reverse<0> to List/Execution/Reverse 1.cpp

--- List/Execution/Reverse 1.cpp
+++ List/Execution/Reverse 1.cpp
@@ -1,15 +1,12 @@
-// C++ program to reverse a linked list using two pointers. 
+// reverse a linked list using two pointers. 
 #include <bits/stdc++.h> 
 using namespace std; 
-typedef uintptr_t ut; 
-  
-/* Link list node */
+typedef uintptr_t ut;
 struct Node { 
     int data; 
     struct Node* next; 
 }; 
-  
-/* Function to reverse the linked list using 2 pointers */
+//Function to reverse the linked list using 2 pointers 
 void reverse(struct Node** head_ref) 
 { 
     struct Node* current = *head_ref; 
@@ -20,18 +17,14 @@ void reverse(struct Node** head_ref)
         next->next = (*head_ref); 
         *head_ref = next; 
     } 
-} 
-  
-/* Function to push a node */
+}
 void push(struct Node** head_ref, int new_data) 
 { 
     struct Node* new_node = new Node; 
     new_node->data = new_data; 
     new_node->next = (*head_ref); 
     (*head_ref) = new_node; 
-} 
-  
-/* Function to print linked list */
+}
 void printList(struct Node* head) 
 { 
     struct Node* temp = head; 
@@ -39,12 +32,9 @@ void printList(struct Node* head)
         printf("%d  ", temp->data); 
         temp = temp->next; 
     } 
-} 
-  
-/* Driver program to test above function*/
+}
 int main() 
-{ 
-    /* Start with the empty list */
+{
     struct Node* head = NULL; 
   
     push(&head, 20); 
