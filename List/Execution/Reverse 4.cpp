21dafd50f2368b1ceb1f3f1373f4796a0a098421
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-31 11:31:13 +0100

    Update and rename List/Singly/Section C/Reverse<1> to List/Execution/Reverse 4.cpp

--- List/Execution/Reverse 4.cpp
+++ List/Execution/Reverse 4.cpp
@@ -1,29 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
-/* Link list node */
 class node 
 { 
     public:
     int data; 
     node* next; 
 }; 
- 
-/* Helper function for kAltReverse() */
+// Helper function for kAltReverse()
 node * _kAltReverse(node *node, int k, bool b); 
- 
-/* Alternatively reverses the given linked list 
-in groups of given size k. */
+// Alternatively reverses the given linked list in groups of given size k. 
 node *kAltReverse(node *head, int k) 
 { 
     return _kAltReverse(head, k, true); 
 } 
- 
-/* Helper function for kAltReverse().  
-It reverses k nodes of the list only if 
-the third parameter b is passed as true, 
-otherwise moves the pointer k nodes ahead
-and recursively calls itself */
+// Helper function for kAltReverse(). It reverses k nodes of the list only if 
+ //the third parameter b is passed as true, otherwise moves the pointer k nodes ahead and recursively calls itself 
 node * _kAltReverse(node *Node, int k, bool b) 
 { 
     if(Node == NULL) 
@@ -33,17 +24,11 @@ node * _kAltReverse(node *Node, int k, bool b)
     node *prev = NULL; 
     node *current = Node; 
     node *next; 
-     
-    /* The loop serves two purposes 
-        1) If b is true, 
-           then it reverses the k nodes 
-        2) If b is false, 
-           then it moves the current pointer */
+    // If b is true, then it reverses the k nodes. If b is false, then it moves the current pointer
     while(current != NULL && count <= k) 
     { 
         next = current->next; 
-     
-        /* Reverse the nodes only if b is true*/
+        //Reverse the nodes only if b is true
         if(b == true) 
             current->next = prev; 
                  
@@ -51,16 +36,13 @@ node * _kAltReverse(node *Node, int k, bool b)
         current = next; 
         count++; 
     } 
-         
-    /* 3) If b is true, then node is the kth node. 
-        So attach rest of the list after node. 
-        4) After attaching, return the new head */
+    // If b is true, then node is the kth node.  So attach rest of the list after node. 
+    // After attaching, return the new head 
     if(b == true) 
     { 
         Node->next = _kAltReverse(current, k, !b); 
         return prev;         
     } 
-         
     /* If b is not true, then attach 
     rest of the list after prev. 
     So attach rest of the list after prev */
@@ -69,27 +51,17 @@ node * _kAltReverse(node *Node, int k, bool b)
         prev->next = _kAltReverse(current, k, !b); 
         return Node;     
     } 
-} 
- 
- 
-/* UTILITY FUNCTIONS */
-/* Function to push a node */
+}
 void push(node** head_ref, int new_data) 
-{ 
-    /* allocate node */
+{
     node* new_node = new node();
- 
-    /* put in the data */
+    // put in the data 
     new_node->data = new_data; 
- 
-    /* link the old list of the new node */
+    // link the old list of the new node
     new_node->next = (*head_ref); 
- 
-    /* move the head to point to the new node */
+    // move the head to point to the new node 
     (*head_ref) = new_node; 
-} 
- 
-/* Function to print linked list */
+}
 void printList(node *node) 
 { 
     int count = 0; 
@@ -99,16 +71,11 @@ void printList(node *node)
         node = node->next; 
         count++; 
     } 
-} 
- 
-// Driver Code
+}
 int main(void) 
-{ 
-    /* Start with the empty list */
+{
     node* head = NULL; 
     int i; 
- 
-    // create a list 1->2->3->4->5...... ->20 
     for(i = 20; i > 0; i--) 
     push(&head, i); 
  
