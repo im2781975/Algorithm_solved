2c8a7168e2da9168210de9161ef83b9ce0c538c7
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-31 10:12:01 +0100

    Update and rename List/Singly/Section C/Merge list to List/Execution/Merge List.cpp

--- List/Execution/Merge List.cpp
+++ List/Execution/Merge List.cpp
@@ -1,79 +1,55 @@
-// C++ Program to merge two sorted linked lists without
-// using any extra space and without changing links
-// of first list
+// merge two sorted linked lists
 #include <bits/stdc++.h>
 using namespace std;
- 
- /* Structure for a linked list node */
 struct Node
 {
     int data;
     struct Node *next;
 };
- 
-/* Given a reference (pointer to pointer) to the head
-  of a list and an int, push a new node on the front
-  of the list. */
 void push(struct Node** head_ref, int new_data)
 {
-    /* allocate node */
+    // allocate node
     struct Node* new_node =
         (struct Node*) malloc(sizeof(struct Node));
- 
-    /* put in the data  */
+    // put in the data  
     new_node->data  = new_data;
- 
-    /* link the old list off the new node */
+    // link the old list off the new node 
     new_node->next = (*head_ref);
- 
-    /* move the head to point to the new node */
+    // move the head to point to the new node 
     (*head_ref)    = new_node;
 }
- 
-// Function to merge two sorted linked lists
-// LL1 and LL2 without using any extra space.
+// Function to merge two sorted linked lists LL1 and LL2 without using any extra space.
 void mergeLists(struct Node *a, struct Node * &b)
 {
     // run till either one of a or b runs out
     while (a && b)
     {
-        // for each element of LL1,
-        // compare it with first element of LL2.
+        // for each element of LL1, compare it with first element of LL2.
         if (a->data > b->data)
         {
-            // swap the two elements involved
-            // if LL1 has a greater element
+            // swap the two elements involve  if LL1 has a greater element
             swap(a->data, b->data);
- 
             struct Node *temp = b;
- 
-            // To keep LL2 sorted, place first
-            // element of LL2 at its correct place
+            // To keep LL2 sorted, place first element of LL2 at its correct place
             if (b->next && b->data > b->next->data)
             {
                 b = b->next;
                 struct Node *ptr= b, *prev = NULL;
- 
-                // find mismatch by traversing the
-                // second linked list once
+                // find mismatch by traversing the second linked list once
                 while (ptr && ptr->data < temp->data)
                 {
                     prev = ptr;
                     ptr = ptr -> next;
                 }
- 
                 // correct the pointers
                 prev->next = temp;
                 temp->next = ptr;
             }
         }
- 
         // move LL1 pointer to next element
         a = a->next;
     }
 }
- 
-// Code to print the linked link
 void printList(struct Node *head)
 {
     while (head)
@@ -83,8 +59,6 @@ void printList(struct Node *head)
     }
     cout << "NULL" << endl;
 }
- 
-// Driver code
 int main()
 {
     struct Node *a = NULL;
