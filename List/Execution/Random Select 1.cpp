7d23467d6bd6bbaf0a8cb19520dd32f37717b4c4
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-31 12:47:43 +0100

    Update and rename List/Singly/Section C/Select random to List/Execution/Random Select 1.cpp

--- List/Execution/Random Select 1.cpp
+++ List/Execution/Random Select 1.cpp
@@ -1,12 +1,9 @@
-/* C++ program to randomly select a node from a singly
-linked list */
+// randomly select a node
 #include<stdio.h>
 #include<stdlib.h>
 #include <time.h>
 #include<iostream>
 using namespace std;
- 
-/* Link list node */
 class Node
 {
     public:
@@ -16,22 +13,16 @@ class Node
     void push(Node**, int);
      
 };
- 
-// A reservoir sampling based function to print a
-// random node from a linked list
+// A reservoir sampling based function to print a random node from a linked list
 void Node::printRandom(Node *head)
 {
     // IF list is empty
     if (head == NULL)
     return;
- 
-    // Use a different seed value so that we don't get
-    // same result each time we run this program
+    // Use a different seed value so that we don't get same result each time we run this program
     srand(time(NULL));
- 
     // Initialize result as first node
     int result = head->key;
- 
     // Iterate from the (k+1)th element to nth element
     Node *current = head;
     int n;
@@ -40,47 +31,32 @@ void Node::printRandom(Node *head)
         // change result with probability 1/n
         if (rand() % n == 0)
         result = current->key;
- 
         // Move to next node
         current = current->next;
     }
- 
     cout<<"Randomly selected key is \n"<< result;
 }
- 
-/* BELOW FUNCTIONS ARE JUST UTILITY TO TEST */
- 
-/* A utility function to create a new node */
 Node* newNode(int new_key)
 {
     // allocate node 
     Node* new_node = (Node*) malloc(sizeof( Node));
- 
-    /// put in the key 
+    // put in the key 
     new_node->key = new_key;
     new_node->next = NULL;
  
     return new_node;
 }
- 
-/* A utility function to insert a node at the beginning
-of linked list */
 void Node:: push(Node** head_ref, int new_key)
 {
-    /* allocate node */
+    // allocate node 
     Node* new_node = new Node;
- 
-    /* put in the key */
+    // put in the key 
     new_node->key = new_key;
- 
-    /* link the old list of the new node */
+    // link the old list of the new node 
     new_node->next = (*head_ref);
- 
-    /* move the head to point to the new node */
+    //move the head to point to the new node 
     (*head_ref) = new_node;
 }
- 
-// Driver program to test above functions
 int main()
 {
     Node n1;
