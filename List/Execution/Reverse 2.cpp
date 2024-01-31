f5463e59b88721c43a51476bf41e0ed84ea9f1be
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-31 10:19:20 +0100

    Update and rename List/Singly/Section C/Reverse to List/Execution/Reverse 2.cpp

--- List/Execution/Reverse 2.cpp
+++ List/Execution/Reverse 2.cpp
@@ -1,18 +1,14 @@
-/ CPP program to reverse a linked list 
-// in groups of given size 
+//reverse a linked list 
 #include <bits/stdc++.h> 
-using namespace std; 
-  
-/* Link list node */
+using namespace std;
 class Node { 
 public: 
     int data; 
     Node* next; 
 }; 
-  
-/* Reverses the linked list in groups 
+// Reverses the linked list in groups 
 of size k and returns the pointer 
-to the new head node. */
+to the new head node.
 Node* reverse(Node* head, int k) 
 { 
     // base case 
@@ -23,7 +19,7 @@ Node* reverse(Node* head, int k)
     Node* prev = NULL; 
     int count = 0; 
   
-    /*reverse first k nodes of the linked list */
+    //reverse first k nodes of the linked list 
     while (current != NULL && count < k) { 
         next = current->next; 
         current->next = prev; 
@@ -31,51 +27,34 @@ Node* reverse(Node* head, int k)
         current = next; 
         count++; 
     } 
-  
-    /* next is now a pointer to (k+1)th node 
-    Recursively call for the list starting from current. 
-    And make rest of the list as next of first node */
+    //next is now a pointer to (k+1)th node  Recursively call for the list starting from current. 
+   // And make rest of the list as next of first node 
     if (next != NULL) 
         head->next = reverse(next, k); 
-  
-    /* prev is new head of the input list */
+    // prev is new head of the input list
     return prev; 
-} 
-  
-/* UTILITY FUNCTIONS */
-/* Function to push a node */
+}
 void push(Node** head_ref, int new_data) 
 { 
-    /* allocate node */
+    //allocate node
     Node* new_node = new Node(); 
-  
-    /* put in the data */
+    //put in the data 
     new_node->data = new_data; 
-  
-    /* link the old list of the new node */
+    // link the old list of the new node 
     new_node->next = (*head_ref); 
-  
-    /* move the head to point to the new node */
+    // move the head to point to the new node 
     (*head_ref) = new_node; 
-} 
-  
-/* Function to print linked list */
+}
 void printList(Node* node) 
 { 
     while (node != NULL) { 
         cout << node->data << " "; 
         node = node->next; 
     } 
-} 
-  
-/* Driver code*/
+}
 int main() 
-{ 
-    /* Start with the empty list */
-    Node* head = NULL; 
-  
-    /* Created Linked list  
-       is 1->2->3->4->5->6->7->8->9 */
+{
+    Node* head = NULL;
     push(&head, 9); 
     push(&head, 8); 
     push(&head, 7); 
