24d08cfb7059d23aa1bdf0d42bd23cc1f42eced2
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-31 15:53:55 +0100

    Update and rename List/Circular<> to List/Single/Cir insert.cpp

--- List/Single/Cir insert.cpp
+++ List/Single/Cir insert.cpp
@@ -1,27 +1,18 @@
-// C++ program to implement 
-// the above approach 
 #include <bits/stdc++.h>
 using namespace std;
- 
-/* structure for a node */
 class Node 
 { 
     public:
     int data; 
     Node *next; 
 }; 
- 
-/* Function to insert a node at the beginning 
-of a Circular linked list */
+// insert a node at the beginning of a Circular linked list
 void push(Node **head_ref, int data) 
 { 
     Node *ptr1 = new Node();
     Node *temp = *head_ref; 
     ptr1->data = data; 
     ptr1->next = *head_ref; 
- 
-    /* If linked list is not NULL then 
-    set the next of last node */
     if (*head_ref != NULL) 
     { 
         while (temp->next != *head_ref) 
@@ -29,13 +20,9 @@ void push(Node **head_ref, int data)
         temp->next = ptr1; 
     } 
     else
-        ptr1->next = ptr1; /*For the first node */
- 
+        ptr1->next = ptr1; 
     *head_ref = ptr1; 
 } 
- 
-/* Function to print nodes in 
-a given Circular linked list */
 void printList(Node *head) 
 { 
     Node *temp = head; 
@@ -48,15 +35,10 @@ void printList(Node *head)
         } 
         while (temp != head); 
     } 
-} 
- 
-/* Driver program to test above functions */
+}
 int main() 
 { 
-    /* Initialize lists as empty */
-    Node *head = NULL; 
- 
-    /* Created linked list will be 11->2->56->12 */
+    Node *head = NULL;
     push(&head, 12); 
     push(&head, 56); 
     push(&head, 2); 
@@ -64,6 +46,5 @@ int main()
  
     cout << "Contents of Circular Linked List\n "; 
     printList(head); 
- 
     return 0; 
 } 
