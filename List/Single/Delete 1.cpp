998e17d099a7ab7d8b7812ab29d1e70a09f1032b
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-31 14:49:52 +0100

    Update and rename Delete<0> to Delete 1.cpp

--- List/Single/Delete 1.cpp
+++ List/Single/Delete 1.cpp
@@ -1,18 +1,11 @@
-// A complete working C++ program to delete
-// a node in a linked list at a given position
+// delete a node in a linked list at a given position
 #include <iostream>
 using namespace std;
- 
-// A linked list node
 class Node {
 public:
     int data;
     Node* next;
 };
- 
-// Given a reference (pointer to pointer) to
-// the head of a list and an int inserts a
-// new node on the front of the list.
 void push(Node** head_ref, int new_data)
 {
     Node* new_node = new Node();
@@ -20,52 +13,36 @@ void push(Node** head_ref, int new_data)
     new_node->next = (*head_ref);
     (*head_ref) = new_node;
 }
- 
-// Given a reference (pointer to pointer) to
-// the head of a list and a position, deletes
-// the node at the given position
+// Given a reference (pointer to pointer) to the head of a list and a position, deletes the node at the given position
 void deleteNode(Node** head_ref, int position)
 {
- 
     // If linked list is empty
     if (*head_ref == NULL)
         return;
- 
     // Store head node
     Node* temp = *head_ref;
- 
     // If head needs to be removed
     if (position == 0) {
- 
         // Change head
         *head_ref = temp->next;
- 
         // Free old head
         free(temp);
         return;
     }
- 
     // Find previous node of the node to be deleted
     for (int i = 0; temp != NULL && i < position - 1; i++)
         temp = temp->next;
- 
     // If position is more than number of nodes
     if (temp == NULL || temp->next == NULL)
         return;
  
-    // Node temp->next is the node to be deleted
-    // Store pointer to the next of node to be deleted
+    // Node temp->next is the node to be deleted Store pointer to the next of node to be deleted
     Node* next = temp->next->next;
- 
     // Unlink the node from linked list
-    free(temp->next); // Free memory
- 
+    free(temp->next);
     // Unlink the deleted node from list
     temp->next = next;
 }
- 
-// This function prints contents of linked
-// list starting from the given node
 void printList(Node* node)
 {
     while (node != NULL) {
@@ -73,12 +50,8 @@ void printList(Node* node)
         node = node->next;
     }
 }
- 
-// Driver code
 int main()
 {
- 
-    // Start with the empty list
     Node* head = NULL;
  
     push(&head, 7);
