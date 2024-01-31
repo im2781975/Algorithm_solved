a44d7425c9653c0674b26ac943481650f6bd3359
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-31 15:24:59 +0100

    Update and rename Reverse<0> to Reverse 7.cpp

--- List/Execution/Reverse 7.cpp
+++ List/Execution/Reverse 7.cpp
@@ -1,44 +1,33 @@
-
-// A simple and tail recursive C++ program to reverse
-// a linked list
+//reverse a linked list
 #include <bits/stdc++.h>
 using namespace std;
- 
 struct Node {
     int data;
     struct Node* next;
 };
- 
 void reverseUtil(Node* curr, Node* prev, Node** head);
- 
-// This function mainly calls reverseUtil()
-// with prev as NULL
 void reverse(Node** head)
 {
     if (!head)
         return;
     reverseUtil(*head, NULL, head);
 }
- 
-// A simple and tail-recursive function to reverse
-// a linked list.  prev is passed as NULL initially.
+// A simple and tail-recursive function to reverse a linked list.  prev is passed as NULL initially.
 void reverseUtil(Node* curr, Node* prev, Node** head)
 {
-    /* If last node mark it head*/
+    // If last node mark it head
     if (!curr->next) {
         *head = curr;
-        /* Update next to prev node */
+        // Update next to prev node
         curr->next = prev;
         return;
     }
-    /* Save curr->next node for recursive call */
+    //Save curr->next node for recursive call 
     Node* next = curr->next;
-    /* and update next ..*/
+    // and update next ..
     curr->next = prev;
     reverseUtil(next, curr, head);
 }
- 
-// A utility function to create a new node
 Node* newNode(int key)
 {
     Node* temp = new Node;
@@ -46,8 +35,6 @@ Node* newNode(int key)
     temp->next = NULL;
     return temp;
 }
- 
-// A utility function to print a linked list
 void printlist(Node* head)
 {
     while (head != NULL) {
@@ -56,8 +43,6 @@ void printlist(Node* head)
     }
     cout << endl;
 }
- 
-// Driver code
 int main()
 {
     Node* head1 = newNode(1);
