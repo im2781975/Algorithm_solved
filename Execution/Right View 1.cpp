7329ff8cb166a3c933f9e4df70ab1a6a6b7564d8
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-02-01 11:56:06 +0100

    Update and rename Queue/Section A/Right view<0> to Execution/Right View 1.cpp

--- Execution/Right View 1.cpp
+++ Execution/Right View 1.cpp
@@ -1,16 +1,10 @@
-// C++ program to print left view of
-// Binary Tree
- 
+// print left view of Binary Tree
 #include <bits/stdc++.h>
 using namespace std;
- 
-// A Binary Tree Node
 struct Node {
     int data;
     struct Node *left, *right;
 };
- 
-// Utility function to create a new tree node
 Node* newNode(int data)
 {
     Node* temp = new Node;
@@ -18,9 +12,6 @@ Node* newNode(int data)
     temp->left = temp->right = NULL;
     return temp;
 }
- 
-// function to print Right view of
-// binary tree
 void printRightView(Node* root)
 {
     if (root == NULL)
@@ -32,35 +23,26 @@ void printRightView(Node* root)
     while (!q.empty()) {
         // get number of nodes for each level
         int n = q.size();
- 
         // traverse all the nodes of the current level
         while (n--) {
  
             Node* x = q.front();
             q.pop();
- 
             // print the last node of each level
             if (n == 0) {
                 cout << x->data << " ";
             }
-            // if left child is not null push it into the
-            // queue
+            // if left child is not null push it into the queue
             if (x->left)
                 q.push(x->left);
-            // if right child is not null push it into the
-            // queue
+            // if right child is not null push it into the queue
             if (x->right)
                 q.push(x->right);
         }
     }
 }
- 
-// Driver code
 int main()
 {
-    // Let's construct the tree as
-    // shown in example
- 
     Node* root = newNode(1);
     root->left = newNode(2);
     root->right = newNode(3);
