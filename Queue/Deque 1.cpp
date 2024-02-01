150ac79157c6a759f4c07977934ac0867b1fd387
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-02-01 06:09:27 +0100

    Update and rename Queue/Section A/Deque<> to Queue/Deque 1.cpp

--- Queue/Deque 1.cpp
+++ Queue/Deque 1.cpp
@@ -1,67 +1,42 @@
-// A C++ program to demonstrate implementation 
-// of k queues in a single
-// array in time and space efficient way
+//implementation of k queues in a single array in time and space efficient way
 #include<iostream>
 #include<climits>
 using namespace std;
- 
-// A C++ class to represent k queues 
-// in a single array of size n
+// represent k queues in a single array of size n
 class kQueues
 {   
-    // Array of size n to store actual 
-    // content to be stored in queue
+    // Array of size n to store actual content to be stored in queue
     int *arr;
- 
-    // Array of size k to store indexes 
-    // of front elements of the queue  
+    // Array of size k to store indexes of front elements of the queue  
     int *front;   
- 
-    // Array of size k to store indexes 
-    // of rear elements of queue
+    // Array of size k to store indexes of rear elements of queue
     int *rear; 
- 
-    // Array of size n to store next 
-    // entry in all queues            
+    // Array of size n to store next entry in all queue
     int *next;  
     int n, k;
- 
-    int free; // To store the beginning index of the free list
- 
-public:
-    //constructor to create k queue 
-    // in an array of size n
+    int free;
+    // To store the beginning index of the free list
+    public:
+    //constructor to create k queue in an array of size n
     kQueues(int k, int n);
- 
-    // A utility function to check if 
-    // there is space available
+    // function to check if there is space available
     bool isFull()   {  return (free == -1);  }
- 
-    // To enqueue an item in queue number 
-    // 'qn' where qn is from 0 to k-1
+    // To enqueue an item in queue number 'qn' where qn is from 0 to k-1
     void enqueue(int item, int qn);
- 
-    // To dequeue an from queue number 
-    // 'qn' where qn is from 0 to k-1
+    // To dequeue an from queue number 'qn' where qn is from 0 to k-1
     int dequeue(int qn);
- 
-    // To check whether queue number
-    // 'qn' is empty or not
+    // To check whether queue number 'qn' is empty or not
     bool isEmpty(int qn)  {  return (front[qn] == -1); }
 };
- 
-// Constructor to create k queues 
-// in an array of size n
+// Constructor to create k queues  in an array of size n
 kQueues::kQueues(int k1, int n1)
 {
-    // Initialize n and k, and allocate
-    // memory for all arrays
+    // Initialize n and k, and allocate memory for all arrays
     k = k1, n = n1;
     arr = new int[n];
     front = new int[k];
     rear = new int[k];
     next = new int[n];
- 
     // Initialize all queues as empty
     for (int i = 0; i < k; i++)
         front[i] = -1;
@@ -70,11 +45,10 @@ kQueues::kQueues(int k1, int n1)
     free = 0;
     for (int i=0; i<n-1; i++)
         next[i] = i+1;
-    next[n-1] = -1;  // -1 is used to indicate end of free list
+    next[n-1] = -1;
+    // -1 is used to indicate end of free list
 }
- 
-// To enqueue an item in queue number 
-// 'qn' where qn is from 0 to k-1
+// To enqueue an item in queue number 'qn' where qn is from 0 to k-1
 void kQueues::enqueue(int item, int qn)
 {
     // Overflow check
@@ -83,8 +57,8 @@ void kQueues::enqueue(int item, int qn)
         cout << "\nQueue Overflow\n";
         return;
     }
- 
-    int i = free;      // Store index of first free slot
+    int i = free;      
+    // Store index of first free slot
  
     // Update index of free slot to index of next slot in free list
     free = next[i];
@@ -95,14 +69,12 @@ void kQueues::enqueue(int item, int qn)
         next[rear[qn]] = i;
  
     next[i] = -1;
- 
     // Update next of rear and then rear for queue number 'qn'
     rear[qn] = i;
  
     // Put the item in array
     arr[i] = item;
 }
- 
 // To dequeue an from queue number 'qn' where qn is from 0 to k-1
 int kQueues::dequeue(int qn)
 {
@@ -112,22 +84,18 @@ int kQueues::dequeue(int qn)
          cout << "\nQueue Underflow\n";
          return INT_MAX;
     }
- 
     // Find index of front item in queue number 'qn'
     int i = front[qn];
    
     // Change top to store next of previous top
     front[qn] = next[i]; 
  
-    // Attach the previous front to the
-    // beginning of free list
+    // Attach the previous front to the beginning of free list
     next[i] = free;
     free = i;
- 
     // Return the previous front item
     return arr[i];
 }
-/* Driver program to test kStacks class */
 int main()
 {
     // Let us create 3 queue in an array of size 10
@@ -137,12 +105,10 @@ int main()
     // Let us put some items in queue number 2
     ks.enqueue(15, 2);
     ks.enqueue(45, 2);
- 
     // Let us put some items in queue number 1
     ks.enqueue(17, 1);
     ks.enqueue(49, 1);
     ks.enqueue(39, 1);
- 
     // Let us put some items in queue number 0
     ks.enqueue(11, 0);
     ks.enqueue(9, 0);
