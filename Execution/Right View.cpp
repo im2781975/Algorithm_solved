586872c703ab5659fa077d4dd7b40b625377a595
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-02-01 11:51:32 +0100

    Update and rename Queue/Section A/Right view to Execution/Right View.cpp

--- Execution/Right View.cpp
+++ Execution/Right View.cpp
@@ -1,13 +1,10 @@
-// C++ program to print right view of Binary Tree
+// print right view of Binary Tree
 #include <bits/stdc++.h>
 using namespace std;
- 
 struct Node {
     int data;
     struct Node *left, *right;
 };
- 
-// A utility function to
 // create a new Binary Tree Node
 struct Node* newNode(int item)
 {
@@ -17,36 +14,28 @@ struct Node* newNode(int item)
     temp->left = temp->right = NULL;
     return temp;
 }
- 
-// Recursive function to print
-// right view of a binary tree.
+// Recursive function to print right view of a binary tree.
 void rightViewUtil(struct Node* root, int level,
                    int* max_level)
 {
     // Base Case
     if (root == NULL)
         return;
- 
     // If this is the last Node of its level
     if (*max_level < level) {
         cout << root->data << "\t";
         *max_level = level;
     }
- 
-    // Recur for right subtree first,
-    // then left subtree
+    // Recur for right subtree first, then left subtree
     rightViewUtil(root->right, level + 1, max_level);
     rightViewUtil(root->left, level + 1, max_level);
 }
- 
 // A wrapper over rightViewUtil()
 void rightView(struct Node* root)
 {
     int max_level = 0;
     rightViewUtil(root, 1, &max_level);
 }
- 
-// Driver Code
 int main()
 {
     struct Node* root = newNode(1);
