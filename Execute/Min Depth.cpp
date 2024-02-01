c433734b009b2b367e1b8f07d9c45bcf26c6d5ed
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-02-01 11:09:58 +0100

    Update and rename Queue/Section A/Min depth to Execute/Min Depth.cpp

--- Execute/Min Depth.cpp
+++ Execute/Min Depth.cpp
@@ -1,21 +1,16 @@
-// C++ program to find minimum depth of a given Binary Tree
+// find minimum depth of a given Binary Tree
 #include<bits/stdc++.h>
 using namespace std;
- 
 // A BT Node
 struct Node
 {
     int data;
     struct Node* left, *right;
 };
- 
 int minDepth(Node *root)
 {
-    // Corner case. Should never be hit unless the code is 
-    // called on root = NULL
     if (root == NULL)
         return 0;
- 
     // Base case : Leaf Node. This accounts for height = 1.
     if (root->left == NULL && root->right == NULL)
     return 1;
@@ -33,8 +28,6 @@ int minDepth(Node *root)
   //height will be minimum of left and right height +1 
     return min(l , r) + 1;
 }
- 
-// Utility function to create new Node
 Node *newNode(int data)
 {
     Node *temp = new Node;
@@ -42,11 +35,8 @@ Node *newNode(int data)
     temp->left = temp->right = NULL;
     return (temp);
 }
- 
-// Driver program
 int main()
 {
-    // Let us construct the Tree shown in the above figure
     Node *root     = newNode(1);
     root->left     = newNode(2);
     root->right     = newNode(3);
