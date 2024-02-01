3fef940ced23d2c5574b7c5c04a88e066da2aba2
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-02-01 13:03:04 +0100

    Update and rename Queue/Section B/Find nxt<> to Execution/Find Next 1.cpp

--- Execution/Find Next 1.cpp
+++ Execution/Find Next 1.cpp
@@ -1,46 +1,31 @@
-/* Program to find next right of a given key */
+//find next right of a given key 
 #include <iostream>
 #include <queue>
 using namespace std;
- 
 // A Binary Tree Node
 struct node {
     struct node *left, *right;
     int key;
 };
- 
-// Method to find next right of given key k, it returns NULL
-// if k is not present in tree or k is the rightmost node of
-// its level
+// Method to find next right of given key k, it returns NULL if k is not present in tree or k is the rightmost node of its level
 node* nextRight(node* root, int key)
 {
-    // Base case
     if (root == NULL)
         return NULL;
- 
     // variable to store the result
     node* res = NULL;
- 
     // queue q to store the nodes of the tree
-    queue<node*> q;
- 
-    // push the root in the queue
+    queue<node*> q:
     q.push(root);
- 
-    // A modified BFS loop
     while (!q.empty()) {
-        // Get the count of the elements in the queue, this
-        // is also the count of elements present at the
-        // current level
+        // Get the count of the elements in the queue, this is also the count of elements present at the current level
         int n = q.size();
         // loop through the elements of the current level
         for (int i = 0; i < n; i++) {
             // take out the front of the queue
             node* temp = q.front();
             q.pop();
-            // if the key is found we check if there is any
-            // element next to it and return the answer
-            // accordingally
+            // if the key is found we check if there is any element next to it and return the answer accordingally
             if (temp->key == key) {
                 if (i != n - 1)
                     return q.front();
@@ -48,20 +33,15 @@ node* nextRight(node* root, int key)
                     return NULL;
             }
             // while the current level elements are
-            // processed we push their children into the
-            // queue
+            // processed we push their children into the queue
             if (temp->left)
                 q.push(temp->left);
             if (temp->right)
                 q.push(temp->right);
         }
     }
-    // If we reach here, it means the element was not found
-    // and thus no next element will be there
     return NULL;
 }
- 
-// Utility function to create a new tree node
 node* newNode(int key)
 {
     node* temp = new node;
@@ -69,8 +49,6 @@ node* newNode(int key)
     temp->left = temp->right = NULL;
     return temp;
 }
- 
-// A utility function to test above functions
 void test(node* root, int k)
 {
     node* nr = nextRight(root, k);
@@ -81,11 +59,8 @@ void test(node* root, int k)
         cout << "No next right node found for " << k
              << endl;
 }
- 
-// Driver program to test above functions
 int main()
 {
-    // Let us create binary tree given in the above example
     node* root = newNode(10);
     root->left = newNode(2);
     root->right = newNode(6);
