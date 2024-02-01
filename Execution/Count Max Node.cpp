a95597e5f3653633be35ffebc839b3b521239221
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-02-01 15:30:00 +0100

    Update and rename Queue/Section B/Max node to Execution/Count Max Node.cpp

--- Execution/Count Max Node.cpp
+++ Execution/Count Max Node.cpp
@@ -1,20 +1,14 @@
-// C++ implementation to find the level
-// having maximum number of Nodes
+// find the level having maximum number of Nodes
 #include <bits/stdc++.h>
 using namespace std;
- 
-/* A binary tree Node has data, pointer
-   to left child and a pointer to right
-   child */
+// A binary tree Node has data, pointer to left child and a pointer to right child
 struct Node
 {
     int data;
     struct Node* left;
     struct Node* right;
 };
- 
-/* Helper function that allocates a new node with the
-   given data and NULL left and right pointers. */
+// Helper function that allocates a new node with the given data and NULL left and right pointers. 
 struct Node* newNode(int data)
 {
     struct Node* node = new Node;
@@ -23,9 +17,7 @@ struct Node* newNode(int data)
     node->right = NULL;
     return(node);
 }
- 
-// function to find the level
-// having maximum number of Nodes
+// function to find the level having maximum number of Nodes
 int maxNodeLevel(Node *root)
 {
     if (root == NULL)
@@ -33,32 +25,24 @@ int maxNodeLevel(Node *root)
  
     queue<Node *> q;
     q.push(root);
- 
     // Current level
     int level = 0;
- 
     // Maximum Nodes at same level
     int max = INT_MIN;
- 
     // Level having maximum Nodes
     int level_no = 0;
- 
     while (1)
     {
         // Count Nodes in a level
         int NodeCount = q.size();
- 
         if (NodeCount == 0)
             break;
- 
-        // If it is maximum till now
-        // Update level_no to current level
+        // If it is maximum till now Update level_no to current level
         if (NodeCount > max)
         {
             max = NodeCount;
             level_no = level;
         }
- 
         // Pop complete current level
         while (NodeCount > 0)
         {
@@ -70,26 +54,21 @@ int maxNodeLevel(Node *root)
                 q.push(Node->right);
             NodeCount--;
         }
- 
         // Increment for next level
         level++;
     }
- 
     return level_no;
 }
- 
-// Driver program to test above
 int main()
 {
     // binary tree formation
-    struct Node *root = newNode(2);      /*        2      */
-    root->left        = newNode(1);      /*      /   \    */
-    root->right       = newNode(3);      /*     1     3      */
-    root->left->left  = newNode(4);      /*   /   \    \  */
-    root->left->right = newNode(6);      /*  4     6    8 */
-    root->right->right  = newNode(8);    /*       /       */
-    root->left->right->left = newNode(5);/*      5        */
- 
+    struct Node *root = newNode(2);
+    root->left        = newNode(1);
+    root->right       = newNode(3);
+    root->left->left  = newNode(4);      
+    root->left->right = newNode(6);
+    root->right->right  = newNode(8);    
+    root->left->right->left = newNode(5);
     printf("Level having maximum number of Nodes : %d",
             maxNodeLevel(root));
     return 0;
