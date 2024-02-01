83644bff5cd64826c1e2fb29421026f8b3c0dd9a
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-02-01 15:19:41 +0100

    Update and rename Queue/Section B/Insect graph to Execution/Check Path.cpp

--- Execution/Check Path.cpp
+++ Execution/Check Path.cpp
@@ -1,54 +1,42 @@
-// C++ program to check if there is exist a path between two vertices
-// of a graph.
+// check if there is exist a path between two vertices of a graph
 #include<iostream>
 #include <list>
 using namespace std;
- 
-// This class represents a directed graph using adjacency list 
-// representation
 class Graph
 {
-    int V;    // No. of vertices
-    list<int> *adj;    // Pointer to an array containing adjacency lists
-public:
-    Graph(int V);  // Constructor
-    void addEdge(int v, int w); // function to add an edge to graph
+    int V;   
+    list<int> *adj;
+    public:
+    Graph(int V);
+    void addEdge(int v, int w);
     bool isReachable(int s, int d);  
 };
- 
 Graph::Graph(int V)
 {
     this->V = V;
     adj = new list<int>[V];
 }
- 
 void Graph::addEdge(int v, int w)
 {
-    adj[v].push_back(w); // Add w to v’s list.
+    adj[v].push_back(w); 
 }
- 
 // A BFS based function to check whether d is reachable from s.
 bool Graph::isReachable(int s, int d)
 {
     // Base case
     if (s == d)
       return true;
- 
     // Mark all the vertices as not visited
     bool *visited = new bool[V];
     for (int i = 0; i < V; i++)
         visited[i] = false;
- 
-    // Create a queue for BFS
+
     list<int> queue;
- 
     // Mark the current node as visited and enqueue it
     visited[s] = true;
     queue.push_back(s);
- 
     // it will be used to get all adjacent vertices of a vertex
     list<int>::iterator i;
- 
     while (!queue.empty())
     {
         // Dequeue a vertex from queue and print it
@@ -56,15 +44,13 @@ bool Graph::isReachable(int s, int d)
         queue.pop_front();
  
         // Get all adjacent vertices of the dequeued vertex s
-        // If a adjacent has not been visited, then mark it visited
-        // and enqueue it
+        // If a adjacent has not been visited, then mark it visited and enqueue it
         for (i = adj[s].begin(); i != adj[s].end(); ++i)
         {
             // If this adjacent node is the destination node, then 
             // return true
             if (*i == d)
                 return true;
- 
             // Else, continue to do BFS
             if (!visited[*i])
             {
@@ -73,15 +59,11 @@ bool Graph::isReachable(int s, int d)
             }
         }
     }
-     
     // If BFS is complete without visiting d
     return false;
 }
- 
-// Driver program to test methods of graph class
 int main()
 {
-    // Create a graph given in the above diagram
     Graph g(4);
     g.addEdge(0, 1);
     g.addEdge(0, 2);
