59f57494b982711f7c4da1c7ad83c9a829a7f94f
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-02-02 21:33:48 +0100

    Update and rename Queue/Section C/Min dst to Execution/Minimum Dist.cpp

--- Execution/Minimum Dist.cpp
+++ Execution/Minimum Dist.cpp
@@ -1,29 +1,20 @@
-// C++ Code implementation for above problem
 #include <bits/stdc++.h>
 using namespace std;
- 
 #define N 4
 #define M 4
- 
-// QItem for current location and distance
-// from source location
+// QItem for current location and distance from source location
 class QItem {
 public:
     int row;
     int col;
     int dist;
     QItem(int x, int y, int w)
-        : row(x), col(y), dist(w)
-    {
-    }
+        : row(x), col(y), dist(w){}
 };
- 
 int minDistance(char grid[N][M])
 {
     QItem source(0, 0, 0);
- 
-    // To keep track of visited QItems. Marking
-    // blocked cells as visited.
+    // To keep track of visited QItems. Marking blocked cells as visited.
     bool visited[N][M];
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < M; j++)
@@ -32,7 +23,6 @@ int minDistance(char grid[N][M])
                 visited[i][j] = true;
             else
                 visited[i][j] = false;
- 
             // Finding source
             if (grid[i][j] == 's')
             {
@@ -41,7 +31,6 @@ int minDistance(char grid[N][M])
             }
         }
     }
- 
     // applying BFS on matrix cells starting from source
     queue<QItem> q;
     q.push(source);
@@ -49,7 +38,6 @@ int minDistance(char grid[N][M])
     while (!q.empty()) {
         QItem p = q.front();
         q.pop();
- 
         // Destination found;
         if (grid[p.row][p.col] == 'd')
             return p.dist;
@@ -60,21 +48,18 @@ int minDistance(char grid[N][M])
             q.push(QItem(p.row - 1, p.col, p.dist + 1));
             visited[p.row - 1][p.col] = true;
         }
- 
         // moving down
         if (p.row + 1 < N &&
             visited[p.row + 1][p.col] == false) {
             q.push(QItem(p.row + 1, p.col, p.dist + 1));
             visited[p.row + 1][p.col] = true;
         }
- 
         // moving left
         if (p.col - 1 >= 0 &&
             visited[p.row][p.col - 1] == false) {
             q.push(QItem(p.row, p.col - 1, p.dist + 1));
             visited[p.row][p.col - 1] = true;
         }
- 
          // moving right
         if (p.col + 1 < M &&
             visited[p.row][p.col + 1] == false) {
@@ -84,15 +69,12 @@ int minDistance(char grid[N][M])
     }
     return -1;
 }
- 
-// Driver code
 int main()
 {
     char grid[N][M] = { { '0', '*', '0', 's' },
-                        { '*', '0', '*', '*' },
-                        { '0', '*', '*', '*' },
-                        { 'd', '*', '*', '*' } };
- 
+    { '*', '0', '*', '*' },
+    { '0', '*', '*', '*' },
+    { 'd', '*', '*', '*' } };
     cout << minDistance(grid);
     return 0;
 }
