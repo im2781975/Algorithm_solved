31f2193bd1235bde37be2134d48c27606d1b060d
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-02-02 08:17:46 +0100

    Update and rename Queue/Section C/Min cost<> to Execution/Minimum Cost.cpp

--- Execution/Minimum Cost.cpp
+++ Execution/Minimum Cost.cpp
@@ -1,82 +1,56 @@
-// C++ Program to implement
-// the above approach
 #include <bits/stdc++.h>
 using namespace std;
- 
 // Stores minimum-cost of path from source
 int minSum = INT_MAX;
  
-// Function to Perform BFS on graph g
-// starting from vertex v
-void getMinPathSum(unordered_map<int,
-                                 vector<pair<int,
-                                             int> > >& graph,
-                   vector<bool>& visited,
-                   vector<int> necessary,
-                   int src, int dest, int currSum)
+// Function to Perform BFS on graph g starting from vertex v
+void getMinPathSum(unordered_map<int,vector<pair<int,
+    int> > >& graph,vector<bool>& visited,
+    vector<int> necessary,int src, int dest, int currSum)
 {
     // If destination is reached
     if (src == dest) {
         // Set flag to true
         bool flag = true;
- 
         // Visit all the intermediate nodes
         for (int i : necessary) {
  
-            // If any intermediate node
-            // is not visited
+            // If any intermediate node is not visited
             if (!visited[i]) {
                 flag = false;
                 break;
             }
         }
- 
-        // If all intermediate
-        // nodes are visited
+        // If all intermediate nodes are visited
         if (flag)
- 
             // Update the minSum
             minSum = min(minSum, currSum);
         return;
     }
     else {
- 
-        // Mark the current node
-        // visited
+        // Mark the current node visited
         visited[src] = true;
- 
         // Traverse adjacent nodes
         for (auto node : graph[src]) {
  
             if (!visited[node.first]) {
- 
                 // Mark the neighbour visited
                 visited[node.first] = true;
- 
-                // Find minimum cost path
-                // considering the neighbour
-                // as the source
-                getMinPathSum(graph, visited,
-                              necessary, node.first,
-                              dest, currSum + node.second);
- 
+                // Find minimum cost path considering the neighbour as the source
+                getMinPathSum(graph, visited,    necessary, node.first,
+                dest, currSum + node.second);
                 // Mark the neighbour unvisited
                 visited[node.first] = false;
             }
         }
- 
         // Mark the source unvisited
         visited[src] = false;
     }
 }
- 
-// Driver Code
 int main()
 {
     // Stores the graph
-    unordered_map<int, vector<pair<int,
-                                   int> > >
-        graph;
+    unordered_map<int, vector<pair<int,int> > > graph;
     graph[0] = { { 1, 2 }, { 2, 3 }, { 3, 2 } };
     graph[1] = { { 4, 4 }, { 0, 1 } };
     graph[2] = { { 4, 5 }, { 5, 6 } };
@@ -87,22 +61,15 @@ int main()
  
     // Number of nodes
     int n = 7;
- 
     // Source
     int source = 0;
- 
     // Destination
     int dest = 6;
- 
-    // Keeps a check on visited
-    // and unvisited nodes
+    // Keeps a check on visited and unvisited nodes
     vector<bool> visited(n, false);
- 
     // Stores intermediate nodes
     vector<int> necessary{ 2, 4 };
- 
-    getMinPathSum(graph, visited, necessary,
-                  source, dest, 0);
+    getMinPathSum(graph, visited, necessary,source, dest, 0);
  
     // If no path is found
     if (minSum == INT_MAX)
