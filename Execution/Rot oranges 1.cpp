5aeefb893dbf55de3a3ef4751d5970352bdb5f51
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-02-03 11:18:10 +0100

    Update and rename Queue/Section C/Min time<1> to Execution/Rot oranges 1.cpp

--- Execution/Rot oranges 1.cpp
+++ Execution/Rot oranges 1.cpp
@@ -1,32 +1,25 @@
-// C++ program to find minimum time required to make all
-// oranges rotten
+//  find minimum time required to make all oranges rotten
 #include <bits/stdc++.h>
 #define R 3
 #define C 5
 using namespace std;
- 
-// This function finds if it is possible to rot all oranges
-// or not. If possible, then it returns minimum time
-// required to rot all, otherwise returns -1
+// finds if it is possible to rot all oranges or not. If possible, then it returns minimum time required to rot all
 int rotOranges(vector<vector<int> >& grid)
 {
-    int n = grid.size(); // row size
-    int m = grid[0].size(); // column size
- 
-    // delrow and delcol are used to traverse in
-    // up,right,bottom and left respectively.
+    int n = grid.size(); 
+    // row size
+    int m = grid[0].size(); 
+    // column size
+    // delrow and delcol are used to traverse in up,right,bottom and left respectively.
  
     int delrow[] = { -1, 0, 1, 0 };
     int delcol[] = { 0, 1, 0, -1 };
  
     // visited matrix to keep track of the visited cell.
     int vis[n][m];
- 
-    // queue stores rowIndex,colIndex and time taken to rot
-    // respectively.
+    // queue stores rowIndex,colIndex and time taken to rot respectively.
  
     queue<pair<pair<int, int>, int> > q;
- 
     // counter to keep track of fresh cells.
     int cntfresh = 0;
  
@@ -34,17 +27,19 @@ int rotOranges(vector<vector<int> >& grid)
         for (int j = 0; j < m; j++) {
             if (grid[i][j] == 2) {
                 q.push({ { i, j },
-                         0 }); // already rotten hence 0
-                               // time to rot.
+                         0 });
+                // already rotten hence 0
+                //time to rot.
                 vis[i][j]
-                    = 2; // visited cell marked as rotten.
+                    = 2; 
+                // visited cell marked as rotten.
             }
             else {
                 vis[i][j] = 0; // unvisited
             }
             if (grid[i][j] == 1)
                 cntfresh++; // maintaining count for fresh
-                            // oranges.
+                // oranges.
         }
     }
     int cnt = 0, tm = 0;
@@ -56,30 +51,25 @@ int rotOranges(vector<vector<int> >& grid)
         q.pop();
  
         tm = max(tm, t);
- 
         // checking for adjacent nodes in 4 directions.
         for (int i = 0; i < 4; i++) {
             int nrow = row + delrow[i];
             int ncol = col + delcol[i];
  
-            // checking the validity of a node and also
-            // vis[nrow][ncol] !=2
+            // checking the validity of a node and also vis[nrow][ncol] !=2
             if (nrow >= 0 && nrow < n && ncol >= 0
                 && ncol < m && grid[nrow][ncol] == 1
                 && vis[nrow][ncol] != 2) {
                 vis[nrow][ncol] = 2; // adj orange is rotten
                 q.push({ { nrow, ncol },
                          t + 1 }); // incrementing time for
-                                   // that orange by 1
+               // that orange by 1
                 cnt++;
             }
         }
     }
- 
     return (cnt == cntfresh) ? tm : -1;
 }
- 
-// Driver program
 int main()
 {
     vector<vector<int> > arr
