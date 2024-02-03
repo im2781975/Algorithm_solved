54d7b40a44809be0c0b27102efa3c6e5a76bc170
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-02-03 21:37:09 +0100

    Update and rename Find common<> to Intersection 1.cpp

--- Execution/Intersection 1.cpp
+++ Execution/Intersection 1.cpp
@@ -1,44 +1,35 @@
-// C++ program to print common elements in three arrays
+// print common elements in three arrays
 #include <bits/stdc++.h>
 using namespace std;
  
-// This function prints common elements in ar1
+// prints common elements in ar1
 void findCommon(int ar1[], int ar2[], int ar3[], int n1,
                 int n2, int n3)
 {
-    // Initialize starting indexes for ar1[], ar2[] and
-    // ar3[]
+    // Initialize starting indexes for ar1[], ar2[] and ar3[]
     int i = 0, j = 0, k = 0;
  
-    // Iterate through three arrays while all arrays have
-    // elements
+    // Iterate through three arrays while all arrays have elements
     while (i < n1 && j < n2 && k < n3) {
- 
-        // If x = y and y = z, print any of them and move
-        // ahead in all arrays
+        // If x = y and y = z, print any of them and move ahead in all arrays
         if (ar1[i] == ar2[j] && ar2[j] == ar3[k]) {
             cout << ar1[i] << " ";
             i++;
             j++;
             k++;
         }
- 
         // x < y
         else if (ar1[i] < ar2[j])
             i++;
- 
         // y < z
         else if (ar2[j] < ar3[k])
             j++;
- 
-        // We reach here when x > y and z < y, i.e., z is
+        // We reach here when z is
         // smallest
         else
             k++;
     }
 }
- 
-// Driver code
 int main()
 {
     int ar1[] = { 1, 5, 10, 20, 40, 80 };
