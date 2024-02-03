2dc89a2f73a9445bf35a1c3a0cb6aac50c319a34
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-02-03 14:57:11 +0100

    Update and rename Circular queue to Circular Queue.cpp

--- Queue/Circular Queue.cpp
+++ Queue/Circular Queue.cpp
@@ -1,18 +1,14 @@
-// C++ program for insertion and
-// deletion in Circular Queue
+// insertion and deletion in Circular Queue
 #include <bits/stdc++.h>
 using namespace std;
- 
 // Structure of a Node
 struct Node {
     int data;
     struct Node* link;
 };
- 
 struct Queue {
     struct Node *front, *rear;
 };
- 
 // Function to create Circular queue
 void enQueue(Queue* q, int value)
 {
@@ -26,7 +22,6 @@ void enQueue(Queue* q, int value)
     q->rear = temp;
     q->rear->link = q->front;
 }
- 
 // Function to delete element from Circular Queue
 int deQueue(Queue* q)
 {
@@ -34,9 +29,8 @@ int deQueue(Queue* q)
         cout << "Queue is empty";
         return INT_MIN;
     }
- 
     // If this is the last node to be deleted
-    int value; // Value to be dequeued
+    int value;
     if (q->front == q->rear) {
         value = q->front->data;
         free(q->front);
@@ -51,11 +45,8 @@ int deQueue(Queue* q)
         q->rear->link = q->front;
         free(temp);
     }
- 
     return value;
 }
- 
-// Function displaying the elements of Circular Queue
 void displayQueue(struct Queue* q)
 {
     struct Node* temp = q->front;
@@ -66,22 +57,15 @@ void displayQueue(struct Queue* q)
     }
     cout << temp->data;
 }
- 
-/* Driver of the program */
 int main()
 {
-    // Create a queue and initialize front and rear
     Queue* q = new Queue;
     q->front = q->rear = NULL;
- 
     // Inserting elements in Circular Queue
     enQueue(q, 14);
     enQueue(q, 22);
     enQueue(q, 6);
- 
-    // Display elements present in Circular Queue
     displayQueue(q);
- 
     // Deleting elements from Circular Queue
     cout << endl << "Deleted value = " << deQueue(q);
     cout << endl << "Deleted value = " << deQueue(q);
