3693da76f7933df73b7c9e7c5cd8ce181bb5d077
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-02-03 14:49:40 +0100

    Update and rename Queue/Section C/slide window to Slide Window 2.cpp

--- Slide Window 2.cpp
+++ Slide Window 2.cpp
@@ -1,14 +1,9 @@
-// C++ program for the above approach
- 
 #include <bits/stdc++.h>
 using namespace std;
- 
-// Method to find the maximum for each
-// and every contiguous subarray of size K.
+// Method to find the maximum for each and every contiguous subarray of size K.
 void printKMax(int arr[], int N, int K)
 {
     int j, max;
- 
     for (int i = 0; i <= N - K; i++) {
         max = arr[i];
  
@@ -19,15 +14,11 @@ void printKMax(int arr[], int N, int K)
         cout << max << " ";
     }
 }
- 
-// Driver's code
 int main()
 {
     int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
     int N = sizeof(arr) / sizeof(arr[0]);
     int K = 3;
-   
-      // Function call
     printKMax(arr, N, K);
     return 0;
 }
