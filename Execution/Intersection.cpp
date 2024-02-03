b66d5c47de2066afc7d5f881d20b8786dd98fb5c
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-02-03 21:35:07 +0100

    Update and rename Find common to Intersection.cpp

--- Execution/Intersection.cpp
+++ Execution/Intersection.cpp
@@ -1,24 +1,20 @@
 #include <iostream>
 using namespace std;
- 
-// Function to find the intersection of two arrays
+// find the intersection of two arrays
 void FindIntersection(int arr1[], int arr2[], int temp[],
-                      int m, int n, int& k)
+    int m, int n, int& k)
 {
     int i = 0, j = 0;
-    // vector to store the intersection of the arr1[] and
-    // arr2[]
+    // vector to store the intersection of the arr1[] and arr2[]
     while (i < m && j < n) {
         // ith element can not be common element
         if (arr1[i] < arr2[j]) {
             i++;
         }
- 
         // jth element can not be common element
         else if (arr2[j] < arr1[i]) {
             j++;
         }
- 
         // if arr1[i] == arr2[j]
         else {
             temp[k] = arr1[i];
@@ -28,7 +24,6 @@ void FindIntersection(int arr1[], int arr2[], int temp[],
         }
     }
 }
- 
 int main()
 {
  
@@ -39,21 +34,16 @@ int main()
     int n2 = sizeof(arr2) / sizeof(arr2[0]);
     int n3 = sizeof(arr3) / sizeof(arr3[0]);
  
-    // temp array to store the common elements of arr1 and
-    // arr2 arrays
+    // temp array to store the common elements of arr1 and arr2 arrays
     int temp[200000];
- 
-    // ans array to store the common elements of temp and
-    // arr3 arrays (i.e common elements of all 3 arrays)
+    // ans array to store the common elements of temp and arr3 arrays (i.e common elements of all 3 arrays)
     int ans[200000];
  
     int k = 0;
- 
     // function call to find the temp array
     FindIntersection(arr1, arr2, temp, n1, n2, k);
     int tempSize = k;
     k = 0;
- 
     // function call to find the ans array.
     FindIntersection(arr3, temp, ans, n3, tempSize, k);
     cout << "Common elements present in arrays are : \n";
