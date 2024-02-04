f3353088e12e7ad02b01461ce3a4cfb1e13cbf6b
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-02-04 21:14:53 +0100

    Update and rename nth largest<1> to Nth Largest 1.cpp

--- Execution/Nth Largest 1.cpp
+++ Execution/Nth Largest 1.cpp
@@ -1,41 +1,27 @@
-// c++ program for the above approach
- 
 #include <bits/stdc++.h>
 using namespace std;
- 
 int partition(int arr[], int l, int r);
- 
-// This function stops at K'th largest element in arr[l..r]
-// using QuickSort based method.
+// This function stops at K'th largest element in arr[l..r] using QuickSort based method.
 void KthLargest(int arr[], int l, int r, int K, int N)
 {
- 
-    // Partition the array around last element and get
-    // position of pivot element in sorted array
+    // Partition the array around last element and get position of pivot element in sorted array
     int pos = partition(arr, l, r);
- 
     // If position is same as k
     if (pos - l == K - 1)
         return;
- 
-    // If position is less, recur
-    // for right subarray
+    // If position is less, recur for right subarray
     else if (pos - l < K - 1)
-        return KthLargest(arr, pos + 1, r, K - pos + l - 1,
-                          N);
- 
+        return KthLargest(arr, pos + 1, r, K - pos + l - 1, N);
     // Else recur for left subarray
     else
         return KthLargest(arr, l, pos - 1, K, N);
 }
- 
 void swap(int* a, int* b)
 {
     int temp = *a;
     *a = *b;
     *b = temp;
 }
- 
 int partition(int arr[], int l, int r)
 {
     int x = arr[r], i = l;
@@ -45,12 +31,9 @@ int partition(int arr[], int l, int r)
             i++;
         }
     }
- 
     swap(&arr[i], &arr[r]);
     return i;
 }
- 
-// Driver code
 int main()
 {
     int arr[]
@@ -58,15 +41,11 @@ int main()
  
     int N = sizeof(arr) / sizeof(arr[0]);
     int k = 3;
- 
     // For Largest
     KthLargest(arr, 0, N - 1, k, N);
- 
     // Print KLargest no.
- 
     cout << k << " largest elements are : ";
     for (int i = N - 1; i >= N - k; i--)
         cout << arr[i] << "  ";
- 
     return 0;
 }
