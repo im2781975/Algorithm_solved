dadbde8c0c6e75c60a5856e8783817b351c864e0
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-02-04 21:16:55 +0100

    Update and rename nth largest<2> to Nth Largest 2.cpp

--- Execution/Nth Largest 2.cpp
+++ Execution/Nth Largest 2.cpp
@@ -1,27 +1,19 @@
-// C++ code for k largest elements in an array
+// k largest elements in an array
 #include <bits/stdc++.h>
 using namespace std;
- 
-// Function to find k largest array element
+// find k largest array element
 void kLargest(vector<int>& v, int N, int K)
 {
-    // Implementation using
-    // a Priority Queue
+    // Implementation using a Priority Queue
     priority_queue<int, vector<int>, greater<int> > pq;
- 
     for (int i = 0; i < N; ++i) {
- 
-        // Insert elements into
-        // the priority queue
+        // Insert elements into the priority queue
         pq.push(v[i]);
- 
-        // If size of the priority
-        // queue exceeds k
+        // If size of the priority queue exceeds k
         if (pq.size() > K) {
             pq.pop();
         }
     }
- 
     // Print the k largest element
     while (!pq.empty()) {
         cout << pq.top() << " ";
@@ -29,13 +21,8 @@ void kLargest(vector<int>& v, int N, int K)
     }
     cout << endl;
 }
- 
- 
- 
-// driver program
 int main()
 {
-    // Given array
     vector<int> arr
         = { 11, 3, 2, 1, 15, 5, 4, 45, 88, 96, 50, 45 };
     // Size of array
@@ -43,5 +30,4 @@ int main()
     int k = 3;
     cout << k << " largest elements are : ";
     kLargest(arr, n, k);
-     
 }
