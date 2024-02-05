a63e7e647f4e06b1ff5a15ff74859539046789db
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-02-05 11:34:26 +0100

    Update and rename Fixed point to Fixed Point.cpp

---
+++ Execution/Fixed Point.cpp
@@ -0,0 +1,36 @@
+// fixed point in an array using linear search 
+#include <bits/stdc++.h> 
+using namespace std; 
+int linearSearch(int arr[], int n) 
+{ 
+    int i; 
+    for (i = 0; i < n; i++) { 
+        if (arr[i] == i) 
+            return i; 
+    } 
+    return -1; 
+}
+int binarySearch(int arr[], int low, int high) 
+{ 
+    if (high >= low) { 
+        int mid = low + (high - low) / 2; 
+        if (mid == arr[mid]) 
+            return mid; 
+        int res = -1; 
+        if (mid + 1 <= arr[high]) 
+            res = binarySearch(arr, (mid + 1), high); 
+        if (res != -1) 
+            return res; 
+        if (mid - 1 >= arr[low]) 
+            return binarySearch(arr, low, (mid - 1)); 
+    } 
+    return -1; 
+} 
+int main() 
+{ 
+    int arr[] = { -10, -1, 0, 3, 10, 11, 30, 50, 100 }; 
+    int n = sizeof(arr) / sizeof(arr[0]); 
+    cout << "Fixed Point is " << linearSearch(arr, n); 
+    cout << "Fixed Point is " << binarySearch(arr, 0, n - 1); 
+    return 0; 
+} 
