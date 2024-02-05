a3bbfac8580b5b7c1b2d30409f07fe47a45f175c
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-02-05 11:15:13 +0100

    Update and rename Find min<> to Dig Min 1.cpp

--- Execution/Dig Min 1.cpp
+++ Execution/Dig Min 1.cpp
@@ -1,14 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
 int findMin(vector<int>& arr, int low, int high)
 {
     // If the array is not rotated
     if (arr[low] <= arr[high]) {
         return arr[low];
     }
- 
-    // Binary search
     while (low <= high) {
         int mid = (low + high) / 2;
  
@@ -16,24 +13,17 @@ int findMin(vector<int>& arr, int low, int high)
         if (mid-1 >= 0 && arr[mid] < arr[mid - 1]) {
             return arr[mid];
         }
-        // If the left half is sorted, the minimum element
-        // must be in the right half
+        // If the left half is sorted, the minimum element must be in the right half
         if (arr[mid] > arr[high]) {
             low = mid + 1;
         }
- 
-        // If the right half is sorted, the minimum element
-        // must be in the left half
+        // If the right half is sorted, the minimum element must be in the left half
         else {
             high = mid - 1;
         }
     }
- 
-    // If no minimum element is found, return -1
     return -1;
 }
- 
-// Driver program to test above functions
 int main()
 {
     vector<int> arr = { 5, 6, 1, 2, 3, 4 };
