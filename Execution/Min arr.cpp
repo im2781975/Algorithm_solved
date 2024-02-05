cad1d005fc5065fd3797290a68a3cef92c6dbc2d
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-02-05 17:23:39 +0100

    Update and rename Min arr to Min arr.cpp

--- Execution/Min arr.cpp
+++ Execution/Min arr.cpp
@@ -1,26 +1,20 @@
-// C++ program of above implementation
 #include <bits/stdc++.h>
 using namespace std;
- 
-// structure is used to return
-// two values from minMax()
+// structure is used to return two values from minMax()
 struct Pair {
     int min;
     int max;
 };
- 
 struct Pair getMinMax(int arr[], int low, int high)
 {
     struct Pair minmax, mml, mmr;
     int mid;
- 
     // If there is only one element
     if (low == high) {
         minmax.max = arr[low];
         minmax.min = arr[low];
         return minmax;
     }
- 
     // If there are two elements
     if (high == low + 1) {
         if (arr[low] > arr[high]) {
@@ -33,12 +27,10 @@ struct Pair getMinMax(int arr[], int low, int high)
         }
         return minmax;
     }
- 
     // If there are more than 2 elements
     mid = (low + high) / 2;
     mml = getMinMax(arr, low, mid);
     mmr = getMinMax(arr, mid + 1, high);
- 
     // Compare minimums of two parts
     if (mml.min < mmr.min)
         minmax.min = mml.min;
@@ -53,8 +45,6 @@ struct Pair getMinMax(int arr[], int low, int high)
  
     return minmax;
 }
- 
-// Driver code
 int main()
 {
     int arr[] = { 1000, 11, 445, 1, 330, 3000 };
