312e54620bc22fae639d65e2d20093f2ae804d91
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-02-08 07:31:04 +0100

    Update and rename Stack/Section A/Imp stack to Stack/Display 2.cpp

--- Stack/Display 2.cpp
+++ Stack/Display 2.cpp
@@ -1,43 +1,30 @@
-// C++ program to implement a stack using
-// single queue
+// implement a stack using single queue
 #include<bits/stdc++.h>
 using namespace std;
- 
-// User defined stack that uses a queue
 class Stack
 {
     queue<int>q;
-public:
+    public:
     void push(int val);
     void pop();
     int top();
     bool empty();
 };
- 
-// Push operation
 void Stack::push(int val)
 {
     //  Get previous size of queue
     int s = q.size();
- 
-    // Push current element
     q.push(val);
  
-    // Pop (or Dequeue) all previous
-    // elements and put them after current
-    // element
+    // Pop (or Dequeue) all previous elements and put them after current element
     for (int i=0; i<s; i++)
     {
-        // this will add front element into
-        // rear of queue
+        // this will add front element into rear of queue
         q.push(q.front());
- 
         // this will delete front element
         q.pop();
     }
 }
- 
-// Removes the top element
 void Stack::pop()
 {
     if (q.empty())
@@ -45,20 +32,15 @@ void Stack::pop()
     else
         q.pop();
 }
- 
-// Returns top of stack
 int  Stack::top()
 {
     return (q.empty())? -1 : q.front();
 }
- 
 // Returns true if Stack is empty else false
 bool Stack::empty()
 {
     return (q.empty());
 }
- 
-// Driver code
 int main()
 {
     Stack s;
