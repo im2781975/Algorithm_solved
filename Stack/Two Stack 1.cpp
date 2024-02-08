1a624268088c4f5eea1e450a7bd9ceeea940c344
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-02-08 06:02:02 +0100

    Update and rename Stack/Section A/Compare stack<> to Stack/Two Stack 1.cpp

--- Stack/Two Stack 1.cpp
+++ Stack/Two Stack 1.cpp
@@ -1,26 +1,20 @@
 #include <bits/stdc++.h> 
 using namespace std; 
-  
 class twoStacks { 
     int* arr; 
     int size; 
     int top1, top2; 
-  
-public: 
-    // Constructor 
+    public: 
     twoStacks(int n) 
     { 
         size = n; 
         arr = new int[n]; 
         top1 = n / 2 + 1; 
         top2 = n / 2; 
-    } 
-  
-    // Method to push an element x to stack1 
+    }
     void push1(int x) 
     { 
-        // There is at least one empty 
-        // space for new element 
+        // There is at least one empty space for new element 
         if (top1 > 0) { 
             top1--; 
             arr[top1] = x; 
@@ -30,15 +24,10 @@ public:
                  << " By element : " << x << endl; 
             return; 
         } 
-    } 
-  
-    // Method to push an element 
-    // x to stack2 
+    }
     void push2(int x) 
     { 
-  
-        // There is at least one empty 
-        // space for new element 
+        // There is at least one empty space for new element 
         if (top2 < size - 1) { 
             top2++; 
             arr[top2] = x; 
@@ -48,9 +37,7 @@ public:
                  << " By element : " << x << endl; 
             return; 
         } 
-    } 
-  
-    // Method to pop an element from first stack 
+    }
     int pop1() 
     { 
         if (top1 <= size / 2) { 
@@ -62,10 +49,7 @@ public:
             cout << "Stack UnderFlow"; 
             exit(1); 
         } 
-    } 
-  
-    // Method to pop an element 
-    // from second stack 
+    }
     int pop2() 
     { 
         if (top2 >= size / 2 + 1) { 
@@ -78,9 +62,7 @@ public:
             exit(1); 
         } 
     } 
-}; 
-  
-/* Driver program to test twoStacks class */
+};
 int main() 
 { 
     twoStacks ts(5); 
