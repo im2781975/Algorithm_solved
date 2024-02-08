75bbad2972b8330c8f219c010ff677ee9ed25cdf
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-02-08 07:36:51 +0100

    Update and rename Stack/Section A/K stacks to Stack/K Stack.cpp

--- Stack/K Stack.cpp
+++ Stack/K Stack.cpp
@@ -1,30 +1,26 @@
-// A C++ program to demonstrate implementation of k stacks in a single 
-// array in time and space efficient way
+//implementation of k stacks in a single array 
 #include<bits/stdc++.h>
 using namespace std;
- 
-// A C++ class to represent k stacks in a single array of size n
 class kStacks
 {
-    int *arr;   // Array of size n to store actual content to be stored in stacks
-    int *top;   // Array of size k to store indexes of top elements of stacks
-    int *next;  // Array of size n to store next entry in all stacks
-                // and free list
+    int *arr;   
+    // Array of size n to store actual content to be stored in stacks
+    int *top;   
+    // Array of size k to store indexes of top elements of stacks
+    int *next;  
+    // Array of size n to store next entry in all stacks and free list
     int n, k;
-    int free; // To store beginning index of free list
-public:
-    //constructor to create k stacks in an array of size n
+    int free; 
+    // To store beginning index of free list
+    public:
     kStacks(int k, int n);
- 
-    // A utility function to check if there is space available
+    // check if there is space available
     bool isFull()   {  return (free == -1);  }
  
     // To push an item in stack number 'sn' where sn is from 0 to k-1
     void push(int item, int sn);
- 
     // To pop an from stack number 'sn' where sn is from 0 to k-1
     int pop(int sn);
- 
     // To check whether stack number 'sn' is empty or not
     bool isEmpty(int sn)  {  return (top[sn] == -1); }
 };
@@ -37,18 +33,16 @@ kStacks::kStacks(int k1, int n1)
     arr = new int[n];
     top = new int[k];
     next = new int[n];
- 
     // Initialize all stacks as empty
     for (int i = 0; i < k; i++)
         top[i] = -1;
- 
     // Initialize all spaces as free
     free = 0;
     for (int i=0; i<n-1; i++)
         next[i] = i+1;
-    next[n-1] = -1;  // -1 is used to indicate end of free list
+    next[n-1] = -1;  
+    // -1 is used to indicate end of free list
 }
- 
 // To push an item in stack number 'sn' where sn is from 0 to k-1
 void kStacks::push(int item, int sn)
 {
@@ -58,8 +52,8 @@ void kStacks::push(int item, int sn)
         cout << "\nStack Overflow\n";
         return;
     }
- 
-    int i = free;      // Store index of first free slot
+    int i = free;      
+    // Store index of first free slot
  
     // Update index of free slot to index of next slot in free list
     free = next[i];
@@ -67,7 +61,6 @@ void kStacks::push(int item, int sn)
     // Update next of top and then top for stack number 'sn'
     next[i] = top[sn];
     top[sn] = i;
- 
     // Put the item in array
     arr[i] = item;
 }
@@ -82,10 +75,8 @@ int kStacks::pop(int sn)
          return INT_MAX;
     }
  
- 
     // Find index of top item in stack number 'sn'
     int i = top[sn];
- 
     top[sn] = next[i];  // Change top to store next of previous top
  
     // Attach the previous top to the beginning of free list
@@ -95,24 +86,22 @@ int kStacks::pop(int sn)
     // Return the previous top item
     return arr[i];
 }
- 
-/* Driver program to test twoStacks class */
 int main()
 {
     // Let us create 3 stacks in an array of size 10
     int k = 3, n = 10;
     kStacks ks(k, n);
  
-    // Let us put some items in stack number 2
+    // put some items in stack number 2
     ks.push(15, 2);
     ks.push(45, 2);
  
-    // Let us put some items in stack number 1
+    // put some items in stack number 1
     ks.push(17, 1);
     ks.push(49, 1);
     ks.push(39, 1);
  
-    // Let us put some items in stack number 0
+    //put some items in stack number 0
     ks.push(11, 0);
     ks.push(9, 0);
     ks.push(7, 0);
@@ -120,6 +109,5 @@ int main()
     cout << "Popped element from stack 2 is " << ks.pop(2) << endl;
     cout << "Popped element from stack 1 is " << ks.pop(1) << endl;
     cout << "Popped element from stack 0 is " << ks.pop(0) << endl;
- 
     return 0;
 }
