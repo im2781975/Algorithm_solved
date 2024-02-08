58332429767c7f20c2bb3417419ed896e29fb54b
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-02-08 06:19:56 +0100

    Update and rename Stack/Section A/Design stack<> to Stack/Designed Stack 1.cpp

--- Stack/Designed Stack 1.cpp
+++ Stack/Designed Stack 1.cpp
@@ -1,28 +1,20 @@
-// C++ program for the above approach
 #include <bits/stdc++.h>
 using namespace std;
- 
 int mini(int a, int b) { return a > b ? b : a; }
- 
 class MinStack {
 public:
     stack<pair<int, int> > s;
  
     void push(int element)
     {
-        /* new max will be given no. if stack is empty else
-        we compare given no. to max at current top of
-        stack*/
- 
+        // new max will be given no. if stack is empty else
+        //we compare given no. to max at current top of stack
         int new_min = s.empty()
                           ? element
                           : mini(element, s.top().second);
- 
         // we push the pair of given_element,new_min in s
- 
         s.push({ element, new_min });
     }
- 
     int pop()
     {
         int popped;
@@ -36,20 +28,15 @@ public:
         }
         return popped;
     }
- 
     int minimum()
     {
         int min_elem = s.top().second;
         return min_elem;
     }
 };
- 
-// Driver code
 int main()
 {
     MinStack s;
- 
-    // Function calls
     s.push(-1);
     s.push(10);
     s.push(-4);
