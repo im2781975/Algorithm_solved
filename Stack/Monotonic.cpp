49847719a082dc2f63c75cd70e9c0b18fb345670
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-02-08 07:52:27 +0100

    Update Monotonic.cpp

--- Stack/Monotonic.cpp
+++ Stack/Monotonic.cpp
@@ -34,10 +34,48 @@ void increasingStack(int arr[], int N)
     }
     cout << endl;
 }
+void decreasingStack(int arr[], int N)
+{
+    stack<int> stk;
+    for (int i = 0; i < N; i++) {
+        // Either stack empty or all smaller nums are popped off
+        while (stk.size() > 0 && stk.top() < arr[i]) {
+            stk.pop();
+        }
+        stk.push(arr[i]);
+    }
+ 
+    int N2 = stk.size();
+    int ans[N2] = { 0 };
+    int j = N2 - 1;
+
+    // Empty stack
+    while (!stk.empty()) {
+ 
+        ans[j] = stk.top();
+        stk.pop();
+        j--;
+    }
+    // Displaying the original array
+    cout << "The Array: ";
+    for (int i = 0; i < N; i++) {
+ 
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    // Displaying Monotonic Decreasing Stack
+    cout << "The Stack: ";
+    for (int i = 0; i < N2; i++) {
+ 
+        cout << ans[i] << " ";
+    }
+    cout << endl;
+}
 int main()
 {
     int arr[] = { 1, 4, 5, 3, 12, 10 };
     int N = sizeof(arr) / sizeof(arr[0]);
     increasingStack(arr, N);
+    decreasingStack(arr, N);
     return 0;
 }
