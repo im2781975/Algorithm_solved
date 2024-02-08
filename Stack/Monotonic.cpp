718bba3036507684385c5fe0e52e73311f37345f
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-02-08 07:50:05 +0100

    Update and rename Stack/Section A/Monotonic to Stack/Monotonic.cpp

--- Stack/Monotonic.cpp
+++ Stack/Monotonic.cpp
@@ -1,25 +1,17 @@
-// C++ code to implement the approach
- 
 #include <bits/stdc++.h>
 using namespace std;
- 
-// Function to build Monotonic
-// increasing stack
+// Function to build Monotonic increasing stack
 void increasingStack(int arr[], int N)
 {
     // Initialise stack
     stack<int> stk;
- 
     for (int i = 0; i < N; i++) {
- 
-        // Either stack is empty or
-        // all bigger nums are popped off
+        // Either stack is empty or all bigger nums are popped off
         while (stk.size() > 0 && stk.top() > arr[i]) {
             stk.pop();
         }
         stk.push(arr[i]);
     }
- 
     int N2 = stk.size();
     int ans[N2] = { 0 };
     int j = N2 - 1;
@@ -30,14 +22,11 @@ void increasingStack(int arr[], int N)
         stk.pop();
         j--;
     }
- 
-    // Displaying the original array
     cout << "The Array: ";
     for (int i = 0; i < N; i++) {
         cout << arr[i] << " ";
     }
     cout << endl;
- 
     // Displaying Monotonic increasing stack
     cout << "The Stack: ";
     for (int i = 0; i < N2; i++) {
@@ -45,15 +34,10 @@ void increasingStack(int arr[], int N)
     }
     cout << endl;
 }
- 
-// Driver code
 int main()
 {
     int arr[] = { 1, 4, 5, 3, 12, 10 };
     int N = sizeof(arr) / sizeof(arr[0]);
- 
-    // Function Call
     increasingStack(arr, N);
- 
     return 0;
 }
