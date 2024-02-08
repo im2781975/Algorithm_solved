81f14919736016dc44d74a86d6f70efa6c1ecf1e
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-02-08 06:08:27 +0100

    Update and rename Stack/Section A/Design stack to Stack/Design Stack.cpp

--- Stack/Design Stack.cpp
+++ Stack/Design Stack.cpp
@@ -1,28 +1,20 @@
-
-// C++ program to implement a stack that supports
-// getMinimum() in O(1) time and O(1) extra space.
 #include <bits/stdc++.h>
 using namespace std;
- 
-// A user defined stack that supports getMin() in
-// addition to push() and pop()
+// A user defined stack that supports getMin() in addition to push() and pop()
 struct MyStack {
     stack<int> s;
     int minEle;
- 
     // Prints minimum element of MyStack
     void getMin()
     {
         if (s.empty())
             cout << "Stack is empty\n";
  
-        // variable minEle stores the minimum element
-        // in the stack.
+        // variable minEle stores the minimum element in the stack.
         else
             cout << "Minimum Element in the stack is: "
                  << minEle << "\n";
     }
- 
     // Prints top element of MyStack
     void peek()
     {
@@ -30,16 +22,11 @@ struct MyStack {
             cout << "Stack is empty ";
             return;
         }
- 
-        int t = s.top(); // Top element.
- 
+        int t = s.top(); 
         cout << "Top Most Element is: ";
- 
-        // If t < minEle means minEle stores
-        // value of t.
+        // If t < minEle means minEle stores value of t.
         (t < minEle) ? cout << minEle : cout << t;
     }
- 
     // Remove the top element from MyStack
     void pop()
     {
@@ -47,22 +34,18 @@ struct MyStack {
             cout << "Stack is empty\n";
             return;
         }
- 
         cout << "Top Most Element Removed: ";
         int t = s.top();
         s.pop();
  
-        // Minimum will change as the minimum element
-        // of the stack is being removed.
+        // Minimum will change as the minimum element of the stack is being removed.
         if (t < minEle) {
             cout << minEle << "\n";
             minEle = 2 * minEle - t;
         }
- 
         else
             cout << t << "\n";
     }
- 
     // Removes top element from MyStack
     void push(int x)
     {
@@ -73,7 +56,6 @@ struct MyStack {
             cout << "Number Inserted: " << x << "\n";
             return;
         }
- 
         // If new number is less than minEle
         else if (x < minEle) {
             s.push(2 * x - minEle);
@@ -86,13 +68,9 @@ struct MyStack {
         cout << "Number Inserted: " << x << "\n";
     }
 };
- 
-// Driver Code
 int main()
 {
     MyStack s;
-   
-      // Function calls
     s.push(3);
     s.push(5);
     s.getMin();
