6b696b0b3d25e02a123670bc8d3a1456e83a91ad
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-02-13 16:07:35 +0100

    Update and rename Stack/Section B/Infix to prefix to Stack/Convert To Prefix.cpp

--- Stack/Convert To Prefix.cpp
+++ Stack/Convert To Prefix.cpp
@@ -1,13 +1,11 @@
-// C++ program to convert infix to prefix
+// convert infix to prefix
 #include <bits/stdc++.h>
 using namespace std;
- 
 // Function to check if the character is an operator
 bool isOperator(char c)
 {
     return (!isalpha(c) && !isdigit(c));
 }
- 
 // Function to get the priority of operators
 int getPriority(char C)
 {
@@ -19,7 +17,6 @@ int getPriority(char C)
         return 3;
     return 0;
 }
- 
 // Function to convert the infix expression to postfix
 string infixToPostfix(string infix)
 {
@@ -27,32 +24,23 @@ string infixToPostfix(string infix)
     int l = infix.size();
     stack<char> char_stack;
     string output;
- 
     for (int i = 0; i < l; i++) {
  
-        // If the scanned character is an
-        // operand, add it to output.
+        // If the scanned character is & operand, add it to output.
         if (isalpha(infix[i]) || isdigit(infix[i]))
             output += infix[i];
- 
-        // If the scanned character is an
-        // ‘(‘, push it to the stack.
+        // If the scanned character is an ‘(‘, push it to the stack.
         else if (infix[i] == '(')
             char_stack.push('(');
- 
-        // If the scanned character is an
-        // ‘)’, pop and output from the stack
-        // until an ‘(‘ is encountered.
+        // If the scanned character is an ‘)’, pop and output from the stack until an ‘(‘ is encountered.
         else if (infix[i] == ')') {
             while (char_stack.top() != '(') {
                 output += char_stack.top();
                 char_stack.pop();
             }
- 
             // Remove '(' from the stack
             char_stack.pop();
         }
- 
         // Operator found
         else {
             if (isOperator(char_stack.top())) {
@@ -72,7 +60,6 @@ string infixToPostfix(string infix)
                         char_stack.pop();
                     }
                 }
- 
                 // Push current Operator on stack
                 char_stack.push(infix[i]);
             }
@@ -84,43 +71,29 @@ string infixToPostfix(string infix)
     }
     return output;
 }
- 
-// Function to convert infix to prefix notation
+// convert infix to prefix notation
 string infixToPrefix(string infix)
 {
-    // Reverse String and replace ( with ) and vice versa
-    // Get Postfix
-    // Reverse Postfix
+    // Reverse String and replace ( with ) and vice versa Get Postfix Reverse Postfix
     int l = infix.size();
- 
     // Reverse infix
     reverse(infix.begin(), infix.end());
- 
     // Replace ( with ) and vice versa
     for (int i = 0; i < l; i++) {
  
-        if (infix[i] == '(') {
+        if (infix[i] == '(')
             infix[i] = ')';
-        }
-        else if (infix[i] == ')') {
+        else if (infix[i] == ')')
             infix[i] = '(';
-        }
     }
- 
     string prefix = infixToPostfix(infix);
- 
     // Reverse postfix
     reverse(prefix.begin(), prefix.end());
- 
     return prefix;
 }
- 
-// Driver code
 int main()
 {
     string s = ("x+y*z/w+u");
-   
-    // Function call
     cout << infixToPrefix(s) << std::endl;
     return 0;
 }
