8c0ed5cde8f77d0f0a86d08f6481840b778a9b3d
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-02-13 17:21:35 +0100

    Update and rename Stack/Section C/Check mirror to Execution/Check Mirror.cpp

--- Execution/Check Mirror.cpp
+++ Execution/Check Mirror.cpp
@@ -1,22 +1,14 @@
-// C++ program to check if two n-ary trees are
-// mirror.
+// check if two n-ary trees are mirror.
 #include <bits/stdc++.h>
 using namespace std;
- 
-// Function to check given two trees are mirror
-// of each other or not
-int checkMirrorTree(int M, int N, int u1[ ], 
-                    int v1[ ] , int u2[], int v2[]) 
+// Function to check given two trees are mirror of each other or not
+int checkMirrorTree(int M, int N, int u1[ ], int v1[ ] , int u2[], int v2[]) 
     {
         // Map to store nodes of the tree
         unordered_map<int , stack<int>>mp;
-   
         // Traverse first tree nodes
         for (int i = 0 ; i < N ; i++ )
-        {
-           mp[u1[i]].push(v1[i]);
-        }
-         
+            mp[u1[i]].push(v1[i]);
         // Traverse second tree nodes
         for (int i = 0 ; i < N ; i++)
         {
@@ -24,27 +16,21 @@ int checkMirrorTree(int M, int N, int u1[ ],
                   return 0;
             mp[u2[i]].pop();
         }
-   
         return 1;
     }
- 
-// Driver code
 int main()
 {
     int M = 7, N = 6;
-     
     //Tree 1
     int u1[] = { 1, 1, 1, 10, 10, 10 };
     int v1[] = { 10, 7, 3, 4, 5, 6 };
-   
     //Tree 2
     int u2[] = { 1, 1, 1, 10, 10, 10 };
     int v2[] = { 3, 7, 10, 6, 5, 4 };
  
     if(checkMirrorTree(M, N, u1, v1, u2, v2))
-       cout<<"Yes";
+        cout<<"Yes";
     else
-       cout<<"No";
-   
+        cout<<"No";
     return 0;
 }
