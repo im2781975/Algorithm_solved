8b57375f7822819435679d7a0acef219a31b9ad5
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-02-13 15:53:10 +0100

    Update and rename Evalution postfix to Evalute Postfix.cpp

--- Stack/Execution/Evalute Postfix.cpp
+++ Stack/Execution/Evalute Postfix.cpp
@@ -1,24 +1,17 @@
-// C++ program to evaluate value of a postfix expression
+// evaluate value of a postfix expression
 #include <bits/stdc++.h>
 using namespace std;
- 
-// The main function that returns value 
-// of a given postfix expression
+// returns value of a given postfix expression
 int evaluatePostfix(string exp)
 {
     // Create a stack of capacity equal to expression size
     stack<int> st;
- 
     // Scan all characters one by one
     for (int i = 0; i < exp.size(); ++i) {
-         
-        // If the scanned character is an operand 
-        // (number here), push it to the stack.
+        // If the scanned character is an operand (number here), push it to the stack.
         if (isdigit(exp[i]))
             st.push(exp[i] - '0');
- 
-        // If the scanned character is an operator, 
-        // pop two elements from stack apply the operator
+        // If the scanned character is an operator,pop two elements from stack apply the operator
         else {
             int val1 = st.top();
             st.pop();
@@ -42,13 +35,9 @@ int evaluatePostfix(string exp)
     }
     return st.top();
 }
- 
-// Driver code
 int main()
 {
     string exp = "231*+9-";
-   
-    // Function call
     cout << "postfix evaluation: " << evaluatePostfix(exp);
     return 0;
 }
