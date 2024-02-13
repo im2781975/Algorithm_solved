bee67e603541fcbd7eab6abbc8447015c10038e4
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-02-13 15:38:39 +0100

    Update and rename Stack/Section A/Stack<queue 2> to Stack/Queue 5.cpp

--- Stack/Queue 5.cpp
+++ Stack/Queue 5.cpp
@@ -1,42 +1,31 @@
-// CPP Program to implement a stack
-// using one queue and recursion
+// implement a stack using one queue and recursion
 #include <bits/stdc++.h>
 using namespace std;
- 
 // Stack Class that acts as a queue
 class Stack {
     queue<int> q;
- 
-public:
+    public:
     void push(int data, int c);
     void pop();
     int top();
     int size();
     bool empty();
 };
- 
-// Push operation
 void Stack::push(int data, int c)
 {
-    // Push the current element first and
-    // After every recursion add the front element again
+    // Push the current element first & After every recursion add the front element again
     q.push(data);
- 
     // Return if size becomes 0
     if (c <= 0)
         return;
- 
     // Store current front
     int x = q.front();
- 
     // Delete front element
     q.pop();
- 
     // Decrement size by 1 in every recursion
     c--;
     Stack::push(x, c);
 }
- 
 // Removes the top element
 void Stack::pop()
 {
@@ -45,19 +34,16 @@ void Stack::pop()
     else
         q.pop();
 }
- 
 // Returns top of stack
 int Stack::top() { return (q.empty()) ? -1 : q.front(); }
- 
 // Returns true if Stack is empty else false
 bool Stack::empty() { return (q.empty()); }
- 
 int Stack::size() { return q.size(); }
- 
 int main()
 {
     Stack st;
-    st.push(1, st.size()); // Value and size
+    st.push(1, st.size()); 
+        // Value and size
     st.push(2, st.size());
     st.push(3, st.size());
     cout << "current size: " << st.size() << "\n";
