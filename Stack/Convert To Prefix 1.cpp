d8392b0c7fbb7cd93fed574fb2c7d379c36f5472
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-02-13 16:11:56 +0100

    Update and rename Stack/Section B/Postfix to prefix to Stack/Convert To Prefix 1.cpp

--- Stack/Convert To Prefix 1.cpp
+++ Stack/Convert To Prefix 1.cpp
@@ -1,7 +1,6 @@
-// CPP Program to convert postfix to prefix
+// convert postfix to prefix
 #include <bits/stdc++.h>
 using namespace std;
- 
 // function to check if character is operator or not
 bool isOperator(char x)
 {
@@ -14,42 +13,32 @@ bool isOperator(char x)
     }
     return false;
 }
- 
 // Convert postfix to Prefix expression
 string postToPre(string post_exp)
 {
     stack<string> s;
- 
     // length of expression
     int length = post_exp.size();
- 
     // reading from left to right
     for (int i = 0; i < length; i++) {
- 
         // check if symbol is operator
         if (isOperator(post_exp[i])) {
- 
             // pop two operands from stack
             string op1 = s.top();
             s.pop();
             string op2 = s.top();
             s.pop();
- 
             // concat the operands and operator
             string temp = post_exp[i] + op2 + op1;
- 
             // Push string temp back to stack
             s.push(temp);
         }
- 
         // if symbol is an operand
         else {
- 
             // push the operand to the stack
             s.push(string(1, post_exp[i]));
         }
     }
- 
     string ans = "";
     while (!s.empty()) {
         ans += s.top();
@@ -57,13 +46,9 @@ string postToPre(string post_exp)
     }
     return ans;
 }
- 
-// Driver Code
 int main()
 {
     string post_exp = "ABC/-AK/L-*";
- 
-    // Function call
     cout << "Prefix : " << postToPre(post_exp);
     return 0;
 }
