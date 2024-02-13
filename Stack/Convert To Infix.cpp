e6c8f249d76b49af90f6d1c82e70e6ebf8093da7
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-02-13 16:09:58 +0100

    Update and rename Stack/Section B/Postfix to infix to Stack/Convert To Infix.cpp

--- Stack/Convert To Infix.cpp
+++ Stack/Convert To Infix.cpp
@@ -1,20 +1,15 @@
-// CPP program to find infix for
-// a given postfix.
+// find infix for a given postfix.
 #include <bits/stdc++.h>
 using namespace std;
- 
 bool isOperand(char x)
 {
    return (x >= 'a' && x <= 'z') ||
           (x >= 'A' && x <= 'Z');
 }
- 
-// Get Infix for a given postfix
-// expression
+// Get Infix for a given postfix expression
 string getInfix(string exp)
 {
     stack<string> s;
- 
     for (int i=0; exp[i]!='\0'; i++)
     {
         // Push operands
@@ -23,10 +18,7 @@ string getInfix(string exp)
            string op(1, exp[i]);
            s.push(op);
         }
- 
-        // We assume that input is
-        // a valid postfix and expect
-        // an operator.
+        // We assume that input is a valid postfix and expect an operator.
         else
         {
             string op1 = s.top();
@@ -37,14 +29,9 @@ string getInfix(string exp)
                    op1 + ")");
         }
     }
- 
-    // There must be a single element
-    // in stack now which is the required
-    // infix.
+    // There must be a single element in stack now which is the required infix.
     return s.top();
 }
- 
-// Driver code
 int main()
 {
     string exp = "ab*c+";
