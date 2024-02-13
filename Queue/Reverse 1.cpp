4e4aeab72093e8484cf52c746c72258d1017c632
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-02-13 19:38:13 +0100

    Update and rename Stack/Section C/Rev value to Queue/Reverse 1.cpp

--- Queue/Reverse 1.cpp
+++ Queue/Reverse 1.cpp
@@ -1,42 +1,31 @@
-// C++ program to reverse first
-// k elements of a queue.
+// reverse first k elements of a queue.
 #include <bits/stdc++.h>
 using namespace std;
- 
-/* Function to reverse the first
-   K elements of the Queue */
+// Function to reverse the first K elements of the Queue
 void reverseQueueFirstKElements(int k, queue<int>& Queue)
 {
     if (Queue.empty() == true || k > Queue.size())
         return;
     if (k <= 0)
         return;
- 
     stack<int> Stack;
  
-    /* Push the first K elements
-       into a Stack*/
+    //Push the first K elements into a Stack
     for (int i = 0; i < k; i++) {
         Stack.push(Queue.front());
         Queue.pop();
     }
- 
-    /* Enqueue the contents of stack
-       at the back of the queue*/
+    // Enqueue the contents of stack at the back of the queue
     while (!Stack.empty()) {
         Queue.push(Stack.top());
         Stack.pop();
     }
- 
-    /* Remove the remaining elements and
-       enqueue them at the end of the Queue*/
+    // Remove the remaining elements & enqueue them at the end of the Queue
     for (int i = 0; i < Queue.size() - k; i++) {
         Queue.push(Queue.front());
         Queue.pop();
     }
 }
- 
-/* Utility Function to print the Queue */
 void Print(queue<int>& Queue)
 {
     while (!Queue.empty()) {
@@ -44,8 +33,6 @@ void Print(queue<int>& Queue)
         Queue.pop();
     }
 }
- 
-// Driver code
 int main()
 {
     queue<int> Queue;
