85f5a8a0a32f5701b3336f0f46af671065ad11be
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-02-13 15:11:18 +0100

    Create Display 3.cpp

---
+++ Queue/Display 3.cpp
@@ -0,0 +1,39 @@
+// implement Queue using one stack and recursive call stack.
+#include <bits/stdc++.h>
+using namespace std;
+struct Queue {
+    stack<int> s;
+    void enQueue(int x)
+    {
+        s.push(x);
+    }
+    // Dequeue an item from the queue
+    int deQueue()
+    {
+        if (s.empty())
+            return -1;
+        // pop an item from the stack
+        int x = s.top();
+        s.pop();
+        // if stack becomes empty, return the popped item
+        if (s.empty())
+            return x;
+        // recursive call
+        int item = deQueue();
+        // push popped item back to the stack
+        s.push(x);
+        // return the result of deQueue() call
+        return item;
+    }
+};
+int main()
+{
+    Queue q;
+    q.enQueue(1);
+    q.enQueue(2);
+    q.enQueue(3);
+    cout << q.deQueue() << '\n';
+    cout << q.deQueue() << '\n';
+    cout << q.deQueue() << '\n';
+    return 0;
+}
