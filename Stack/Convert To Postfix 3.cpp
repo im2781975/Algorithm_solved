4d081837e6701f9bfd155c7bd169a0511686e0df
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-02-13 17:16:51 +0100

    Update and rename Stack/Section B/infix to postfix to Stack/Convert To Postfix 3.cpp

--- Stack/Convert To Postfix 3.cpp
+++ Stack/Convert To Postfix 3.cpp
@@ -1,8 +1,6 @@
-// C++ code to convert infix expression to postfix
- 
+// convert infix expression to postfix
 #include <bits/stdc++.h>
 using namespace std;
- 
 // Function to return precedence of operators
 int prec(char c)
 {
@@ -15,32 +13,22 @@ int prec(char c)
     else
         return -1;
 }
- 
-// The main function to convert infix expression
-// to postfix expression
+// convert infix expression to postfix expression
 void infixToPostfix(string s)
 {
- 
     stack<char> st;
     string result;
  
     for (int i = 0; i < s.length(); i++) {
         char c = s[i];
- 
-        // If the scanned character is
-        // an operand, add it to output string.
+        // If the scanned character is an operand, add it to output string.
         if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
             || (c >= '0' && c <= '9'))
             result += c;
- 
-        // If the scanned character is an
-        // ‘(‘, push it to the stack.
+        // If the scanned character is an ‘(‘, push it to the stack.
         else if (c == '(')
             st.push('(');
- 
-        // If the scanned character is an ‘)’,
-        // pop and add to output string from the stack
-        // until an ‘(‘ is encountered.
+        // If the scanned character is an ‘)’, pop and add to output string from the stack until an ‘(‘ is encountered.
         else if (c == ')') {
             while (st.top() != '(') {
                 result += st.top();
@@ -48,7 +36,6 @@ void infixToPostfix(string s)
             }
             st.pop();
         }
- 
         // If an operator is scanned
         else {
             while (!st.empty()
@@ -59,22 +46,16 @@ void infixToPostfix(string s)
             st.push(c);
         }
     }
- 
     // Pop all the remaining elements from the stack
     while (!st.empty()) {
         result += st.top();
         st.pop();
     }
- 
     cout << result << endl;
 }
- 
-// Driver code
 int main()
 {
     string exp = "a+b*(c^d-e)^(f+g*h)-i";
- 
-    // Function call
     infixToPostfix(exp);
    
     return 0;
