cd74a4976ea69d313457c562c30ed5dd93aaa775
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-02-13 15:56:04 +0100

    Update and rename Evalution postfix<> to Evalute Postfix 1.cpp

--- Stack/Execution/Evalute Postfix 1.cpp
+++ Stack/Execution/Evalute Postfix 1.cpp
@@ -1,42 +1,31 @@
-// C++ program to evaluate value of a postfix
-// expression having multiple digit operands
+// evaluate value of a postfix expression having multiple digit operands
 #include <bits/stdc++.h>
 using namespace std;
- 
-// The main function that returns value
-// of a given postfix expression
+// returns value of a given postfix expression
 int evaluatePostfix(char* exp)
 {
     // Create a stack of capacity equal to expression size
     stack<int> st;
     int i;
- 
     // Scan all characters one by one
-    for (i = 0; exp[i]; ++i) {
-         
+    for (i = 0; exp[i]; ++i){
         // If the character is blank space then continue
         if (exp[i] == ' ')
             continue;
- 
-        // If the scanned character is an
-        // operand (number here),extract the full number
+        // If the scanned character is an operand (number here),extract the full number
         // Push it to the stack.
         else if (isdigit(exp[i])) {
             int num = 0;
- 
             // Extract full number
             while (isdigit(exp[i])) {
                 num = num * 10 + (int)(exp[i] - '0');
                 i++;
             }
             i--;
- 
             // Push the element in the stack
             st.push(num);
         }
- 
-        // If the scanned character is an operator, pop two
-        // elements from stack apply the operator
+        // If the scanned character is an operator, pop two elements from stack apply the operator
         else {
             int val1 = st.top();
             st.pop();
@@ -61,13 +50,9 @@ int evaluatePostfix(char* exp)
     }
     return st.top();
 }
- 
-// Driver code
 int main()
 {
     char exp[] = "100 200 + 2 / 5 * 7 +";
-   
-    // Function call
     cout << evaluatePostfix(exp);
     return 0;
 }
