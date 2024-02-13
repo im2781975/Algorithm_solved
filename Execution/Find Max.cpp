4a245ce5c3ecdfd0828128d58492be0eb5204127
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-02-13 18:04:54 +0100

    Update Find Max.cpp

--- Execution/Find Max.cpp
+++ Execution/Find Max.cpp
@@ -55,6 +55,28 @@ void printMaxOfMin(int arr[], int n)
     for (int i = 1; i <= n; i++)
         cout << ans[i] << " ";
 }
+void printMaxOfMin(int arr[], int n)
+{
+    // Consider all windows of different sizes starting from size 1
+    for (int k = 1; k <= n; k++) {
+        // Initialize max of min for current window size k
+        int maxOfMin = INT_MIN;
+        // Traverse through all windows of current size k
+        for (int i = 0; i <= n - k; i++) {
+            // Find minimum of current window
+            int min = arr[i];
+            for (int j = 1; j < k; j++) {
+                if (arr[i + j] < min)
+                    min = arr[i + j];
+            }
+            // Update maxOfMin if required
+            if (min > maxOfMin)
+                maxOfMin = min;
+        }
+        // Print max of min for current window size
+        cout << maxOfMin << " ";
+    }
+}
 int main()
 {
     int arr[] = { 10, 20, 30, 50, 10, 70, 30 };
