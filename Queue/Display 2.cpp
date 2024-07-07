43315870ba06f8b504a864abfd68d058296183a0
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-07-07 23:08:25 +0200

    Update and rename Display 3.cpp to Display 2.cpp

--- Queue/Display 2.cpp
+++ Queue/Display 2.cpp
@@ -21,6 +21,26 @@ struct Queue{
         return item;
     }
 };
+// implement Queue using two stacks with costly deQueue()
+struct Queue{
+    stack <int> a, b;
+    void enque(int x){
+        a.push(x);
+    }
+    int deque(){
+        if(a.empty() && b.empty())
+            return -1;
+        if(b.empty()){
+            while(!a.empty()){
+                b.push(a.top());
+                a.pop();
+            }
+        }
+        int x = b.top();
+        b.pop();
+        return x;
+    }
+};
 int main(){
     Queue q;
     q.enqueue(1);
@@ -29,4 +49,11 @@ int main(){
     cout << q.dequeue() << '\n';
     cout << q.dequeue() << '\n';
     cout << q.dequeue() << '\n';
+    ///
+    Queue q;
+    cout << q.deque() << "\n";
+    for(int i = 1; i <= 5; i++){
+        q.enque(i);
+        cout << q.deque() << " ";
+    }
 }
