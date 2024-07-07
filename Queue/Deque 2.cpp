976f6d01bec87f5ac8d5edd124d02a2809c758f9
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-07-07 08:56:18 +0200

    Update Deque 2.cpp

--- Queue/Deque 2.cpp
+++ Queue/Deque 2.cpp
@@ -1,166 +1,125 @@
 // implement stack and queue using Deque
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-// structure for a node of deque
-struct DQueNode {
-    int value;
-    DQueNode* next;
-    DQueNode* prev;
+struct node{
+    int data;
+    node *nxt, *prv;
 };
-// Implementation of deque class
-class Deque {
-    private:
-    // pointers to head and tail of deque
-    DQueNode* head;
-    DQueNode* tail;
+struct Deque{
+    node *head, *tail;
     public:
-    Deque()
-    {
+    Deque(){
         head = tail = NULL;
     }
-    // if list is empty
-    bool isEmpty()
-    {
-        if (head == NULL)
+    bool IsEmpty(){
+        if(head == NULL)
             return true;
         return false;
     }
-    // count the number of nodes in list
-    int size()
-    {
-        // if list is not empty
-        if (!isEmpty()) {
-            DQueNode* temp = head;
+    int size(){
+        if(!IsEmpty()){
             int len = 0;
-            while (temp != NULL) {
+            node *tmp = head;
+            while(tmp!=NULL){
                 len++;
-                temp = temp->next;
+                tmp = tmp->nxt;
             }
             return len;
         }
         return 0;
     }
-    // insert at the first position
-    void insert_first(int element)
-    {
-        // allocating node of DQueNode type
-        DQueNode* temp = new DQueNode[sizeof(DQueNode)];
-        temp->value = element;
-        // if the element is first element
-        if (head == NULL) {
-            head = tail = temp;
-            temp->next = temp->prev = NULL;
-        }
-        else {
-            head->prev = temp;
-            temp->next = head;
-            temp->prev = NULL;
-            head = temp;
+    void InsertFirst(int val){
+        node *newnode = new node;
+        newnode->data = val;
+        if(head == NULL)
+            head = tail = newnode;
+        else{
+            head ->prv = newnode;
+            newnode->nxt = head;
+            newnode->prv = NULL;
+            head = newnode;
         }
     }
-    // insert at last position of deque
-    void insert_last(int element)
-    {
-        // allocating node of DQueNode type
-        DQueNode* temp = new DQueNode[sizeof(DQueNode)];
-        temp->value = element;
-        // if element is the first element
-        if (head == NULL) {
-            head = tail = temp;
-            temp->next = temp->prev = NULL;
-        }
-        else {
-            tail->next = temp;
-            temp->next = NULL;
-            temp->prev = tail;
-            tail = temp;
+    void InsertLast(int val){
+        node *newnode = new node;
+        newnode->data = val;
+        if(head == NULL)
+            head = tail = newnode;
+        else{
+            newnode->prv = tail;
+            tail->nxt = newnode;
+            newnode->nxt = NULL;
+            tail = newnode;
         }
     }
-    // remove element at the first position
-    void remove_first()
-    {
-        // if list is not empty
-        if (!isEmpty()) {
-            DQueNode* temp = head;
-            head = head->next;
-            if(head) head->prev = NULL;
-            delete temp;
-            if(head == NULL) tail = NULL;
+    void RemoveFirst(){
+        if(!IsEmpty()){
+            node *tmp = head;
+            head = head->nxt;
+            if(head != NULL)
+                head->prv = NULL;
+            delete tmp;
+            if(head == NULL)
+                tail = NULL;
             return;
         }
-        cout << "List is Empty" << endl;
+        cout << "\nList Is Empty";
     }
-    // remove element at the last position
-    void remove_last()
-    {
-        // if list is not empty
-        if (!isEmpty()) {
-            DQueNode* temp = tail;
-            tail = tail->prev;
-            if(tail) tail->next = NULL;
-            delete temp;
-            if(tail == NULL) head = NULL;
+    void RemoveLast(){
+        if(!IsEmpty()){
+            node *tmp = tail;
+            tail = tail->prv;
+            if(tail !=NULL)
+                tail->nxt = NULL;
+            delete tmp;
+            if(tail == NULL)
+                head = NULL;
             return;
         }
-        cout << "List is Empty" << endl;
     }
-    // displays the elements in deque
-    void display()
-    {
-        // if list is not empty
-        if (!isEmpty()) {
-            DQueNode* temp = head;
-            while (temp != NULL) {
-                cout << temp->value << " ";
-                temp = temp->next;
+    void Display(){
+        if(!IsEmpty()){
+            node *tmp = head;
+            while(tmp!=NULL){
+                cout << tmp->data << " ";
+                tmp = tmp->nxt;
             }
-            cout << endl;
-            return;
+            cout << "\n";
         }
-        cout << "List is Empty" << endl;
+        else
+            cout << "List Is Empty";
     }
 };
-// Class to implement stack using Deque
-class Stack : public Deque {
+class Stack :public Deque{
     public:
-    void push(int element)
-        insert_last(element);
-    void pop()
-        remove_last();
+    void push(int val){
+        InsertLast(val);
+    }
+    void pop(){
+        RemoveLast();
+    }
 };
-// class to implement queue using deque
-class Queue : public Deque {
+class Queue :public Deque{
     public:
-    void enqueue(int element)
-        insert_last(element);
-    void dequeue()
-        remove_first();
+    void enqueue(int val){
+        InsertLast(val);
+    }
+    void Dequeue(){
+        RemoveFirst();
+    }
 };
-int main()
-{
-    Stack stk;
-
-    stk.push(7);
-    stk.push(8);
-    cout << "\nStack: ";
-    stk.display();
- 
-    stk.pop();
-    cout << "\nStack: ";
-    stk.display();
-    // object of Queue
-    Queue que;
- 
-    que.enqueue(12);
-    que.enqueue(13);
-    cout << "\nQueue: ";
-    que.display();
- 
-    que.dequeue();
-    cout << "\nQueue: ";
-    que.display();
- 
-    cout << "\nSize of Stack is " << stk.size() ;
-    cout << "\nSize of Queue is " << que.size() ;
-    return 0;
+int main(){
+    Stack st;
+    for(int i = 1; i < 6; i++)
+        st.push(i);
+    st.pop();
+    st.Display();
+    Queue Q;
+    for(int i = 5; i < 11; i++)
+        Q.enqueue(i);
+    Q.Dequeue();
+    Q.Display();
+    cout << "\nStack size: " << st.size();
+    cout << "\nQueue size: " << Q.size();
 }
+
