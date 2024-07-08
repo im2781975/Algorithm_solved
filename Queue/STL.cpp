040e9f5fb92ab2f324a251b99cb5b4681677e9b9
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-07-08 08:22:08 +0200

    Update STL.cpp

--- Queue/STL.cpp
+++ Queue/STL.cpp
@@ -1,37 +1,27 @@
-#include <iostream>
-#include <queue>
+#include<iostream>
+#include<queue>
 using namespace std;
-void print_queue(queue<int> q)
+void print(queue <int> q)
 {
-    queue<int> temp = q;
-    while (!temp.empty()) {
-        cout << temp.front()<<" ";
-        temp.pop();
+    queue <int> tmp = q;
+    while(!tmp.empty()){
+        cout << tmp.front() << " ";
+        tmp.pop();
     }
-    cout << '\n';
+    cout << "\n";
 }
-int main()
-{
-    queue<int> q1;
-    q1.push(1);
-    q1.push(2);
-    q1.push(3);
-    cout << "The first queue is : ";
-    print_queue(q1);
-   
-     queue<int> q2;
-    q2.push(4);
-    q2.push(5);
-    q2.push(6);
- 
-    cout << "The second queue is : ";
-    print_queue(q2);
-    q1.swap(q2);
-    cout << "After swapping, the first queue is : ";
-    print_queue(q1);
-      cout << "After swapping the second queue is : ";
-    print_queue(q2);
-   
-    cout<<q1.empty(); 
-    return 0;
+int main(){
+    queue <int> a, b;
+    for(int i =0; i < 10; i+= 2)
+        a.push(i);
+    cout << "Befor Swap: \n";
+    print(a);
+    for(int i = 10; i < 20; i+= 2)
+        b.push(i);
+    print(b);
+    cout << "\nAfter Swap: \n";
+    a.swap(b);
+    print(a);
+    print(b);
+    cout << "Is empty: " << a.empty();
 }
