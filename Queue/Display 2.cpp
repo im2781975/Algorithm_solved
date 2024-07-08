68a4232ffe14127ca40c383fe6a7046557264ac2
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-07-08 12:28:39 +0200

    Update Display 2.cpp

--- Queue/Display 2.cpp
+++ Queue/Display 2.cpp
@@ -41,6 +41,16 @@ struct Queue{
         return x;
     }
 };
+// 
+void print(queue <int> q)
+{
+    queue <int> tmp = q;
+    while(!tmp.empty()){
+        cout << tmp.front() << " ";
+        tmp.pop();
+    }
+    cout << "\n";
+}
 int main(){
     Queue q;
     q.enqueue(1);
@@ -56,4 +66,18 @@ int main(){
         q.enque(i);
         cout << q.deque() << " ";
     }
+    ////
+    queue <int> a, b;
+    for(int i =0; i < 10; i+= 2)
+        a.push(i);
+    cout << "Befor Swap: \n";
+    print(a);
+    for(int i = 10; i < 20; i+= 2)
+        b.push(i);
+    print(b);
+    cout << "\nAfter Swap: \n";
+    a.swap(b);
+    print(a);
+    print(b);
+    cout << "Is empty: " << a.empty();
 }
