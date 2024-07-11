7e8d02eb08d611eeb7067c9ffdcb8bdde1e1cd11
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-07-11 10:33:32 +0200

    Update Monotonic.cpp

--- Stack/Monotonic.cpp
+++ Stack/Monotonic.cpp
@@ -1,81 +1,50 @@
-#include <bits/stdc++.h>
+#include<iostream>
+#include<stack>
 using namespace std;
 // Function to build Monotonic increasing stack
-void increasingStack(int arr[], int N)
-{
-    // Initialise stack
-    stack<int> stk;
-    for (int i = 0; i < N; i++) {
-        // Either stack is empty or all bigger nums are popped off
-        while (stk.size() > 0 && stk.top() > arr[i]) {
-            stk.pop();
-        }
-        stk.push(arr[i]);
+void Increasing(int *arr, int n){
+    stack <int> st;
+    for(int i = 0; i < n; i++){
+        while(st.size() > 0 && st.top() > arr[i])
+            st.pop();
+        st.push(arr[i]);
     }
-    int N2 = stk.size();
-    int ans[N2] = { 0 };
-    int j = N2 - 1;
- 
-    // Empty Stack
-    while (!stk.empty()) {
-        ans[j] = stk.top();
-        stk.pop();
-        j--;
+    int sz = st.size();
+    int ans[sz], j = sz - 1;
+    while(!st.empty()){
+        ans[j] = st.top();
+        st.pop(); j--;
     }
-    cout << "The Array: ";
-    for (int i = 0; i < N; i++) {
+    cout << "\nArray is: ";
+    for(int i = 0; i < n; i++)
         cout << arr[i] << " ";
-    }
-    cout << endl;
-    // Displaying Monotonic increasing stack
-    cout << "The Stack: ";
-    for (int i = 0; i < N2; i++) {
+    cout << "\nStack is: ";
+    for(int i = 0; i < sz; i++)
         cout << ans[i] << " ";
-    }
-    cout << endl;
 }
-void decreasingStack(int arr[], int N)
-{
-    stack<int> stk;
-    for (int i = 0; i < N; i++) {
-        // Either stack empty or all smaller nums are popped off
-        while (stk.size() > 0 && stk.top() < arr[i]) {
-            stk.pop();
-        }
-        stk.push(arr[i]);
+void Decreasing(int *arr, int n){
+    stack <int> st;
+    for(int i = 0; i < n; i++){
+        while(st.size() > 0 && st.top() < arr[i])
+            st.pop();
+        st.push(arr[i]);
     }
- 
-    int N2 = stk.size();
-    int ans[N2] = { 0 };
-    int j = N2 - 1;
-
-    // Empty stack
-    while (!stk.empty()) {
- 
-        ans[j] = stk.top();
-        stk.pop();
-        j--;
+    int sz = st.size();
+    int ans[sz], j = sz - 1;
+    while(!st.empty()){
+        ans[j] = st.top();
+        st.pop(); j--;
     }
-    // Displaying the original array
-    cout << "The Array: ";
-    for (int i = 0; i < N; i++) {
- 
+    cout << "\nArray is: ";
+    for(int i = 0; i < n; i++)
         cout << arr[i] << " ";
-    }
-    cout << endl;
-    // Displaying Monotonic Decreasing Stack
-    cout << "The Stack: ";
-    for (int i = 0; i < N2; i++) {
- 
+    cout << "\nStack is: ";
+    for(int i = 0; i < sz; i++)
         cout << ans[i] << " ";
-    }
-    cout << endl;
 }
-int main()
-{
-    int arr[] = { 1, 4, 5, 3, 12, 10 };
-    int N = sizeof(arr) / sizeof(arr[0]);
-    increasingStack(arr, N);
-    decreasingStack(arr, N);
-    return 0;
+int main(){
+    int arr[]{1, 4, 5, 3, 12, 10};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    Increasing(arr, n);
+    Decreasing(arr, n);
 }
