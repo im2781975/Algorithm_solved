c1d6b66dc52ed6a636fc4bbfa8ef5cd4414443fd
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-07-11 18:37:44 +0200

    Update Queue 3.cpp

--- Stack/Queue 3.cpp
+++ Stack/Queue 3.cpp
@@ -1,7 +1,41 @@
 // implement a stack using two queue
-#include<iostream>
-#include<queue>
+#include <iostream>
+#include <queue>
 using namespace std;
+class Queue{
+    queue <int > a, b;
+    public:
+    void pop(){
+        if(a.empty())
+            return;
+        if(a.size() !=1){
+            b.push(a.front());
+            a.pop();
+        }
+        // Pop the only left element from a
+        a.pop();
+        queue <int> q = a;
+        a = b; b = q;
+    }
+    void push(int x){
+        a.push(x);
+    }
+    int top(){
+        if(a.empty())
+            return -1;
+        if(a.size() !=1){
+            b.push(a.front());
+            a.pop();
+        }
+        int tmp = a.front();
+        a.pop();
+        b.push(tmp);
+        queue <int> q = a;
+        a = b; b = q;
+        return tmp;
+    }
+};
+//Another Implementation
 class Queue{
     queue <int> a, b;
     public:
@@ -36,4 +70,3 @@ int main(){
         q.pop();
     }
 }
-
