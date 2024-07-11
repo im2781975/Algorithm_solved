91e080660aea7ad65f179ab1d16bb3302f61a54e
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-07-11 16:01:05 +0200

    Update Display.cpp

--- Stack/Display.cpp
+++ Stack/Display.cpp
@@ -1,71 +1,56 @@
-#include <bits/stdc++.h> 
-using namespace std; 
-#define MAX 1000 
-  
-class Stack { 
-    int top; 
-  
-    public: 
-    int a[MAX];
-    Stack() { top = -1; } 
-    bool push(int x); 
-    int pop(); 
-    int peek(); 
-    bool isEmpty(); 
-}; 
-bool Stack::push(int x) 
-{ 
-    if (top >= (MAX - 1)) { 
-        cout << "Stack Overflow"; 
-        return false; 
-    } 
-    else { 
-        a[++top] = x; 
-        cout << x << " pushed into stack\n"; 
-        return true; 
-    } 
+#include<iostream>
+#include<stack>
+using namespace std;
+#define Max 1000
+class Stack{
+    int top;
+    public:
+    int arr[Max];
+    Stack():top(-1){}
+    bool push(int x);
+    int pop();
+    int peek();
+    bool IsEmpty();
+};
+bool Stack::push(int x){
+    if(top >= Max -1){
+        cout << "Underflow";
+        return false;
+    }
+    else{
+        arr[++top] = x;
+        //cout << x << " ";
+        return true;
+    }
 }
-int Stack::pop() 
-{ 
-    if (top < 0) { 
-        cout << "Stack Underflow"; 
-        return 0; 
-    } 
-    else { 
-        int x = a[top--]; 
-        return x; 
-    } 
-} 
-int Stack::peek() 
-{ 
-    if (top < 0) { 
-        cout << "Stack is Empty"; 
-        return 0; 
-    } 
-    else { 
-        int x = a[top]; 
-        return x; 
-    } 
-} 
-  
-bool Stack::isEmpty() 
-{ 
-    return (top < 0); 
+int Stack::pop(){
+    if(top < 0){
+        cout << "Underflow";
+        return 0;
+    }
+    int x = arr[top--];
+    return x;
 }
-int main() 
-{ 
-    class Stack s; 
-    s.push(10); 
-    s.push(20); 
-    s.push(30); 
-    cout << s.pop() << " Popped from stack\n"; 
-    
-    cout << "Top element is : " << s.peek() << endl;
-    cout <<"Elements present in stack : "; 
-    while(!s.isEmpty()) 
-    { 
-        cout << s.peek() <<" "; 
-        s.pop(); 
-    } 
-    return 0; 
+int Stack::peek(){
+    if(top < 0){
+        cout << "\nUnderflow";
+        return 0;
+    }
+    else
+        return arr[top];
+}
+bool Stack::IsEmpty(){
+    if(top < 0)
+        return true;
+    return false;
+}
+int main(){
+    Stack st;
+    for(int i = 1; i <= 6; i++)
+        st.push(i);
+    cout << "\nTop Element is: " << st.peek();
+    cout << "\nElements are: ";
+    while(!st.IsEmpty()){
+        cout << st.pop() << " ";
+    }
 }
