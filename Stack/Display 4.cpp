019ecbb6da80ff5a474c0eb8c79128ceeda168e7
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-07-11 16:34:18 +0200

    Update Display 4.cpp

--- Stack/Display 4.cpp
+++ Stack/Display 4.cpp
@@ -1,112 +1,64 @@
 // Implement a stack using singly linked list
-#include <bits/stdc++.h>
+#include<iostream>
+#include<stack>
 using namespace std;
-class Node {
-public:
-    int data;
-    Node* link;
-    Node(int n)
-    {
-        this->data = n;
-        this->link = NULL;
-    }
+class node{
+    public:
+    int data; node *nxt;
+    node(int val):data(val), nxt(NULL){}
 };
-class Stack {
-    Node* top;
- 
-public:
-    Stack() { top = NULL; }
- 
-    void push(int data)
-    {
-        // Create new node temp and allocate memory in heap
-        Node* temp = new Node(data);
-        // Check if stack (heap) is full. Then inserting an element would lead to stack overflow
-        if (!temp) {
-            cout << "\nStack Overflow";
+class Stack{
+    node *top;
+    public:
+    Stack():top(NULL){}
+    void push(int x){
+        node *newnode = new node(x);
+        if(newnode == NULL){
+            cout << "Stack overflow";
             exit(1);
         }
-        // Initialize data into temp data field
-        temp->data = data;
-        // Put top pointer reference into temp link
-        temp->link = top;
-        // Make temp as top of Stack
-        top = temp;
+        newnode->data = x;
+        newnode->nxt = top;
+        top = newnode;
     }
-    // check if the stack is empty or not
-    bool isEmpty()
-    {
-        // If top is NULL it means that there are no elements are in stack
+    bool IsEmpty(){
         return top == NULL;
     }
-    // return top element in a stack
-    int peek()
-    {
-        // If stack is not empty , return the top element
-        if (!isEmpty())
+    int peek(){
+        if(!IsEmpty())
             return top->data;
-        else
-            exit(1);
+        exit(1);
     }
-    // Function to remove a key from given queue q
-    void pop()
-    {
-        Node* temp;
-        // Check for stack underflow
-        if (top == NULL) {
-            cout << "\nStack Underflow" << endl;
+    void pop(){
+        node *tmp = top;
+        if(top == NULL){
+            cout << "\nUnderflow";
             exit(1);
         }
-        else {
-            // Assign top to temp
-            temp = top;
-            // Assign second node to top
-            top = top->link;
- 
-            // This will automatically destroy the link between first node and second node Release memory of top node
-            free(temp);
-        }
+        top = top->nxt;
+        delete tmp;
     }
-    void display()
-    {
-        Node* temp;
-        // Check for stack underflow
-        if (top == NULL) {
-            cout << "\nStack Underflow";
+    void Display(){
+        node *tmp = top;
+        if(top == NULL){
+            cout << "\nUnderflow";
             exit(1);
         }
-        else {
-            temp = top;
-            while (temp != NULL) {
- 
-                // Print node data
-                cout << temp->data;
-                // Assign temp link to temp
-                temp = temp->link;
-                if (temp != NULL)
-                    cout << " -> ";
-            }
+        cout << "\nElements are: ";
+        while(tmp!=NULL){
+            cout << tmp->data << " ";
+            tmp = tmp->nxt;
         }
     }
 };
-int main()
-{
-    Stack s;
-    // Push the elements of stack
-    s.push(11);
-    s.push(22);
-    s.push(33);
-    s.push(44);
-    // Display stack elements
-    s.display();
-    cout << "\nTop element is " << s.peek() << endl;
-    // Delete top elements of stack
-    s.pop();
-    s.pop();
-    s.display();
- 
-    // Print top element of stack
-    cout << "\nTop element is " << s.peek() << endl;
- 
-    return 0;
+int main(){
+    Stack st;
+    for(int i = 1; i <= 10; i++)
+        st.push(i);
+    st.Display();
+    cout << "\nElements are: ";
+    while(!st.IsEmpty()){
+        cout << st.peek() << " ";
+        st.pop();
+    }
 }
