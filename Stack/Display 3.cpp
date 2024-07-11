f1eb60c65c25cd38d0edb6b76035786350858bfb
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-07-11 16:44:35 +0200

    Update Display 3.cpp

--- Stack/Display 3.cpp
+++ Stack/Display 3.cpp
@@ -1,20 +1,12 @@
-#include <iostream> 
-#include <stack>
+#include<iostream>
+#include<stack>
 using namespace std;
-int main() {
-    stack<int> stack;
-    stack.push(21);
-    stack.push(22);
-    stack.push(24);
-    stack.push(25);
-    int num=0;
-      stack.push(num);
-    stack.pop();
-    stack.pop();
-      stack.pop();
-   
-    while (!stack.empty()) {
-        cout << stack.top() <<" ";
-        stack.pop();
+int main(){
+    stack <int> st;
+    for(int i = 21; i <= 30; i++)
+        st.push(i);
+    while(!st.empty()){
+        cout << st.top() << " ";
+        st.pop();
     }
 }
