8f0c7c005e264b682c2a06b148ef806f6771b73d
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-07-11 09:18:34 +0200

    Update Queue 7.cpp

--- Stack/Queue 7.cpp
+++ Stack/Queue 7.cpp
@@ -1,84 +1,49 @@
-#include <bits/stdc++.h>
+#include<iostream>
+#include<stack>
 using namespace std;
-class Stack {
-    // Indices to keep track of the front, rear and size of the queue
-    int front, rear, size;
-    // Maximum capacity of the queue
-    unsigned capacity;
-    // Pointer to the array used to store the elements
-    int* arr;
+class Stack{
+    int front, rear, cap, size, *arr;
     public:
-    Stack(unsigned capacity)
-    {
-        this->capacity = capacity;
-        // Initially, front index and size are set to 0
+    Stack(int cap){
+        this->cap = cap;
+        arr = new int[cap];
         front = size = 0;
-        // Rear index is set to the last index of the array
-        rear = capacity - 1;
-        // Dynamic allocation of memory for the array
-        arr = new int[this->capacity];
+        rear = cap -1;
     }
-    bool isFull()
-    {
-        // If size is equal to the capacity, the queue is full
-        return (size == capacity);
+    bool IsFull(){
+        return (size == cap);
     }
-    bool isEmpty()
-    {
-        // If size is 0, the queue is empty
+    bool IsEmpty(){
         return (size == 0);
     }
-    void push(int x)
-    {
-        if (isFull())
-            // If the queue is full, return without adding the element
+    void push(int x){
+        if(IsFull())
             return;
-        // Increase the rear index by 1 (with wraparound)
-        rear = (rear + 1) % capacity;
-        // Add the element to the rear position
+        rear = (rear + 1) % cap;
         arr[rear] = x;
-        // Increase the size of the queue by 1
         size++;
     }
-    void pop()
-    {
-        if (isEmpty())
+    void pop(){
+        if(IsEmpty())
             return;
-            // If the queue is empty, return without doing anything
-        // Increase the front index by 1 (with wraparound)
-        front = (front + 1) % capacity;
-        // Decrease the size of the queue by 1
+        front = (front + 1) %cap;
         size--;
     }
-    int top()
-    {
-        if (isEmpty())
-            // If the queue is empty, return -1
+    int top(){
+        if(IsEmpty())
             return -1;
-        // Return the element at the front position
         return arr[front];
     }
-    int getSize()
-    {
-        // Return the current size of the queue
+    int GetSize(){
         return size;
     }
 };
-int main()
-{
-    // Create a stack of maximum size 3
-    Stack s(3);
-    s.push(1);
-    s.push(2);
-    s.push(3);
- 
-    cout << "current size: " << s.getSize() << endl;
-    cout << s.top() << endl;
-    s.pop();
-    cout << s.top() << endl;
-    s.pop();
-    cout << s.top() << endl;
- 
-    cout << "current size: " << s.getSize() << endl;
-    return 0;
+int main(){
+    Stack st(5);
+    for(int i = 1; i <= 6; i++){
+        st.push(i);
+        cout << st.top() << " ";
+        st.pop();
+    }
+    cout <<"\nSize is: " << st.GetSize();
 }
