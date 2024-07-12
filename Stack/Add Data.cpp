e0513253182c1f963d5058bdcd97f0d42535d350
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-07-12 08:10:48 +0200

    Update Add Data.cpp

--- Stack/Add Data.cpp
+++ Stack/Add Data.cpp
@@ -1,138 +1,114 @@
 //add two numbers represented by Linked Lists using Stack
-#include <bits/stdc++.h>
+#include<iostream>
+#include<stack>
 using namespace std;
-class Node {
-public:
+class node{
+    public:
     int data;
-    Node* next;
+    node *nxt;
 };
-Node* newnode(int data)
-{
-    Node* x = new Node();
-    x->data = data;
-    return x;
+node *create(int val){
+    node *newnode = new node();
+    newnode->nxt = NULL;
+    newnode-> data = val;
+    return newnode;
 }
 // function that returns the sum of two numbers represented by linked lists
-Node* addTwoNumbers(Node* l1, Node* l2)
-{
-    Node* prev = NULL;
-    // Create 3 stacks
-    stack<Node*> s1, s2, s3;
-    // Fill first stack with first List Elements
-    while (l1 != NULL) {
-        s1.push(l1);
-        l1 = l1->next;
+node *AddTwoNum(node *l1, node *l2){
+    node *prv = NULL;
+    stack <node*>a, b, c;
+    //Fill First & second second with list elements
+    while(l1 !=NULL){
+        a.push(l1);
+        l1 = l1->nxt;
     }
-    // Fill second stack with second List Elements
-    while (l2 != NULL) {
-        s2.push(l2);
-        l2 = l2->next;
+    while(l2!=NULL){
+        b.push(l2);
+        l2 = l2->nxt;
     }
     int carry = 0;
-    // Fill the third stack with the sum of first and second stack
-    while (!s1.empty() && !s2.empty()) {
-        int sum = s1.top()->data + s2.top()->data + carry;
-        Node* temp = newnode(sum % 10);
-        s3.push(temp);
-        if (sum > 9) {
+    while(!a.empty() &&! b.empty()){
+        int sum = a.top()->data + b.top()->data + carry;
+        node *tmp = create(sum % 10);
+        c.push(tmp);
+        if(sum > 9)
             carry = 1;
-        }
-        else {
+        else
             carry = 0;
-        }
-        s1.pop();
-        s2.pop();
+        a.pop(); b.pop();
     }
-    while (!s1.empty()) {
-        int sum = carry + s1.top()->data;
-        Node* temp = newnode(sum % 10);
-        s3.push(temp);
-        if (sum > 9) {
+    while(!a.empty()){
+        int sum = a.top()->data + carry;
+        node *tmp =create(sum % 10);
+        c.push(tmp);
+        if(sum > 9)
             carry = 1;
-        }
-        else {
+        else
             carry = 0;
-        }
-        s1.pop();
+        a.pop();
     }
-    while (!s2.empty()) {
-        int sum = carry + s2.top()->data;
-        Node* temp = newnode(sum % 10);
-        s3.push(temp);
-        if (sum > 9) {
+    while(!b.empty()){
+        int sum = b.top()->data + carry;
+        node *tmp = create(sum % 10);
+        c.push(tmp);
+        if(sum > 9)
             carry = 1;
-        }
-        else {
+        else
             carry = 0;
-        }
-        s2.pop();
+        b.pop();
     }
-    // If carry is still present create a new node with value 1 and push it to the third stack
-    if (carry == 1) {
-        Node* temp = newnode(1);
-        s3.push(temp);
+    // If carry is still present create a new node with 
+    //value 1 and push it to the third stack
+    if(carry == 1){
+        node *tmp = create(1);
+        c.push(tmp);
     }
     // Link all the elements inside third stack with each other
-    if (!s3.empty())
-        prev = s3.top();
-    while (!s3.empty()) {
-        Node* temp = s3.top();
-        s3.pop();
-        if (s3.size() == 0) {
-            temp->next = NULL;
-        }
-        else {
-            temp->next = s3.top();
-        }
+    if(!c.empty())
+        prv = c.top();
+    while(!c.empty()){
+        node *tmp = c.top();
+        c.pop();
+        if(c.size() == 0)
+            tmp->nxt = NULL;
+        else
+            tmp->nxt = c.top();
     }
-    return prev;
+    return prv;
 }
-void Display(Node* head)
-{
-    if (head == NULL) {
+void Display(node *head){
+    cout << "\nElements are: ";
+    if(head == NULL)
+        return;
+    while(head->nxt !=NULL){
+        cout << head->data << " ";
+        head = head ->nxt;
+    }
+    cout << head->data << " ";
+}
+void push(node **head, int val){
+    node *newnode = create(val);
+    if(*head == NULL){
+        newnode ->nxt = *head;
+        *head = newnode;
         return;
     }
-    while (head->next != NULL) {
-        cout << head->data << " -> ";
-        head = head->next;
-    }
-    cout << head->data << endl;
-}
-// Function that adds element at the end of the Linked List
-void push(Node** head_ref, int d)
-{
-    Node* new_node = newnode(d);
-    new_node->next = NULL;
-    if (*head_ref == NULL) {
-        new_node->next = *head_ref;
-        *head_ref = new_node;
-        return;
-    }
-    Node* last = *head_ref;
-    while (last->next != NULL && last != NULL) {
-        last = last->next;
-    }
-    last->next = new_node;
+    node *trv = *head;
+    while(trv !=NULL && trv->nxt !=NULL)
+        trv = trv->nxt;
+    trv->nxt = newnode;
     return;
 }
-int main()
-{
-    Node* first = NULL;
-    Node* second = NULL;
-    Node* sum = NULL;
-    push(&first, 7);
-    push(&first, 5);
-    push(&first, 9);
-    push(&first, 4);
-    push(&first, 6);
-    push(&second, 8);
-    push(&second, 4);
-    cout << "First List : ";
-    Display(first);
-    cout << "Second List : ";
-    Display(second);
-    sum = addTwoNumbers(first, second);
-    cout << "Sum List : ";
-    Display(sum);
-    return 0;
+int main(){
+    node *x = NULL;
+    node *y = NULL;
+    node *z = NULL;
+    for(int i = 1; i < 11; i+= 2)
+        push(&x, i);
+    for(int i = 0; i < 11; i+= 2)
+        push(&y, i);
+    Display(x);
+    Display(y);
+    z = AddTwoNum(x, y);
+    Display(z);
 }
