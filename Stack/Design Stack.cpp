3ff27fa055081c51f26882d8af0390ca5f65125e
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-07-13 08:04:03 +0200

    Update Design Stack.cpp

--- Stack/Design Stack.cpp
+++ Stack/Design Stack.cpp
@@ -1,86 +1,58 @@
-#include <bits/stdc++.h>
+#include<iostream>
+#include<stack>
 using namespace std;
 // A user defined stack that supports getMin() in addition to push() and pop()
-struct MyStack {
-    stack<int> s;
-    int minEle;
-    // Prints minimum element of MyStack
-    void getMin()
-    {
-        if (s.empty())
-            cout << "Stack is empty\n";
- 
-        // variable minEle stores the minimum element in the stack.
-        else
-            cout << "Minimum Element in the stack is: "
-                 << minEle << "\n";
-    }
-    // Prints top element of MyStack
-    void peek()
-    {
-        if (s.empty()) {
-            cout << "Stack is empty ";
+struct Stack{
+    stack <int> st;
+    int mini;
+    void push(int x){
+        if(st.empty()){
+            mini = x;
+            st.push(x);
             return;
         }
-        int t = s.top(); 
-        cout << "Top Most Element is: ";
-        // If t < minEle means minEle stores value of t.
-        (t < minEle) ? cout << minEle : cout << t;
-    }
-    // Remove the top element from MyStack
-    void pop()
-    {
-        if (s.empty()) {
-            cout << "Stack is empty\n";
-            return;
-        }
-        cout << "Top Most Element Removed: ";
-        int t = s.top();
-        s.pop();
- 
-        // Minimum will change as the minimum element of the stack is being removed.
-        if (t < minEle) {
-            cout << minEle << "\n";
-            minEle = 2 * minEle - t;
+        else if(x < mini){
+            st.push(2 * x - mini);
+            mini = x;
         }
         else
-            cout << t << "\n";
+            st.push(x);
+        cout << "\nInserted Element is: " << x;
     }
-    // Removes top element from MyStack
-    void push(int x)
-    {
-        // Insert new number into the stack
-        if (s.empty()) {
-            minEle = x;
-            s.push(x);
-            cout << "Number Inserted: " << x << "\n";
+    void pop(){
+        if(st.empty()){
+            cout << "Stack is empty";
             return;
         }
-        // If new number is less than minEle
-        else if (x < minEle) {
-            s.push(2 * x - minEle);
-            minEle = x;
+        int Top = st.top();
+        st.pop();
+        if(Top < mini){
+            cout << mini << " ";
+            mini = 2 * mini - Top;
         }
- 
         else
-            s.push(x);
- 
-        cout << "Number Inserted: " << x << "\n";
+            cout << Top;
+    }
+    void peek(){
+        if(st.empty()){
+            cout << "Stack Is empty";
+            abort();
+        }
+        int Top = st.top();
+        (Top < mini)?cout << "\nPeek: " << Top:cout << "\nPeek: " << mini;
+    }
+    void GetMin(){
+        if(st.empty()){
+            cout << "stack is empty";
+            abort();
+        }
+        cout << "\nMin: " << mini;
     }
 };
-int main()
-{
-    MyStack s;
-    s.push(3);
-    s.push(5);
-    s.getMin();
-    s.push(2);
-    s.push(1);
-    s.getMin();
-    s.pop();
-    s.getMin();
-    s.pop();
-    s.peek();
- 
-    return 0;
+int main(){
+    Stack st;
+    for(int i = 1; i <= 5; i++)
+        st.push(i);
+    st.GetMin();
+    st.peek();
 }
