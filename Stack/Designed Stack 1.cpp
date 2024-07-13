c4e582ed407bb71c1a681093d7ab838c32d77a81
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-07-13 08:20:37 +0200

    Update Designed Stack 1.cpp

--- Stack/Designed Stack 1.cpp
+++ Stack/Designed Stack 1.cpp
@@ -1,49 +1,32 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-int mini(int a, int b) { return a > b ? b : a; }
-class MinStack {
-public:
-    stack<pair<int, int> > s;
- 
-    void push(int element)
-    {
-        // new max will be given no. if stack is empty else
-        //we compare given no. to max at current top of stack
-        int new_min = s.empty()
-                          ? element
-                          : mini(element, s.top().second);
-        // we push the pair of given_element,new_min in s
-        s.push({ element, new_min });
+int mini(int a, int b){
+    return a < b ?a: b;
+}
+struct MinStack{
+    stack<pair<int, int> >st;
+    void push(int val){
+        int Mini = (st.empty())?val :mini(val, st.top().second);
+        st.push({val, Mini});
     }
-    int pop()
-    {
-        int popped;
-        if (!s.empty()) {
-            // popped has popped number
-            popped = s.top().first;
-            s.pop();
+    int pop(){
+        int x;
+        if(!st.empty()){
+            x = st.top().first;
+            st.pop();
         }
-        else {
-            // print a message or throw exception etc
-        }
-        return popped;
+        else
+            cout << "\nstack is empty";
+        return x;
     }
-    int minimum()
-    {
-        int min_elem = s.top().second;
-        return min_elem;
+    int GetMin(){
+        int m = st.top().second;
+        return m;
     }
 };
-int main()
-{
-    MinStack s;
-    s.push(-1);
-    s.push(10);
-    s.push(-4);
-    s.push(0);
-    cout << s.minimum() << endl;
-    cout << s.pop() << endl;
-    cout << s.pop() << endl;
-    cout << s.minimum();
-    return 0;
+int main(){
+    MinStack st;
+    for(int i = 0; i < 6; i++)
+        st.push(i);
+    cout << st.GetMin();
 }
