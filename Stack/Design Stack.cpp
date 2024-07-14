069f05dd8d11a7a458c2a6888f963cfe4504fe31
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-07-14 22:37:05 +0200

    Update Design Stack.cpp

--- Stack/Design Stack.cpp
+++ Stack/Design Stack.cpp
@@ -49,10 +49,40 @@ struct Stack{
         cout << "\nMin: " << mini;
     }
 };
+//****//
+int mini(int a, int b){
+    return a < b ?a: b;
+}
+struct MinStack{
+    stack<pair<int, int> >st;
+    void push(int val){
+        int Mini = (st.empty())?val :mini(val, st.top().second);
+        st.push({val, Mini});
+    }
+    int pop(){
+        int x;
+        if(!st.empty()){
+            x = st.top().first;
+            st.pop();
+        }
+        else
+            cout << "\nstack is empty";
+        return x;
+    }
+    int GetMin(){
+        int m = st.top().second;
+        return m;
+    }
+};
 int main(){
     Stack st;
     for(int i = 1; i <= 5; i++)
         st.push(i);
     st.GetMin();
     st.peek();
+    //****//
+    MinStack st;
+    for(int i = 0; i < 6; i++)
+        st.push(i);
+    cout << st.GetMin();
 }
