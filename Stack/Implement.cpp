67a570a41011ee1d479ac856dbbbebba3f797616
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-07-14 22:46:09 +0200

    Update Implement.cpp

--- Stack/Implement.cpp
+++ Stack/Implement.cpp
@@ -49,6 +49,56 @@ class Stack{
         top = prv;
     }
 };
+// Implement a stack using singly linked list
+class node{
+    public:
+    int data; node *nxt;
+    node(int val):data(val), nxt(NULL){}
+};
+class Stack{
+    node *top;
+    public:
+    Stack():top(NULL){}
+    void push(int x){
+        node *newnode = new node(x);
+        if(newnode == NULL){
+            cout << "Stack overflow";
+            exit(1);
+        }
+        newnode->data = x;
+        newnode->nxt = top;
+        top = newnode;
+    }
+    bool IsEmpty(){
+        return top == NULL;
+    }
+    int peek(){
+        if(!IsEmpty())
+            return top->data;
+        exit(1);
+    }
+    void pop(){
+        node *tmp = top;
+        if(top == NULL){
+            cout << "\nUnderflow";
+            exit(1);
+        }
+        top = top->nxt;
+        delete tmp;
+    }
+    void Display(){
+        node *tmp = top;
+        if(top == NULL){
+            cout << "\nUnderflow";
+            exit(1);
+        }
+        cout << "\nElements are: ";
+        while(tmp!=NULL){
+            cout << tmp->data << " ";
+            tmp = tmp->nxt;
+        }
+    }
+};
 int main(){
     Stack *st = new Stack();
     for(int i = 1; i <= 6; i++){
@@ -57,5 +107,14 @@ int main(){
     st->Display();
     st->Reverse();
     st->Display();
+    //****//
+    Stack st;
+    for(int i = 1; i <= 10; i++)
+        st.push(i);
+    st.Display();
+    cout << "\nElements are: ";
+    while(!st.IsEmpty()){
+        cout << st.peek() << " ";
+        st.pop();
+    }
 }
-
