bef503b0fc4fbf6065ad4023ab747a9a699b129e
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-07-14 18:39:27 +0200

    Update and rename Stack/Convert To Postfix 3.cpp to Convert To Postfix.cpp

--- Convert To Postfix.cpp
+++ Convert To Postfix.cpp
@@ -43,7 +43,28 @@ void InfixToPostfix(string str){
     }
     cout << result;
 }
+bool IsOperator(char x){
+    if(x == '+' || x == '-' || x == '*' || x == '/')
+        return true;
+    return false;
+}
+string PrefixToPostfix(string str){
+    stack < string> st;
+    for(int i = str.size() -1; i >= 0; i--){
+        if(IsOperator(str[i])){
+            string x = st.top(); st.pop();
+            string y = st.top(); st.pop();
+            string tmp = x + y + str[i];
+            st.push(tmp);
+        }
+        else
+            st.push(string(1, str[i]));
+    }
+    return st.top();
+}
 int main(){
     string str ="a+b*(c^d-e)^(f+g*h)-i";
     InfixToPostfix(str);
+    string str = "*-A/BC-/AKL";
+    cout << PrefixToPostfix(str);
 }
