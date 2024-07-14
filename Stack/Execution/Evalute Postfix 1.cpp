619176ae8c88f257fde7bc1677ef78236c1de7ff
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-07-14 07:44:19 +0200

    Update Evalute Postfix 1.cpp

--- Stack/Execution/Evalute Postfix 1.cpp
+++ Stack/Execution/Evalute Postfix 1.cpp
@@ -1,59 +1,47 @@
-// evaluate value of a postfix expression having multiple digit operands
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-// returns value of a given postfix expression
-int evaluatePostfix(char* exp)
-{
-    // Create a stack of capacity equal to expression size
-    stack<int> st;
+// evaluate value of a postfix expression having multiple digit operands
+int EvalutePostfix(char *str){
+    stack <int> st;
     int i;
-    // Scan all characters one by one
-    for (i = 0; exp[i]; ++i){
-        // If the character is blank space then continue
-        if (exp[i] == ' ')
+    //in loop str[i] means str[i]!=NULL
+    for(i = 0; str[i]; i++){
+        if(str[i] == ' ')
             continue;
-        // If the scanned character is an operand (number here),extract the full number
-        // Push it to the stack.
-        else if (isdigit(exp[i])) {
+        // If the scanned character is an operand (number here)
+        //extract the full number Push it to the stack.
+        else if(isdigit(str[i])){
             int num = 0;
-            // Extract full number
-            while (isdigit(exp[i])) {
-                num = num * 10 + (int)(exp[i] - '0');
+            while(isdigit(str[i])){
+                num = num * 10 + (int)(str[i] - '0');
                 i++;
             }
-            After the loop exits, i has been incremented one extra time (pointing to a non-digit character). i-- decrements the index back to the last digit character.
+            //After the loop exits, i has been incremented one extra time 
+            //(pointing to a non-digit character). i-- decrements the index back to the last digit character.
             i--;
-            // Push the element in the stack
             st.push(num);
-        }
-        // If the scanned character is an operator, pop two elements from stack apply the operator
-        else {
-            int val1 = st.top();
-            st.pop();
-            int val2 = st.top();
-            st.pop();
- 
-            switch (exp[i]) {
-            case '+':
-                st.push(val2 + val1);
-                break;
-            case '-':
-                st.push(val2 - val1);
-                break;
-            case '*':
-                st.push(val2 * val1);
-                break;
-            case '/':
-                st.push(val2 / val1);
-                break;
+        } else {
+            int x = st.top(); st.pop();
+            int y = st.top(); st.pop();
+            switch(str[i]){
+                case '+':
+                     st.push(x + y);
+                     break;
+                case '-':
+                    st.push(y - x);
+                    break;
+                case '*':
+                    st.push(x * y);
+                    break;
+                case '/':
+                    st.push(y / x);
+                    break;
             }
         }
     }
     return st.top();
 }
-int main()
-{
-    char exp[] = "100 200 + 2 / 5 * 7 +";
-    cout << evaluatePostfix(exp);
-    return 0;
+int main(){
+    char str[] = "100 200 + 2 / 5 * 7 +";
+    cout << EvalutePostfix(str);
 }
